197cb2e1ce02f703d9e89151503b473b83f93dd7
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-03-04 00:21:08 +0200

    Added an exception handler for RecordNotFound

--- apps_task/C4_Blog_Django/blog/db/schema.rb
+++ apps_task/C4_Blog_Django/blog/db/schema.rb
@@ -13,15 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120224225432) do
 
-  create_table "articles", :force => true do |t|
-    t.string   "name"
-    t.text     "content"
-    t.string   "author_name"
-    t.datetime "published_at"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password_digest"
