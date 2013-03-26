a497e96d71f0e32a24a4e8332a466b5d1d4af603
mohammad-abdulkhaliq <mohammad28march1993@gmail.com>
2013-03-27 00:55:17 +0200

    Issue

--- config/routes.rb
+++ config/routes.rb
@@ -54,12 +54,11 @@ Sprint0::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
   
   #2.3 Create/Edit Tags
-  resources :tags do
-    resources :tags
-  end
+  resources :tags
+  
+  match 'tags/:id/synonym' => 'tags#addsym', :via => :put
   
   root :to => 'tags#index'
   
