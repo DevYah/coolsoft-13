2360899ba4faa5a18e65aa7df2f2c05ccd4b8c33
Hesham Nabil <heshamnabil13@gmail.com>
2013-03-27 20:46:28 +0200

    added documentation to index method.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
-	
+	#index method is the method used to display the idea stream, top ten and trending ideas.
+	#@approved is the list of all the approved ideas according to the flag "approved"
 	def index
 		@approved = Idea.find(:all, :conditions => { :approved => true })
 
