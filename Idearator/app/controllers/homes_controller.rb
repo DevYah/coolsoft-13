368ad0fbc4a4ca64259b1e978cb77776c2e2ce3e
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-03-27 09:54:45 +0200

    issue #38 ,adds top ten view showing title number of votes and user name

--- Idearator/app/controllers/homes_controller.rb
+++ Idearator/app/controllers/homes_controller.rb
@@ -1,12 +1,9 @@
 class HomesController < ApplicationController
+	
 	def index
-
 @top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
-
-
 render :action => 'index.html.erb'
-
-end
+    end
 
 end
 
