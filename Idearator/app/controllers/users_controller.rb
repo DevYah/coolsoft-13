34f81dba91faf1c8d2c63090226f430f1bb50ffa
AminaZoheir <amina.zoheir@gmail.com>
2013-03-27 09:37:27 +0200

    Issue #42 Adds action deactivate

--- Idearator/app/controllers/users_controller.rb
+++ Idearator/app/controllers/users_controller.rb
@@ -4,4 +4,21 @@ class UsersController < ApplicationController
 		@user = current_user
 	end
 
+	def deactivate
+		if current_user.password == params[:user][:password]
+			current_user.status = "deactivated"
+			current_user.save
+			#sign_out current_user
+			respond_to do |format|
+				format.html { redirect_to  '/' , notice: 'Successfully deactivated' }
+				format.json { head :no_content }
+			end
+		else
+			respond_to do |format|
+				format.html { redirect_to action: 'confirm_deactivate' , notice: 'Wrong password'}
+				format.json { head :no_content }
+			end
+		end
+	end
+
 end
