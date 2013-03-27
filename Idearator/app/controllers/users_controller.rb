f9a1207b3c852b6ac41f6a7e64376e7100b85056
Mohamed Sameh Zaki <mohamed.sameh226@gmail.com>
2013-03-27 21:29:49 +0200

    issue #37, added committee choose area of expertise

--- Idearator/app/controllers/users_controller.rb
+++ Idearator/app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+	before_filter :authenticate_user!, :only => [:expertise, :new_committee_tag]
+	def expertise
+		@user= current_user
+		@tags= Tag.all
+	end
+	def new_committee_tag
+		if params[:user] == nil
+			respond_to do |format|
+				format.html{
+				flash[:notice] = 'You must choose at least 1 area of expertise'
+				redirect_to action: 'expertise'
+				}
+			end
+		else
+		@tags= params[:user][:tags]
+			@tags.each do |tag|
+				CommitteesTags.create(:committee_id => current_user.id , :tag_id => tag)
+			end
+		respond_to do |format|
+				format.html{
+				redirect_to controller: 'home', action: 'index'
+				}
+			end
+		end
+	end
 end
