810c9d4b22349079a6387aab75926ae341c3b6f7
AminaZoheir <amina.zoheir@gmail.com>
2013-03-27 21:37:01 +0200

    Adds documentation

--- Idearator/app/controllers/users_controller.rb
+++ Idearator/app/controllers/users_controller.rb
@@ -2,10 +2,18 @@ class UsersController < ApplicationController
 
 	before_filter :authenticate_user!, :only => [:deactivate, :confirm_deactivate, :activate]
 
+	#method displays a form where the user enters his password to confrim deactivation.
+	#Params: none
+	#Author: Amina Zoheir
 	def confirm_deactivate
 		@user = current_user
 	end
 
+	#method checks the entered password if it's the current users password 
+	#it changes his status to deactivated and signs him out. 
+	#Params: 
+	#password:: the parameter is an instance of User passed through the form form confirm deactivate.
+	#Author: Amina Zoheir
 	def deactivate
 		if current_user.password == params[:user][:password]
 			current_user.status = "deactivated"
@@ -25,6 +33,9 @@ class UsersController < ApplicationController
 		end
 	end
 
+	#method sets the status of the current user to active
+	#Params: none
+	#Author: Amina Zoheir
 	def activate
 		current_user.status = "active"
 		respond_to do |format|
