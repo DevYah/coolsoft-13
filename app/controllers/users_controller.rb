66a9359e60fef2f145f44bb1804885d2a599b503
Mohamed Sameh Zaki <mohamed.sameh226@gmail.com>
2013-03-27 21:43:23 +0200

    issue #37, added documentation to my methods in the controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
 	before_filter :authenticate_user!, :only => [:expertise, :new_committee_tag]
+	#This method sends to the view all the tags available in the tag table
+	#so they can be projected as checkboxes for the user 
+	#to choose his area of expertise from them.
+	#It also Sends the user_id "current_user" to be used in the form in the view.
+	#Author: Mohamed Sameh
 	def expertise
 		@user= current_user
 		@tags= Tag.all
 	end
+	#This method recieves all the checked tags from the view expertise in params,
+	# and loops through all these tags and enters each one in the table committees_tags
+	#it then redirects the user to the homepage. 
+	#Author: Mohamed Sameh
 	def new_committee_tag
 		if params[:user] == nil
 			respond_to do |format|
