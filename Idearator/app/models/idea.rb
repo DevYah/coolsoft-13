a8ee0a3b610adbf7b4b1d3d19446230d0bdf7be1
marwa-mehanna <marwa.mehana@gmail.com>
2013-03-27 15:12:04 +0200

    Issue#39 Adding Idea's MVC

--- Idearator/app/models/idea.rb
+++ Idearator/app/models/idea.rb
@@ -1,5 +1,5 @@
 class Idea < ActiveRecord::Base
-   attr_accessible :title, :description, :problem_solved
+   attr_accessible :title, :description, :problem_solved ,:photo
 
   belongs_to :user
   has_many :comments
@@ -8,4 +8,5 @@ class Idea < ActiveRecord::Base
   has_and_belongs_to_many :committees
   has_and_belongs_to_many :tags
   has_and_belongs_to_many :users, :join_table => :votes
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
