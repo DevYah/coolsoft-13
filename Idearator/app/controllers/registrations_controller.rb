4fd65aef4bc4f31c37c6c3c0cdcbd76c3d50439f
mennaamr <menna.amr2@gmail.com>
2013-03-27 22:15:07 +0200

    Fixed redirection bug for committee sign up

--- Idearator/app/controllers/registrations_controller.rb
+++ Idearator/app/controllers/registrations_controller.rb
@@ -2,13 +2,17 @@ class RegistrationsController < Devise::RegistrationsController
   protected
     def after_sign_up_path_for(resource)
       if resource.type.is_a? Committee
-        url_for :controller => "users", :action => "expertise"
+        "/users/expertise"
+      else
+        "/"
       end
     end        
 
     def after_inactive_sign_up_path_for(resource)
       if resource.type.is_a? Committee
-        url_for :controller => "users", :action => "expertise"
+        "/users/expertise"
+      else
+        "/"
       end
     end        
 end
