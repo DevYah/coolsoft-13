982f336894f872c87cf0eab9dc870e1840b6e04b
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-03-28 00:22:46 +0200

    Added brief documentation

--- Idearator/app/controllers/users_controller.rb
+++ Idearator/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  #Description:Two variables are needed.A variable "user" to get the user for this particular profile,
+  # ana a variable "ideas" that stores this user's ideas. The view accesses the attributes of both the two
+  #variables and shows the information.
+  #Author:Hisham ElGezeery
   def show
     @user = User.find(params[:id])
     @ideas = Idea.find(:all, :conditions => { :user_id => @user.id })
