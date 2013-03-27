46991eb5161d6170c20c54557273f8d821b8810b
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-03-27 23:04:29 +0200

    issue #38 ,added documentation

--- Idearator/app/controllers/home_controller.rb
+++ Idearator/app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
-	# Method gets all ideas , order them in descending order according to number of votes 
+	# Method gets all ideas, order them in descending order according to number of votes 
 	# and sends maximum ten ideas to index view page
-	#Lina Basheer 
+	# Author: Lina Basheer 
 	def index
 @top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
 render :action => 'index.html.erb'
