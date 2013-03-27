bc42df55d2a4b6e18155f5d3805d8fee4c0d41f1
Hesham Nabil <heshamnabil13@gmail.com>
2013-03-27 20:15:04 +0200

    #36 removed homes_conteroller

--- Idearator/app/controllers/home_controller.rb
+++ Idearator/app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
 	
 	def index
 		@approved = Idea.find(:all, :conditions => { :approved => true })
+
+		@top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
+        render :action => 'index.html.erb'
 	end
 		
 end
