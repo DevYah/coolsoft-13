9cef8272b71ec68589277ee14e7df08143c4e3b7
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-03-27 22:29:43 +0200

    added image in top view and document

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
-	
+	# Method gets all ideas , order them in descending order according to number of votes 
+	# and sends maximum ten ideas to index view page
+	#Lina Basheer 
 	def index
 @top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
 render :action => 'index.html.erb'
     end
-
 end
 
