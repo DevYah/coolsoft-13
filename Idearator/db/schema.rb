681689dcd825dd961e24082aae9ac24dc3a0a1ce
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-03-27 16:41:40 +0200

    Edit committeIdeas relationship

--- Idearator/db/schema.rb
+++ Idearator/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130327142819) do
+ActiveRecord::Schema.define(:version => 20130327143347) do
 
   create_table "action_notifications", :force => true do |t|
     t.string   "action"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(:version => 20130327142819) do
     t.datetime "created_at",                                       :null => false
     t.datetime "updated_at",                                       :null => false
     t.boolean  "approved",                      :default => false
+    t.integer  "committee_id"
   end
 
   create_table "ideas_tags", :id => false, :force => true do |t|
