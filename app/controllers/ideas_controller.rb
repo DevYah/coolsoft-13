e1d1451784277a09335bb4d61a3e7059f3aa5cd2
MahmoudHashish <mahmoudhashish@ubuntu.ubuntu-domain>
2013-03-27 16:54:13 +0200

    Issue#44 Deleting Ideas from database without photos

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,53 +1,79 @@
 class IdeasController < ApplicationController
-def show
-      @idea = Idea.find(params[:id])
-     # @idea=Idea.new
-       #rescue ActiveRecord::RecordNotFound
+	def show
+      		@idea = Idea.find(params[:id])
+     		# @idea=Idea.new
+     		#rescue ActiveRecord::RecordNotFound
       
-      respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @idea }
-       end
-     end
+      		respond_to do |format|
+      			format.html # show.html.erb
+      			format.json { render json: @idea }
+      		end
+     	end
+	
 	def index
 
 	end
+	
 	def new
 		@idea=Idea.new
 		
 		respond_to do |format|
-	      format.html # new.html.erb
-	      format.json { render json: @idea }
-	    end
+	      		format.html # new.html.erb
+	      		format.json { render json: @idea }
+	    	end
 	end
+	
 	def edit   
-    @idea = Idea.find(params[:id])
-  end
-  def update
+    		@idea = Idea.find(params[:id])
+  	end
+  	
+	def update
     
-    @idea = Idea.find(params[:id])
-    respond_to do |format|
-      if @idea.update_attributes(params[:idea])
-        format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+    		@idea = Idea.find(params[:id])
+    
+		respond_to do |format|
+      			if @idea.update_attributes(params[:idea])
+        			format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
+        			format.json { head :no_content }
+      			else
+        			format.html { render action: "edit" }
+        			format.json { render json: @idea.errors, status: :unprocessable_entity }
+      			end
+    		end
+  	end
+	
 	def create
-    @idea = Idea.new(params[:idea])
+    		@idea = Idea.new(params[:idea])
      
-    respond_to do |format|
-      if @idea.save
-        format.html { redirect_to @idea, notice: 'idea was successfully created.' }
-        format.json { render json: @idea, status: :created, location: @idea }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+    		respond_to do |format|
+      			if @idea.save
+        			format.html { redirect_to @idea, notice: 'idea was successfully created.' }
+        			format.json { render json: @idea, status: :created, location: @idea }
+      			else
+        			format.html { render action: "new" }
+        			format.json { render json: @idea.errors, status: :unprocessable_entity }
+      			end
+   		end
+  	end
+
+	def destroy
+		@current_user = User.first
+    		@idea = Idea.find(params[:id])
+		puts "DEBUG: " + @current_user.id.to_s + " " + @idea.user_id.to_s
+		if @current_user.id == @idea.user_id    
+			puts "authed!"		
+			@idea.destroy
+    			
+    			respond_to do |format|
+    				format.html { redirect_to '/' }
+      				format.json { head :no_content }
+    			end
+		else
+			respond_to do |format|
+    				format.html { redirect_to @idea }
+      				format.json { notice 'idea cannot be deleted' }
+    			end
+		end
+  	end
 end
 
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,11 +1,11 @@
 class Idea < ActiveRecord::Base
-   attr_accessible :title, :description, :problem_solved
+  attr_accessible :title, :description, :problem_solved, :user_id
 
   belongs_to :user
+  belongs_to :committee
   has_many :comments
   has_many :action_notifications
   has_many :ratings
-  has_and_belongs_to_many :committees
   has_and_belongs_to_many :tags
   has_and_belongs_to_many :users, :join_table => :votes
 end
