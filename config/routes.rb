105c1aa22c1a12dbe43a75ff76ce51ba7edaee62
AminaZoheir <amina.zoheir@gmail.com>
2013-03-28 21:15:25 +0200

    Issue #42 Adds Deactivate link

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Sprint0::Application.routes.draw do
   default_url_options :host => "localhost:3000"
 
   devise_for :users, :controllers => { :registrations => "registrations" }
+  devise_for :committees, :controllers => { :registrations => "registrations" }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
