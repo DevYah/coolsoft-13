b90647afcadacab4c3582d094f3a139817a34a29
Mohamed Sameh Zaki <mohamed.sameh226@gmail.com>
2013-03-28 17:06:26 +0200

    issue #37, Code problems fixed.

--- Idearator/app/controllers/users_controller.rb
+++ Idearator/app/controllers/users_controller.rb
@@ -17,18 +17,18 @@ class UsersController < ApplicationController
 		if params[:user] == nil
 			respond_to do |format|
 				format.html{
-				flash[:notice] = 'You must choose at least 1 area of expertise'
-				redirect_to action: 'expertise'
+					flash[:notice] = 'You must choose at least 1 area of expertise'
+					redirect_to action: 'expertise'
 				}
 			end
 		else
-		@tags= params[:user][:tags]
+			@tags= params[:user][:tags]
 			@tags.each do |tag|
 				CommitteesTags.create(:committee_id => current_user.id , :tag_id => tag)
 			end
-		respond_to do |format|
+			respond_to do |format|
 				format.html{
-				redirect_to controller: 'home', action: 'index'
+					redirect_to controller: 'home', action: 'index'
 				}
 			end
 		end
