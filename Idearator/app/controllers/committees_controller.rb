d0dac5d0f582e256d793e3109725ddf4f4a071d0
Omar Kassem <omar.kassem67@gmail.com>
2013-03-28 21:28:07 +0200

    Issue #43  Unmerged code final

--- Idearator/app/controllers/committees_controller.rb
+++ Idearator/app/controllers/committees_controller.rb
@@ -2,7 +2,7 @@ class CommitteesController < ApplicationController
 #.reject! iterates on every element in a list and removes every element that accepts the following condition
 # the & is used to generate a list having the commen values of the two lists
     def review_ideas 
-	@committee=current_user
+	@committee=Committee.find(1)
 
 	@ideas=Idea.find(:all, :conditions =>{:approved => false})
         @ideas.reject! do |i|
