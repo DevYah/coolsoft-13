a4acaad99ae31bb8da5bb3e206ae0b09fca2ad3c
Dayna161193 <daynahany@gmail.com>
2013-03-28 21:49:35 +0200

    Revert "Resolved conflicts"
    This reverts commit e708588229bd99ea55ae51e060f799a2406310ce, reversing
    changes made to 0e67c3d731e69507be7dcdacd60a9e218f8e1fbf.

--- Idearator/app/controllers/ideas_controller.rb
+++ Idearator/app/controllers/ideas_controller.rb
@@ -1,92 +1,68 @@
 class IdeasController < ApplicationController
-  	#before_filter :authenticate_user!, :only => [:create, :edit]
 
-	def show
-  		current_user = User.find(1)
-    		#@user=current_user.id
-     		@idea = Idea.find(params[:id])
-     		# @idea=Idea.new
-       		#rescue ActiveRecord::RecordNotFound
+  #before_filter :authenticate_user!, :only => [:create, :edit]
+
+def show
+  current_user = User.find(1)
+    #@user=current_user.id
+      @idea = Idea.find(params[:id])
+     # @idea=Idea.new
+       #rescue ActiveRecord::RecordNotFound
       
-      		respond_to do |format|
-      			format.html # show.html.erb
-      			format.json { render json: @idea }
-      		end
-     	end
-	
+      respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @idea }
+       end
+     end
 	def index
 
 	end
-	
 	def new
 		@idea=Idea.new
-    		@tags= Tag.all
+    @tags= Tag.all
 		
 		respond_to do |format|
-	      		format.html # new.html.erb
-	      		format.json { render json: @idea }
-	    	end
+	      format.html # new.html.erb
+	      format.json { render json: @idea }
+	    end
 	end
-	
-	def edit
-    		@idea = Idea.find(params[:id])
-  	end
-  	
-  	def update
-    		@idea = Idea.find(params[:id])
-    
-		respond_to do |format|
-      			if @idea.update_attributes(params[:idea])
-        			format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
-        			format.json { head :no_content }
-      			else
-        			format.html { render action: "edit" }
-        			format.json { render json: @idea.errors, status: :unprocessable_entity }
-      			end
-    		end
-  	end
-	
-	def create
-    		#puts params
-    		#current_user = User.find(1)
-    		@idea = Idea.new(params[:idea])
-     		@idea.user_id=current_user.id
-    
-		respond_to do |format|
-      			if @idea.save
-        			@tags= params[:idea_tags][:tags]
-        			@tags.each do |tag|
-          				IdeasTag.create(:idea_id => @idea.id , :tag_id => tag)
-       				end
-        			format.html { redirect_to @idea, notice: 'idea was successfully created.' }
-        			format.json { render json: @idea, status: :created, location: @idea }
-      			else
-        			format.html { render action: "new" }
-        			format.json { render json: @idea.errors, status: :unprocessable_entity }
-      			end
-    		end
-  	end
+	def edit   
 
-	# Deletes the all records related to the +Idea+ instance from the database
-  	# Params:
-  	# +id+:: the id of the +Idea+ passed from the previous view, it helps in finding the +Idea+ instance from the database to process
-	# Author: Mahmoud Abdelghany Hashish
-	
-	def destroy
-		@idea = Idea.find(params[:id])
-		if current_user.id == @idea.user_id    
-			@idea.destroy
-    			
-    			respond_to do |format|
-    				format.html { redirect_to '/' }
-      				format.json { head :no_content }
-    			end
-		else
-			respond_to do |format|
-    				format.html { redirect_to @idea }
-      				format.json { notice 'idea cannot be deleted' }
-    			end
-		end
-  	end
+    @idea = Idea.find(params[:id])
+  end
+  def update
+    
+    @idea = Idea.find(params[:id])
+    respond_to do |format|
+      if @idea.update_attributes(params[:idea])
+        format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+	def create
+    puts params
+    current_user = User.find(1)
+    @idea = Idea.new(params[:idea])
+    
+
+     @idea.user_id=current_user.id
+    respond_to do |format|
+      if @idea.save
+        @tags= params[:idea_tags][:tags]
+        @tags.each do |tag|
+          IdeasTag.create(:idea_id => @idea.id , :tag_id => tag)
+        end
+        format.html { redirect_to @idea, notice: 'idea was successfully created.' }
+        format.json { render json: @idea, status: :created, location: @idea }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
 
