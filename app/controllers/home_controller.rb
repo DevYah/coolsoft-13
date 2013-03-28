198a044dc7e9388f52181301d34a420457a28871
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-03-28 22:25:22 +0200

    issue #38 , corrected documnetation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	# Method gets all ideas, order them in descending order according to number of votes 
-	# and sends maximum ten ideas to index view page
+	# and sends top ten ideas to index view page
 	# Author: Lina Basheer 
 	def index
 @top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
