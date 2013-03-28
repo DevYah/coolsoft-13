0e41f455cebd9d59b256d080eab1330fb43e1ee1
Dayna161193 <daynahany@gmail.com>
2013-03-28 23:32:00 +0200

    Updated the documentation

--- Idearator/app/controllers/comments_controller.rb
+++ Idearator/app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
-
+#Show all Comments 
+#author dayna
 def show
-    @idea = Idea.find(params[:id])
-    respond_to do |format|
+   @idea = Idea.find(params[:id])
+      respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @comment }
     end
@@ -10,37 +11,41 @@ def show
 def new
     @comment = Comment.new
 #@comment = Comment.new(params[:comment])
-    respond_to do |format|
+   respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @comment }
     end
   end
+#create new Comment by building comments after getting the id of the idea 
+#author dayna 
 def create
 ###@comment = Comment.new(params[:comment])
-  @idea = Idea.find(params[:idea_id])
-  @comment = @idea.comments.create(params[:comment])
-  @comment.update_attributes(:idea_id => @idea.id)
+   @idea = Idea.find(params[:idea_id])
+    @comment = @idea.comments.create(params[:comment])
+    @comment.update_attributes(:idea_id => @idea.id)
 
-   respond_to do |format|
+    respond_to do |format|
       if @comment.save
-        format.html { redirect_to(@idea, :notice => 'Comment was successfully created.') }
+       format.html { redirect_to(@idea, :notice => 'Comment was successfully created.') }
        format.xml  { render :xml => @idea, :status => :created, :location => @idea }
       else
-        format.html { redirect_to(@idea, :notice => 
+       format.html { redirect_to(@idea, :notice => 
         'Comment could not be saved. Please fill in all fields')}
         format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
-     end
+      end
     end
-redirect_to comment_path(@idea)
-  end
+   redirect_to comment_path(@idea)
+end
+#edit comment by checking the comment's id and modifying it
+#author dayna
 def edit
     @comment = Comment.find(params[:id])
     @idea = Idea.find(params[:idea_id])
-  end
+end
 def update
     @comment = Comment.find(params[:id])
     @idea = Idea.find(params[:idea_id])
-    respond_to do |format|
+     respond_to do |format|
       if @comment.update_attributes(params[:comment])
         format.html { redirect_to(@idea, :notice => 'Comment was successfully updated.') }
         format.xml  { head :no_content }
