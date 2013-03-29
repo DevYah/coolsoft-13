c981ed3620913eda10af58065d0a160ea4048508
mennaamr <menna.amr2@gmail.com>
2013-03-29 21:22:26 +0200

    Updated development file

--- Idearator/config/environments/development.rb
+++ Idearator/config/environments/development.rb
@@ -14,7 +14,7 @@ Sprint0::Application.configure do
   config.action_controller.perform_caching = false
 
   # Notify me if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
 
   # Print deprecation notices to the Rails logger
@@ -37,11 +37,9 @@ Sprint0::Application.configure do
   config.assets.debug = true
 
 #SMTP configuration for mail sending
-config.action_mailer.default_url_options = { :host => "localhost:3000" }
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
 
 
