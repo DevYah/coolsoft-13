7128b22e998e778ad76852ebf99d5b2aa4323def
Hesham Nabil <heshamnabil13@gmail.com>
2013-03-29 16:19:26 +0200

    fixed doc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	#index method is the method used to display the idea stream, top ten and trending ideas.
+	#Used to display the idea stream, top ten and trending ideas.
 	#Author: Hesham Nabil
 	def index
 		@approved = Idea.find(:all, :conditions => { :approved => true })
