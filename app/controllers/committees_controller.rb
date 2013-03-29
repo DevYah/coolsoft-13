42d5beaed9279b9a047df9090ab31ff162bca6e2
Omar Kassem <omar.kassem67@gmail.com>
2013-03-29 21:50:55 +0200

    #43,updated UML

--- app/controllers/committees_controller.rb
+++ app/controllers/committees_controller.rb
@@ -3,7 +3,6 @@ before_filter :authenticate_user!
 #.reject! iterates on every element in a list and removes every element that accepts the following condition
 # the & is used to generate a list having the commen values of the two lists
   def review_ideas 
-    
     @committee=current_user
     @ideas=Idea.find(:all, :conditions =>{:approved => false})
     @ideas.reject! do |i|
