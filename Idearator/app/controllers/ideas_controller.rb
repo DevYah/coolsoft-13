f53112e7c43b91e8659cfbfa711dcbe56531dc2c
marwa-mehanna <marwa.mehana@gmail.com>
2013-03-29 03:08:18 +0200

    Issue#39 adding final controllers

--- Idearator/app/controllers/ideas_controller.rb
+++ Idearator/app/controllers/ideas_controller.rb
@@ -1,50 +1,47 @@
 class IdeasController < ApplicationController
-
+  before_filter :authenticate_user!, :only => [:show, :create ,:edit]
   # view idea of current user
-  #+id+
+    #+id+ :: +Idea+
   #Marwa Mehanna
-def show
-  #current_user = User.find(1)
-    #@user=current_user.id
-      @idea = Idea.find(params[:id])
-     # @idea=Idea.new
-       #rescue ActiveRecord::RecordNotFound
-      
-      respond_to do |format|
+  def show
+    @user=current_user.id
+    @idea = Idea.find(params[:id])
+    respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @idea }
-       end
-     end
-	def index
-
-	end
+    end
+  end
+  def index
+  end
   # making new Idea
   #Marwa Mehanna
-	def new
-		@idea=Idea.new
+  def new
+    @idea=Idea.new
     @tags= Tag.all
-		
-		respond_to do |format|
-	      format.html # new.html.erb
-	      format.json { render json: @idea }
-	    end
-	end
-	# editing Idea
+    @chosentags=[]
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @idea }
+    end
+  end
+  # editing Idea
   #Params
-  #+id+
+    #+id+ :: +Idea+
   #Marwa Mehanna
   def edit   
-
     @idea = Idea.find(params[:id])
+    @tags= Tag.all
+    @chosentags= Idea.find(params[:id]).tags
   end
   # updating Idea
   #Params
-  #+id+ 
-  #+idea+ 
+    #+ideas_tags:: +IdeaTags+ 
+    #+id+ :: +Idea+
   #Marwa Mehanna
   def update
-    
+    puts(params[:ideas_tags][:tags])
     @idea = Idea.find(params[:id])
+    @idea.tag_ids=params["ideas_tags"]["tags"].collect{|t|t.to_i}
     respond_to do |format|
       if @idea.update_attributes(params[:idea])
         format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
@@ -57,32 +54,26 @@ def show
   end
   # creating new Idea
   #Params
-  #+idea+ 
-  #+idea_tags+ 
-  #+tags+ 
-  #+idea_id+
-  #+tag_id+
+    #+idea+ :: +IdeaTable+
+    #+idea_tags+ :: +IdeasTags+
+    #+tags+ :: +Tags+
   #Marwa Mehanna
-	def create
-    #puts params
-    #current_user = User.find(1)
+  def create
     @idea = Idea.new(params[:idea])
-    
-
-     @idea.user_id=current_user.id
-    respond_to do |format|
-      if @idea.save
-        @tags= params[:idea_tags][:tags]
-        @tags.each do |tag|
-          IdeasTag.create(:idea_id => @idea.id , :tag_id => tag)
-        end
-        format.html { redirect_to @idea, notice: 'idea was successfully created.' }
-        format.json { render json: @idea, status: :created, location: @idea }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
+    @idea.user_id=current_user.id
+      respond_to do |format|
+        if @idea.save
+          #fixme..>make sure that tags not nil
+          @tags= params[:ideas_tags][:tags]
+          @tags.each do |tag|
+            IdeasTags.create(:idea_id => @idea.id , :tag_id => tag)
+          end
+          format.html { redirect_to @idea, notice: 'idea was successfully created.' }
+          format.json { render json: @idea, status: :created, location: @idea }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @idea.errors, status: :unprocessable_entity }
       end
     end
   end
 end
-
