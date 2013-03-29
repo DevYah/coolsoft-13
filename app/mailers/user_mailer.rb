253f5d2dc41e299a2c7c9d63089f5c031bb1d27c
mennaamr <menna.amr2@gmail.com>
2013-03-29 17:19:38 +0200

    Added documentation to UserMailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,9 @@ class UserMailer < ActionMailer::Base
   default from: "idearator.13@gmail.com"
 
 #Sends email to user after Sign Up
+#Params:
+#user, passed through the ActionMailer
+#Author: Menna Amr
   def welcome_email(user)
     @user = user
     @url  = "http://localhost:3000/users/login"
