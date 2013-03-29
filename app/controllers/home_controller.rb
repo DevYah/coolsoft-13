e5e69e9423033156c0c62d2fc72c7699e0e9db55
Hesham Nabil <heshamnabil13@gmail.com>
2013-03-29 16:16:06 +0200

    #36 , added filter ideas according to user status and removed search/top ten

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,8 @@
 class HomeController < ApplicationController
 	#index method is the method used to display the idea stream, top ten and trending ideas.
 	#Author: Hesham Nabil
-	# Method gets all ideas, order them in descending order according to number of votes 
-	# and sends maximum ten ideas to index view page
-	# Author: Lina Basheer
 	def index
 		@approved = Idea.find(:all, :conditions => { :approved => true })
-		@top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
-        render :action => 'index.html.erb'
-	end
-		
-	def show
-		@ideas = Idea.all
-		@search = Idea.search do
-			fulltext params[:search]
-		end
-		@ideas = @search.results
-		if @ideas.size == 0
-			flash[:alert] = "NO match"
-		end
 	end
 	 
 end
