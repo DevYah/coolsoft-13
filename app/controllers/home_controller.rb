b590e6f1bb1ecfb5ae3e966f82ffbbb7dde4e9a9
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-03-29 11:43:06 +0200

    issu #38 , corrected code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < ApplicationController
 	# and sends top ten ideas to index view page
 	# Author: Lina Basheer 
 	def index
-@top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
-render :action => 'index.html.erb'
+      @top= Idea.find(:all,:order=> "num_votes desc",:limit=>10)
+      render :action => 'index.html.erb'
     end
 end
 
