bdd8d9c23c76f948352d60cfcf06592aed8713a4
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-01 16:39:17 +0200

    Revert "Merge pull request #54 from DevYah/c3_MahmoudHashish_44_Delete_Idea"
    This reverts commit 1a9c64d28a2405dc0ed5aa6fee35a882c4fca003, reversing
    changes made to b08ead5ffacf19fedac0019aa17754033ad080d9.
    
    This unmerges Mahmoud Hashish's pull request for Issue #44

--- Idearator/app/controllers/ideas_controller.rb
+++ Idearator/app/controllers/ideas_controller.rb
@@ -1,30 +1,4 @@
 class IdeasController < ApplicationController
-  	before_filter :authenticate_user!, :only => [:show, :create ,:edit]
-  
-	
-	
-	# Deletes the all records related to the +Idea+ instance from the database
-  	# Params:
-  	# +id+:: the id of the +Idea+ passed from the previous view, it helps in finding the +Idea+ instance from the database to process
-	# Author: Mahmoud Abdelghany Hashish
-	
-	def destroy
-		@idea = Idea.find(params[:id])
-		if current_user.id == @idea.user_id    
-			@idea.destroy
-    			
-    			respond_to do |format|
-    				format.html { redirect_to '/' }
-      				format.json { head :no_content }
-    			end
-		else
-			respond_to do |format|
-    				format.html { redirect_to @idea }
-      				format.json { notice 'idea cannot be deleted' }
-    			end
-		end
-  	end
-
   # GET /ideas
   # GET /ideas.json
   def index
@@ -36,12 +10,9 @@ class IdeasController < ApplicationController
     end
   end
 
-  # view idea of current user
-  # Params
-  # +id+:: is passed in params through the new idea view, it is used to identify the instance of +Idea+ to be viewed
-  # Marwa Mehanna
+  # GET /ideas/1
+  # GET /ideas/1.json
   def show
-    @user=current_user.id
     @idea = Idea.find(params[:id])
 
     respond_to do |format|
@@ -50,29 +21,38 @@ class IdeasController < ApplicationController
     end
   end
 
-  # making new Idea
-  #Marwa Mehanna
+  # GET /ideas/new
+  # GET /ideas/new.json
   def new
-    @idea=Idea.new
-    @tags= Tag.all
-    @chosentags=[]
+    @idea = Idea.new
+
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @idea }
     end
   end
 
-  # editing Idea
-  # Params
-  # +id+ :: this is an instance of +Idea+ passed through _form.html.erb, used to identify which +Idea+ to edit
-  # Author: Marwa Mehanna
+  # GET /ideas/1/edit
   def edit
     @idea = Idea.find(params[:id])
-    @tags= Tag.all
-    @chosentags= Idea.find(params[:id]).tags
   end
 
-  
+  # POST /ideas
+  # POST /ideas.json
+  def create
+    @idea = Idea.new(params[:idea])
+
+    respond_to do |format|
+      if @idea.save
+        format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
+        format.json { render json: @idea, status: :created, location: @idea }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   # PUT /ideas/1
   # PUT /ideas/1.json
   def update
@@ -88,26 +68,16 @@ class IdeasController < ApplicationController
       end
     end
   end
-  
-  # creating new Idea
-  # Params
-  # +idea+ :: this is an instance of +Idea+ passed through _form.html.erb, identifying the idea which will be added to records
-  # +idea_tags+ :: this is an instance of +IdeaTags+ passed through _form.html.erb, this is where +tags+ will be added
-  # +tags+ :: this is an instance of +Tags+ passed through _form.html.erb, used to identify which +Tags+ to add
-  # Author: Marwa Mehanna
-  def create
-    @idea = Idea.new(params[:idea])
-    @idea.user_id=current_user.id
+
+  # DELETE /ideas/1
+  # DELETE /ideas/1.json
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+
     respond_to do |format|
-      if @idea.save
-        @tags= params[:ideas_tags][:tags]
-        @tags.each do |tag|
-          IdeasTags.create(:idea_id => @idea.id , :tag_id => tag)
-        end
-        format.html { redirect_to @idea, notice: 'idea was successfully created.' }
-        format.json { render json: @idea, status: :created, location: @idea }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      format.html { redirect_to ideas_url }
+      format.json { head :no_content }
     end
+  end
 end
