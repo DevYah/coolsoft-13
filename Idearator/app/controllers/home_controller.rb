464fd35d58f6c786a4af737921392552fbc429c2
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-01 18:06:25 +0200

    Revert "Merge pull request #70 from DevYah/C4_salah_#45_searching-in-ideas"
    This reverts commit 148e78d4c424d3564018d9f9e6929cd8bf7ed22b, reversing
    changes made to 914871c42397225bbc7af4c3310a8111f64a4565.
    
    This unmerges Mohamed Salah's pull request for Issue #45

--- Idearator/app/controllers/home_controller.rb
+++ Idearator/app/controllers/home_controller.rb
@@ -1,22 +1,18 @@
 class HomeController < ApplicationController
-
+	# Method gets all ideas, order them in descending order according to number of votes 
+	# and sends top ten ideas to index view page
+	# Author: Lina Basheer 
+	def index
+      @top= Idea.find(:all, :conditions => { :approved => true },:order=> "num_votes desc",:limit=>10)
+      render :action => 'index.html.erb'
+    end
 	#Used to display the idea stream, top ten and trending ideas.
 	#Author: Hesham Nabil
-	# Method gets all ideas, order them in descending order according to number of votes 
-	# and sends maximum ten ideas to index view page
-	# Author: Lina Basheer
-	#in the index method if there is a search to be done then 
-	#it will return the list containing the search results.
-	#Author:Mohamed Salah Nazir
-	
 	def index
-		@user = current_user
-		@all = Idea.find(:all, :conditions => {:approved => true})
 		@approved = Idea.find(:all, :conditions => { :approved => true })
-		@top= Idea.find(:all, :conditions => { :approved => true },:order=> "num_votes desc",:limit=>10)
-        @search = Idea.search do
-			fulltext params[:search]
-		end
-		@approved = @search.results
-	end
+		@user = current_user
+        @top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
+        render :action => 'index.html.erb'
+    end
 end
+
