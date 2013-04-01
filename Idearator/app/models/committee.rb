7741cf35ac315adf67b07d648a9a25394dd115e7
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-01 16:52:14 +0200

    Revert "Merge pull request #71 from DevYah/C2_omar_25_review_ideas"
    This reverts commit 55876f56afd67e23acbf92bd2ac4b71990c3ce7a, reversing
    changes made to bb9ccaa59bd91557c27a08e14b90e8e7164ef125.
    
    This unmerges Omar Mahmoud's pull request for Issue #25

--- Idearator/app/models/committee.rb
+++ Idearator/app/models/committee.rb
@@ -1,6 +1,6 @@
 class Committee < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :password
+
   has_and_belongs_to_many :ideas
-  has_and_belongs_to_many :tags
+   has_and_belongs_to_many :tags
 end
--- Idearator/config/routes.rb
+++ Idearator/config/routes.rb
@@ -65,7 +65,6 @@ Sprint0::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-
   # match ':controller(/:action(/:id))(.:format)'
 
 
@@ -74,14 +73,6 @@ Sprint0::Application.routes.draw do
 
   match '/users/confirm_deactivate' => 'users#confirm_deactivate'
   match '/users/deactivate' => 'users#deactivate'
-
-
-
-
-  #2.3 Create/Edit Tags
-  resources :tags
-  match 'tags/:id/synonym' => 'tags#addsym', :via => :put
-  
 
 
 end
