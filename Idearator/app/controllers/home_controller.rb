8cd2f1b124389455f01dcc904131a5a0b7f49f89
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-02 13:12:54 +0200

    Revert "Merge pull request #69 from DevYah/C4_lina_#38_Topten"
    This reverts commit 914871c42397225bbc7af4c3310a8111f64a4565, reversing
    changes made to fe95239beeaa36ba92108f374bd625670e0b9c9d.
    
    This unmerges Lina Basheer's pull request for Issue #38

--- Idearator/app/controllers/home_controller.rb
+++ Idearator/app/controllers/home_controller.rb
@@ -1,11 +1,4 @@
 class HomeController < ApplicationController
-	# Method gets all ideas, order them in descending order according to number of votes 
-	# and sends top ten ideas to index view page
-	# Author: Lina Basheer 
-	def index
-      @top= Idea.find(:all, :conditions => { :approved => true },:order=> "num_votes desc",:limit=>10)
-      render :action => 'index.html.erb'
-    end
 	#Used to display the idea stream, top ten and trending ideas.
 	#Author: Hesham Nabil
 	def index
@@ -14,5 +7,6 @@ class HomeController < ApplicationController
         @top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
         render :action => 'index.html.erb'
     end
+
 end
 
