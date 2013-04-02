ec2c54e10b74229e5a43254d327ef1a80b691754
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-03 01:06:25 +0200

    Issue#44 Add again the destory method

--- Idearator/app/controllers/ideas_controller.rb
+++ Idearator/app/controllers/ideas_controller.rb
@@ -1,2 +1,23 @@
 class IdeasController < ApplicationController
+	# Deletes all records related to a specific idea
+	# Params:
+	# +id+:: is used to specify which instance of +Idea+ will be deleted
+	# Author: Mahmoud Abdelghany Hashish
+	def destroy
+		@idea = Idea.find(params[:id])
+
+		if current_user.id == @idea.user_id
+			@idea.destroy
+
+			respond_to do |format|
+      			format.html { redirect_to '/' }
+      			format.json { head :no_content }
+    		end
+    	else
+    		respond_to do |format|
+      			format.html { redirect_to @idea }
+      			format.json { head :no_content }
+    		end
+    	end
+    end
 end
