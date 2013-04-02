da543d5e66d115ff00842e99a434b9b350e6ada4
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-03 01:26:52 +0200

    Issue#90 Add archive/unarchive method

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,23 @@
 class IdeasController < ApplicationController
+	# Archives/Unarchives a specific idea
+	# Params:
+	# +id+:: is used to specify the instance of +Idea+ to be archived
+	# Author: Mahmoud Abdelghany Hashish
+	def archiveAndUnarchive
+		@idea = Idea.find(params[:id])
+
+		if current_user.isAdmin || current_user.id == @idea.user_id
+			@idea.archived = !@idea.archived
+    		
+    		respond_to do |format|
+      			format.html { redirect_to '/', alert: 'Idea has been successfully archived.' }
+      			format.json { head :no_content }
+    		end
+    	else
+    		respond_to do |format|
+      			format.html { redirect_to @idea, alert: "Idea isn't archived, you are not allowed to archive it." }
+      			format.json { head :no_content }
+    		end
+    	end
+    end 		
 end
