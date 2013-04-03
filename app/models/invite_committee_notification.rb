1b5679542f81aa213bfae86e21149d7dc7caa2a3
AminaZoheir <amina.zoheir@gmail.com>
2013-04-03 23:05:32 +0200

    Issue #73 Add send and text methods to InviteCommitteeNotification model

--- app/models/invite_committee_notification.rb
+++ app/models/invite_committee_notification.rb
@@ -1,2 +1,14 @@
 class InviteCommittee < UserNotification
+
+  def self.send_notification(user_sender, users_receivers) 
+    invite_notification = InviteCommitteeNotification.new
+    invite_notification.link = '/'
+    invite_notification.user = user_sender
+    invite_notification.users << users_receivers
+  end
+
+  def text
+    return "You have been invited to become a committee member."
+  end
+
 end
