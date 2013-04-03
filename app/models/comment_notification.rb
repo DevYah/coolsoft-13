f9530b4209920b5f470c7462894bfc0c72c4e068
AminaZoheir <amina.zoheir@gmail.com>
2013-04-03 22:41:08 +0200

    Issue #73 Add send and text methods to CommentNotification model

--- app/models/comment_notification.rb
+++ app/models/comment_notification.rb
@@ -1,9 +1,16 @@
 class CommentNotification < IdeaNotification
 
   def self.send_notification(user_sender, users_receivers) 
+    comment_notification = CommentNotification.new
+    comment_notification.link = 'idea#show'
+    comment_notification.user = user_sender
+    comment_notification.idea = idea
+    comment_notification.users << users_receivers
+    comment_notification.save
   end
 
   def text
+    User.find(self.user_id).first_name + " commented on your idea " + Idea.find(self.idea_id).title + "."
   end
 
 
