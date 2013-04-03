23086691b688486d88b20ae852579a68466d9f3b
AminaZoheir <amina.zoheir@gmail.com>
2013-04-03 23:08:54 +0200

    Issue #73 Add send and text methods to ApproveCommitteeNotification model

--- app/models/approve_committee_notification.rb
+++ app/models/approve_committee_notification.rb
@@ -1,2 +1,14 @@
 class ApproveCommittee < UserNotification
+
+  def self.send_notification(user_sender, users_receivers) 
+    approve_notification = ApproveCommitteeNotification.new
+    approve_notification.link = '/'
+    approve_notification.user = user_sender
+    approve_notification.users << users_receivers
+  end
+
+  def text
+    return User.find(self.user_id).first_name + " has signed up as a committee member."
+  end
+
 end
