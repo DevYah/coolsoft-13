440a091698ee59321acda450106da4f79bd55ac1
AminaZoheir <amina.zoheir@gmail.com>
2013-04-03 22:45:55 +0200

    Issue #73 Add send and text methods to DeleteNotification model

--- app/models/delete_notification.rb
+++ app/models/delete_notification.rb
@@ -1,9 +1,16 @@
 class DeleteNotification < IdeaNotification
 
   def self.send_notification(user_sender, users_receivers)
+    delete_notification = CommentNotification.new
+    delete_notification.link = '/'
+    delete_notification.user = user_sender
+    delete_notification.idea = idea
+    delete_notification.users << users_receivers
+    delete_notification.save
   end
 
   def text
+    User.find(self.user_id).first_name + " deleted his idea " + Idea.find(self.idea_id).title + "."
   end
 
 end
