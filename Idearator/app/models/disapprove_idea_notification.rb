dd8a1e328d5bc15e2810b86eb0ce25bef7558900
AminaZoheir <amina.zoheir@gmail.com>
2013-04-03 22:56:45 +0200

    Issue #73 Add send and text methods to DisapproveTdeaNotification model

--- Idearator/app/models/disapprove_idea_notification.rb
+++ Idearator/app/models/disapprove_idea_notification.rb
@@ -1,9 +1,15 @@
 class DisapproveIdeaNotification < IdeaNotification
 
   def self.send_notification(user_sender, users_receivers) 
+    disapprove_notification = CommentNotification.new
+    disapprove_notification.link = 'idea#show'
+    disapprove_notification.user = user_sender
+    disapprove_notification.idea = idea
+    disapprove_notification.users << users_receivers
   end
 
   def text
+    return "Your " + Idea.find(self.idea_id).title + " wasn't approved by the committee."
   end
 
 end
