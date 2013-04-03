8b0208f1a0802a1fd453e3a1f7d858d3e0fd3beb
AminaZoheir <amina.zoheir@gmail.com>
2013-04-03 22:46:20 +0200

    Issue #73 Add send and text methods to EditNotification model

--- Idearator/app/models/edit_notification.rb
+++ Idearator/app/models/edit_notification.rb
@@ -1,9 +1,16 @@
 class EditNotification < IdeaNotification
 
   def self.send_notification(user_sender, users_receivers) 
+    edit_notification = CommentNotification.new
+    edit_notification.link = 'idea#show'
+    edit_notification.user = user_sender
+    edit_notification.idea = idea
+    edit_notification.users << users_receivers
+    edit_notification.save
   end
 
   def text
+    User.find(self.user_id).first_name + " edited his idea " + Idea.find(self.idea_id).title + "."
   end
 
 end
