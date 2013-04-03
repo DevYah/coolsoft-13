27f91c11b2ea9a0ba0a1b9d062529984c7152ce3
AminaZoheir <amina.zoheir@gmail.com>
2013-04-03 22:40:04 +0200

    Issue #73 Add send and text methods to VoteNotification model

--- app/models/vote_notification.rb
+++ app/models/vote_notification.rb
@@ -1,9 +1,16 @@
 class VoteNotification < IdeaNotification
 
   def self.send_notification (user_sender, users_receivers) 
+    vote_notification = VoteNotification.new
+    vote_notification.link = 'idea#show'
+    vote_notification.user = user_sender
+    vote_notification.idea = idea
+    vote_notification.users << users_receivers
+    vote_notification.save
   end
 
   def text
+  	"Your idea " + Idea.find(self.idea_id).title + " got a vote."
   end
 
 end
