54d90436aff0ab0218a31f81dd499549713910c9
AminaZoheir <amina.zoheir@gmail.com>
2013-04-04 02:48:28 +0200

    Issue #73 Rename class UserNotificatin

--- app/models/user_notification.rb
+++ app/models/user_notification.rb
@@ -1,4 +1,4 @@
-class UserNotifications < ActiveRecord::Base
+class UserNotification < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :users
   attr_accessible :link, :type, :user, :users 
