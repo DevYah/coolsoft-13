fd4bdee46409166e41487e8ed437a72b9aac4853
muhammedabuali <muhammedabuali@gmail.com>
2013-04-05 00:36:44 +0200

    added json controller client and server

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,6 @@
 class TagsController < ApplicationController
+  def ajax
+    @tags = Tag.find(:all, conditions: ['name LIKE(?)', "%#{ params[:q] }%"]).take(5)
+    render :json => @tags , only: [:id, :name]
+  end 
 end
