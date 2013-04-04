79e3b6c09e547c734b2b29bc3959869738e87f29
Dayna161193 <daynahany@gmail.com>
2013-04-04 15:22:44 +0200

    Added Models

--- Idearator/app/models/comment.rb
+++ Idearator/app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-  # attr_accessible :title, :body
-
+  attr_accessible :content, :idea_id
+  validates :content, :presence=> true
   belongs_to :idea
   belongs_to :user
   has_and_belongs_to_many :users, :join_table => :likes
