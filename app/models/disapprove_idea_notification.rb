1185a8981dd077b4aeeb58d0a77be6e78cd4c17f
AminaZoheir <amina.zoheir@gmail.com>
2013-04-05 13:07:45 +0200

    Issue #73 Add link method to notification models

--- app/models/disapprove_idea_notification.rb
+++ app/models/disapprove_idea_notification.rb
@@ -2,11 +2,14 @@ class DisapproveIdeaNotification < IdeaNotification
 
   def self.send_notification(user_sender, idea, users_receivers) 
     disapprove_notification = DisapproveIdeaNotification.create(user: user_sender, idea: idea, users: users_receivers)
-    #link = 'idea#show', link_name = 'view idea'
   end
 
   def text
     return "Your " + Idea.find(self.idea_id).title + " wasn't approved by the committee."
+  end
+
+  def link
+    link_to "view idea", "idea#show"
   end
 
 end
