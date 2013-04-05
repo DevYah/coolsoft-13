c0d71a3038a082c66b8d715668b09adeaf72abcd
Hesham Nabil <heshamnabil13@gmail.com>
2013-04-05 18:19:30 +0200

    #88, added documentation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
 	#Used to display the idea stream, top ten and trending ideas.
+	#returns a list of ideas ordered by the date of creation in pages 
+	#of 10 ideas.
+	#Params:
+	#+page+:: the parameter is the page user is currently browsing.
 	#Author: Hesham Nabil
 	def index
 		@approved = Idea.order(:created_at).page(params[:page]).per(10)
