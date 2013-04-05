c5af409128eb620c5a0619911bd8cf7354e4d797
AminaZoheir <amina.zoheir@gmail.com>
2013-04-05 21:33:50 +0200

    Issue #73 Add view_notification action and it's view

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,10 +1,12 @@
 class NotificationsController < ApplicationController
+	before_filter :authenticate_user!, :only => [:view_notifications]
 
 	def view_notifications
 		@idea_notifications = current_user.idea_notifications
 		@user_notifications = current_user.user_notifications
-
-		puts current_user.user_notifications
+		@not = @idea_notifications + @user_notifications
+		@not2 = @not.sort_by &:created_at
+		@notifications = @not2.reverse
 	end
 
 end
