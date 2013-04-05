68b496b74189f6e862da28639c00b11688de5ff0
AminaZoheir <amina.zoheir@gmail.com>
2013-04-06 01:44:47 +0200

    Issue #73 Add view all notificattions action and view

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,12 +1,20 @@
 class NotificationsController < ApplicationController
-	before_filter :authenticate_user!, :only => [:view_notifications]
+  before_filter :authenticate_user!, :only => [:view_notifications]
 
-	def view_notifications
-		@idea_notifications = current_user.idea_notifications
-		@user_notifications = current_user.user_notifications
-		@not = @idea_notifications + @user_notifications
-		@not2 = @not.sort_by &:created_at
-		@notifications = @not2.reverse
-	end
+  def view_notifications
+    @idea_notifications = current_user.idea_notifications
+    @user_notifications = current_user.user_notifications
+    @not = @idea_notifications + @user_notifications
+    @not2 = @not.sort_by &:created_at
+    @notifications = @not2.reverse
+  end
+
+  def view_all_notifications
+    @idea_notifications = current_user.idea_notifications
+    @user_notifications = current_user.user_notifications
+    @not = @idea_notifications + @user_notifications
+    @not2 = @not.sort_by &:created_at
+    @notifications = @not2.reverse
+  end
 
 end
