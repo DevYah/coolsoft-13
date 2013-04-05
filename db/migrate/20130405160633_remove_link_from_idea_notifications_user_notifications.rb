76cb5289312d6a918aee87a24d1fd28bdd9f12e8
AminaZoheir <amina.zoheir@gmail.com>
2013-04-05 18:35:17 +0200

    Issue #73 Add comment to last migration

--- db/migrate/20130405160633_remove_link_from_idea_notifications_user_notifications.rb
+++ db/migrate/20130405160633_remove_link_from_idea_notifications_user_notifications.rb
@@ -1,4 +1,6 @@
 class RemoveLinkFromIdeaNotificationsUserNotifications < ActiveRecord::Migration
+  #This migration file removes the columns link and link_name from table idea_notifications and user_notifications 
+  #as the link part is going to be handled in the view so it is not supposed to be in the database.
   def up
   	remove_column :user_notifications, :link_name
   	remove_column :user_notifications, :link
