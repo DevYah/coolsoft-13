b1159041f6c87c9238d44b93b15e3a6f84df31cb
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-04-06 13:50:38 +0200

    Issue #38 ,Added Bootstrap on top ten

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,14 @@
 class HomeController < ApplicationController
 	#Used to display the idea stream, top ten and trending ideas.
 	#Author: Hesham Nabil
+	#Method gets all ideas, order them in descending order according to number of votes 
+	#and sends top ten ideas to index view page
+	#Author: Lina Basheer 
 	def index
-		@approved = Idea.find(:all, :conditions => { :approved => true })
+		@approved = Idea.find (:all, :conditions => { :approved => true })
 		@user = current_user
-        @top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
-        render :action => 'index.html.erb'
-    end
-
+    @top = Idea.find(:all, :conditions => { :approved => true }, :order=> 'num_votes desc', :limit=>10)
+    render :action => 'index.html.erb'
+  end
 end
 
