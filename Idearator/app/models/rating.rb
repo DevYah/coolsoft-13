f5cfb19fb941c3a66f5ce3b82877bdb8465f03ff
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-07 01:14:19 +0200

    Add average rating method to model Rating

--- Idearator/app/models/rating.rb
+++ Idearator/app/models/rating.rb
@@ -3,4 +3,13 @@ class Rating < ActiveRecord::Base
   belongs_to :idea
   has_many :users, :through => :user_rating
   has_many :user_ratings
+
+  def average_rating
+    @value = 0
+    self.user_ratings.each do |user_rating|
+      @value = @value + user_rating.value
+    end
+    @total = self.user_ratings.size
+    @value.to_f / @total.to_f
+  end
 end
