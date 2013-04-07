ff4cd27704ec7776f09fb26a7ea9d990bd66bd9a
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-07 13:10:03 +0200

    Issue#44 Add ideas_controller_spec.rb

---
+++ Idearator/spec/controllers/ideas_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe IdeasController do
+  describe 'DELETE destroy' do
+    context 'idea creator wants to delete' do
+      before :each do
+        @user = Factory.build(:user_one)
+        @user.confirm!
+        @idea = Factory.build(:idea_one)
+      end
+
+      it 'deletes the contact' do
+        expect { delete :destroy, id: @idea }.to change(Idea, :count).by(-1)
+      end
+
+      it 'redirects to home' do
+        delete :destroy, id: @idea
+          response.should redirect_to '/'
+      end
+    end
+
+    context 'normal user wants to delete idea' do
+      before :each do
+        @userone = Factory.build(:user_one)
+        @userone.confrim!
+        @usertwo = Factory.build(:user_two)
+        @usertwo.confirm!
+        @idea = Factory.build(:idea)
+      end
+
+      it 'deletes the contact' do
+        expect { delete :destroy, id: @idea }.to change(Idea, :count).by(0)
+      end
+
+      it 'redirects to idea' do
+        delete :destroy, id: @idea
+        response.should redirect_to @idea
+      end
+    end
+  end
+end
