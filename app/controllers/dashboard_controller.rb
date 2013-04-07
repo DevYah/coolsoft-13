7e38250b66229d95c268baa868f4e0518e506a67
Mohamed Salah Nazir <mohamedsalahnazir93@gmail.com>
2013-04-07 15:44:28 +0200

    Issue #77, adds dashboard controller and views

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,19 @@
+class DashboardController < ApplicationController
+  def index
+    @tagid = params[:tagid]
+    @ideastagsall = IdeasTags.find(:all, :conditions => {:tag_id => params[:tagid]})
+    @ideasall = Idea.where(:id => @ideastagsall.map(&:idea_id))
+    ###############
+    @ideaid = params[:ideaid]
+    @ideatagsthen = IdeasTags.find(:all, :conditions => {:idea_id => @ideaid})
+    @ideatags = Tag.where(:id => @ideatagsthen.map(&:tag_id))
+    #########
+    @user = User.find(3)
+    @ideas = Idea.find(:all, :conditions => {:user_id => @user.id})
+    #########
+    respond_to do |format|
+    format.html
+    format.js
+    end
+  end
+end
