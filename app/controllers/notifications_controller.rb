2c44cf99c7229e9f9d1bddf1c1f7994eb0ff97eb
AminaZoheir <amina.zoheir@gmail.com>
2013-04-07 14:02:39 +0200

    Issue #73 Add load_notifications to application controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,20 +1,14 @@
 class NotificationsController < ApplicationController
   before_filter :authenticate_user!, :only => [:view_notifications]
 
-  def view_notifications
-    @idea_notifications = current_user.idea_notifications
-    @user_notifications = current_user.user_notifications
-    @not = @idea_notifications + @user_notifications
-    @not2 = @not.sort_by &:created_at
-    @notifications = @not2.reverse.first(10)
-  end
-
   def view_all_notifications
-    @idea_notifications = current_user.idea_notifications
-    @user_notifications = current_user.user_notifications
-    @not = @idea_notifications + @user_notifications
-    @not2 = @not.sort_by &:created_at
-    @notifications = @not2.reverse
+    if(user_signed_in?)
+      idea_notifications = current_user.idea_notifications
+      user_notifications = current_user.user_notifications
+      not1 = idea_notifications + user_notifications
+      not2 = not1.sort_by &:created_at
+      @all_notifications = not2.reverse
+    end
   end
 
 end
