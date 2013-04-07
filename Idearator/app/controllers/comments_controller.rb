5578f4ef9b2a5a20e826bb61aeca0777cd915534
Dayna161193 <daynahany@gmail.com>
2013-04-07 15:16:11 +0200

    Add controller

--- Idearator/app/controllers/comments_controller.rb
+++ Idearator/app/controllers/comments_controller.rb
@@ -1,8 +1,10 @@
 class CommentsController < ApplicationController
-#Show all Comments 
+#Show all Comments
 #Params:
-#+idea_id+ :: the parameter is an instance of +Idea+ passed through the form of action create
+#+idea_id+ :: the parameter is an instance of +Idea+
+#passed through the form of action create
 #author dayna
+
 def show
    @idea = Idea.find(params[:id])
    respond_to do |format|
@@ -10,34 +12,40 @@ def show
      format.xml  { render :xml => @comment }
    end
 end
-#create new Comment 
+#create new Comment
 #Params:
-#+idea_id+ :: the parameter is an instance of +Idea+ passed to get the id of the idea to build the comments 
-#+comment_id+ :: the parameter is an instance of +Comment+ and it's used to show the comments after posting it 
-#author dayna 
+#+idea_id+ :: the parameter is an instance
+#of +Idea+ passed to get the id of the idea to build the comments
+#+comment_id+ :: the parameter is an instance
+# of +Comment+ and it's used to show the comments after posting it
+#author dayna
+
 def create
    @idea = Idea.find(params[:idea_id])
     @comment = @idea.comments.create(params[:comment])
     @comment.update_attributes(:idea_id => @idea.id)
     respond_to do |format|
       if @comment.save
-       format.html { redirect_to(@idea, :notice => 'Comment was successfully created.') }
-       format.xml  { render :xml => @idea, :status => :created, :location => @idea }
+       format.html redirect_to(@idea, :notice => 'Comment was successfully created.')
+       format.xml  render :xml => @idea, :status => :created, :location => @idea
       else
-       format.html { redirect_to(@idea, :notice => 
+       format.html  redirect_to(@idea, :notice =>
         'Comment could not be saved. Please fill in all fields')}
-        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+        format.xml  render :xml => @comment.errors, :status => :unprocessable_entity
       end
     end
 end
 #edit comment and update it
 #Params:
-#+comment_id+ :: the parameter is an instance of +Comment+ to get the comment's id in order to modify it  
+#+comment_id+ :: the parameter is an instance of +Comment+
+#to get the comment's id in order to modify it
 #author dayna
+
 def edit
     @comment = Comment.find(params[:id])
     @idea = Idea.find(params[:idea_id])
 end
+
 def update
     @comment = Comment.find(params[:id])
     @idea = Idea.find(params[:idea_id])
@@ -46,30 +54,34 @@ def update
         format.html { redirect_to(@idea, :notice => 'Comment was successfully updated.') }
         format.xml  { head :no_content }
       else
-        format.html { render :action => "edit" }
+        format.html { render :action => 'edit' }
         format.xml  { render :xml => @idea.errors, :status => :unprocessable_entity }
       end
     end
   end
 #delete comment
 #Params:
-#+comment_id+ :: the parameter is an instance of +Comment+ to get the comment's id in order to delete it
-#+idea_id+ :: the parameter is an instance of +Idea+ to get the idea's id in order to modify it after deleting the comment
+#+comment_id+ :: the parameter is an instance
+#of +Comment+ to get the comment's id in order to delete it
+#+idea_id+ :: the parameter is an instance of
+# +Idea+ to get the idea's id in order to modify it after deleting the comment
 #author dayna
+
 def destroy
     @idea = Idea.find(params[:idea_id])
     @comment = @idea.comments.find(params[:id])
     @comment.destroy
     redirect_to idea_path(@idea)
 end
-/def like 
+/def like
  # @current_user =  User.find(params[:id])
   @current_user =  User.first
   @idea = Idea.find(params[:idea_id])
   @comment = @idea.comments.find(params[:id])
   @commentsLiked = Like.find(params[:user_id]).comment_id
-  #if Like.find(params[:comment_id]) == @comment && Like.find(params[:user_id]) == @current_user
-#else 
+  #if Like.find(params[:comment_id]) == @comment
+  && Like.find(params[:user_id]) == @current_user
+#else
   if @commentsLiked =! @comment
   @comment.num_likes+=1
   # @comment.num_likes + 1
@@ -78,15 +90,15 @@ end
    #    format.html { redirect_to(@idea, :notice => 'like was successfully created.') }
     #   format.xml  { render :xml => @idea, :status => :created, :location => @idea }
      # else
-      # format.html { redirect_to(@idea, :notice => 
+      # format.html { redirect_to(@idea, :notice =>
        # 'Error')}
        # format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
     #end
    redirect_to idea_path , :notice => 'like was successfully created.'
-  else 
+  else
      redirect_to idea_path , :notice => 'You already liked that before .'
 
-  end 
+  end
   end/
 
   /def like
@@ -98,29 +110,30 @@ end
        redirect_to idea_path , :notice => 'like was successfully created.'
 
   end/
+
       def like
   #@current_user =  User.find(params[:id])
   @current_user =  User.first
   #@idea = Idea.find(params[:idea_id])
   @comment = Comment.find(params[:id])
-  #if Like.find(params[:comment_id]) == @comment && Like.find(params[:user_id]) == @current_user
-#else 
-  @comment.num_likes+=1
+  #if Like.find(params[:comment_id]) == @comment
+  # && Like.find(params[:user_id]) == @current_user
+#else
+  @comment.num_likes += 1
   # @comment.num_likes + 1
   @like = @comment.num_likes
   #if @like.save
    #    format.html { redirect_to(@idea, :notice => 'like was successfully created.') }
     #   format.xml  { render :xml => @idea, :status => :created, :location => @idea }
      # else
-      # format.html { redirect_to(@idea, :notice => 
+      # format.html { redirect_to(@idea, :notice =>
        # 'Error')}
        # format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
    # end
    redirect_to idea_path , :notice => 'like was successfully created.'
-  end  
-end 
- 
- 
+  end
+end
+
 def unlike
 @like = Like.find(params[:user_id][:comment_id])
     #@story = @like.story
@@ -131,5 +144,5 @@ def unlike
       format.js
       format.json { head :ok }
     end
-end 
+end
 
