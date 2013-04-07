005294d24777c568375992c3702e5f18c77ab4e4
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-07 02:00:57 +0200

    Issue#89 Add rating_ballot method documentation

--- app/helpers/user_ratings_helper.rb
+++ app/helpers/user_ratings_helper.rb
@@ -1,4 +1,8 @@
 module UserRatingsHelper
+
+  # passes either newely created or the existing user rating ballot to the user_ratings/_form.html.erb
+  # Params:
+  # +rating_id+:: is the id of the instance of +Rating+, i.e Rating Perspective, which is being rated
   def rating_ballot
     if @user_rating = current_user.user_ratings.find_by_rating_id(params[:rating_id])
         @User_rating
