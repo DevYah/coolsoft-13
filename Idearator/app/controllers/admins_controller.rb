4778a58e1d235f4ec90f24e283f280f938baf8d3
mohammad-abdulkhaliq <mohammad28march1993@gmail.com>
2013-04-07 23:36:35 +0200

    Issue #96 added controller actions and views

--- Idearator/app/controllers/admins_controller.rb
+++ Idearator/app/controllers/admins_controller.rb
@@ -1,5 +1,38 @@
 class AdminsController < ApplicationController
   def show_users
-    @users = User.all
+      @users = User.all
+      respond_to do |format|
+        format.html  # index.html.erb
+        format.json  { render :json => @posts }
+    end
   end
+  def approve_committeee
+    @user = User.find(params[:id])
+    @user.type = 'Committee'
+    respond_to do |format|
+       if @user.save
+         format.html  { redirect_to(admins_path,
+                       :notice => 'User successfully initiated as a Committee.') }
+         format.json  { head :no_content }
+       else
+         format.html  { redirect_to(admins_path,
+                       :notice => @user.errors.full_messages) }
+         format.json  { render :json => :no_content }
+       end
+     end
+   end
+   def reject_committee
+     @user = User.find(params[:id])
+     respond_to do |format|
+        if @user.save
+          format.html  { redirect_to(admins_path,
+                        :notice => 'User successfully initiated as a Committee.') }
+          format.json  { head :no_content }
+        else
+          format.html  { redirect_to(admins_path,
+                        :notice => @user.errors.full_messages) }
+          format.json  { render :json => :no_content }
+        end
+      end
+    end
 end
--- Idearator/app/controllers/users_controller.rb
+++ Idearator/app/controllers/users_controller.rb
@@ -2,16 +2,14 @@
 
 class UsersController < ApplicationController
 	before_filter :authenticate_user!, :only => [:deactivate, :confirm_deactivate, :activate, :expertise, :new_committee_tag]
-
-
+  
 	# displays a form where the user enters his password to confrim deactivation.
 	# Params: none
 	# Author: Amina Zoheir
 	def confirm_deactivate
 		@user = current_user
 	end
-
-
+  
 	# checks the entered password if it's the current users password 
 	# it changes the value of his active field to false and signs him out. 
 	# Params: 
@@ -22,8 +20,7 @@ class UsersController < ApplicationController
       @password = params[:committee][:password]
     else
       @password = params[:user][:password]
-    end
-    
+    end  
     if current_user.valid_password?(@password)
       current_user.active = false
       current_user.save
@@ -41,8 +38,7 @@ class UsersController < ApplicationController
       end
     end
   end
-
-
+  
   # sets the active field of the current user to true
   # Params: none
   # Author: Amina Zoheir
@@ -53,8 +49,7 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
-
-
+  
 	# Pass the current_user and all the tags to the  expertise view
 	# Params:
 	# none
@@ -79,6 +74,7 @@ class UsersController < ApplicationController
 			end
 		end
 	end
+  
 	# Enter chosen tags sent from expertise view, in committeestags table 
 	# Params:
 	# +tags[]+:: the parameter is ana instance of +tag+ passed through the form from expertise action
@@ -103,23 +99,22 @@ class UsersController < ApplicationController
 			end
 		end
 	end
-
+  
   #This method is used to generate the view of each User Profile. A specific user and his ideas are made
   #available to the view to be presented in the appropriate manner.
   #Author: Hisham ElGezeery
   def show
     @user = User.find(params[:id])
     @ideas = Idea.find(:all, :conditions => { :user_id => @user.id })
-
+    @invited = ApproveCommitteeNotification.where(:user_id => params[:id]).exists?
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
   	end
   end 
-
+  
 	# POST /users
   # POST /users.json
-	
   # This method creates a new User and calls UserMailer to send a confirmation email.
   #Author: Menna Amr
   def create
@@ -129,7 +124,6 @@ class UsersController < ApplicationController
       if @user.save
         # Tell the UserMailer to send a welcome Email after save
         UserMailer.welcome_email(@user).deliver
- 
         format.html { redirect_to(@user, :notice => 'User was successfully created.') }
         format.json { render :json => @user, :status => :created, :location => @user }
       else
