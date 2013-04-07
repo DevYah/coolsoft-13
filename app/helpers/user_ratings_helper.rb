62d2c53a73a7b9223154c1a5d1d245e51c45668e
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-07 02:03:21 +0200

    Issue#89 Add current_user_rating method documentation

--- app/helpers/user_ratings_helper.rb
+++ app/helpers/user_ratings_helper.rb
@@ -11,6 +11,9 @@ module UserRatingsHelper
     end
   end
 
+  # passes the existing user rating value, else string 'N/A', of a certain perspective to the user_ratings/_form.html.erb
+  # Params:
+  # +rating_id+:: is the id of the instance of +Rating+, i.e Rating Perspective, which is being rated
   def current_user_rating
     current_user = User.find(1)
     if @user_rating = current_user.user_ratings.find_by_rating_id(params[:rating_id])
