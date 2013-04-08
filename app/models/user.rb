3dec4e972190993e4468abe7192a436313a9fb08
Dayna161193 <daynahany@gmail.com>
2013-04-08 18:39:24 +0200

    Add the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
   has_many :user_ratings
   has_and_belongs_to_many :idea_notifications
   has_and_belongs_to_many :user_notifications
-  has_and_belongs_to_many :likes, :class_name => 'Comment', :join_table => :votes
+  has_and_belongs_to_many :likes, :class_name => 'Comment', :join_table => :likes
   has_and_belongs_to_many :votes, :class_name => 'Idea', :join_table => :votes
 
 end
