02df39580c6fe9b63580592098543fccf5575ba0
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-08 12:41:34 +0200

    Generalize the omniauth callback.
    Issue #74
    The callback is going to be very similar for most providers anyway

--- Idearator/app/controllers/users/omniauth_callbacks_controller.rb
+++ Idearator/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,31 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+  def default_oauth_callback(provider, &block)
+    auth = request.env['omniauth.auth']
 
-    if @user.persisted?
-      print ">>>>>>>", @user 
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      print ">>>>>>> new", request.env["omniauth.auth"]
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+    @user = User.send("find_for_#{provider.to_s}_oauth", auth, current_user)
+    unless @user && @user.persisted?
+      # save oauth data if user creation failed
+      session["devise.#{provider.to_s}_data"] = auth
     end
+
+    if block
+      yield auth
+    else
+      if @user && @user.persisted?
+        @user.confirm!
+        sign_in_and_redirect @user, :event => :authentication # this will throw if @user is not activated
+        set_flash_message(:notice, :success, :kind => provider.to_s.capitalize) if is_navigational_format?
+      else
+        redirect_to auth.redirect if auth.redirect
+      end
+    end
+  end
+
+  def facebook
+    default_oauth_callback(:facebook)
+  end
+
+  def twitter
+    default_oauth_callback(:twitter)
   end
 end
