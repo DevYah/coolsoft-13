b636e6433faec0e8aca3e841dc483740f597c5d8
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-04-08 18:33:52 +0200

    Issue #92,resolved conflicts

--- app/controllers/home_controller_spec.rb
+++ app/controllers/home_controller_spec.rb
@@ -1,12 +1,11 @@
-describe "GEt #index" do 
-  it "populates an array of Top ten ideas"
-  top =Factory(:top)
+describe 'GEt #index' do
+  it 'populates an array of Top ten ideas'
+  top = Factory(:top)
   get :index
   assigns(:top).should eq([top])
   end
-  it "renders the :index view" do
+  it 'renders the :index view' do
     get :index
     response.should render_template :index
   end
 end
-    
