5cdef37839e6985a7b298c24602db9d57740fcf4
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-09 00:24:36 +0200

    Add twitter login logic.
    Issue #74
    User can login via twitter. Username clashes are handled.

--- Idearator/app/controllers/registrations_controller.rb
+++ Idearator/app/controllers/registrations_controller.rb
@@ -1,9 +1,37 @@
 class RegistrationsController < Devise::RegistrationsController
-  protected
-   
-#Redirecting to sign in page after sign up
+  def twitter_screen_name_clash
+    auth = session['devise.twitter_data']
+    raise ActiveResource::UnauthorizedAccess.new('Unauthorized') unless auth
 
-       def after_sign_up_path_for(resource)
-      "users/sign_in"
-    end        
+    if params[:username]
+      auth.chosen_user_name = params[:username]
+      render and return if User.exists? username: params[:username]
+      user = User.create_user_from_twitter_oauth(auth)
+      if not user
+        user.confirm!
+        sign_in_and_redirect @user, :event => :authentication
+        set_flash_message(:notice, :success, :kind => provider.to_s.capitalize) if is_navigational_format?
+      else
+        flash[:alert] = user.errors.to_a[0]
+        redirect_to controller: 'twitter_screen_name_clash'
+      end
+    end
+  end
+
+  protected
+  def after_sign_up_path_for(resource)
+    if resource.type.is_a? Committee
+      '/users/expertise'
+    else
+      '/'
+    end
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    if resource.type.is_a? Committee
+      '/users/expertise'
+    else
+      '/'
+    end
+  end
 end
--- Idearator/app/models/user.rb
+++ Idearator/app/models/user.rb
@@ -9,9 +9,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook, :twitter]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :date_of_birth, :type, :active , :first_name , :last_name ,
-  :username , :date_of_birth , :gender , :about_me , :recieve_vote_notification ,
-  :recieve_comment_notification
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :username, :date_of_birth, :type, :active, :first_name,
+                  :last_name, :gender, :about_me, :recieve_vote_notification,
+                  :recieve_comment_notification, :provider, :uid
 
   has_many :idea_notifications
   has_many :user_notifications
@@ -26,10 +27,32 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :likes, :class_name => 'Comment', :join_table => :votes
   has_and_belongs_to_many :votes, :class_name => 'Idea', :join_table => :votes
 
-def self.find_for_facebook_oauth(auth, signed_in_resource = nil)
-   user = User.where(email: auth['info']['email']).first
+  def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    unless user
+      # username may already be taken, user will have to enter another one
+      if not User.where(username: auth.extra.raw_info.screen_name).empty?
+        # FIXME handle failure
+        user = create_user_from_twitter_oauth(auth)
+      else
+        auth.redirect = { controller: '/registrations', action: 'twitter_screen_name_clash' }
+      end
+    end
+    user
+  end
 
-   user
- end
+  def self.create_user_from_twitter_oauth(auth)
+    name = auth.info.name.split(' ', 2)
+    user = User.create(first_name: name[0],
+                       last_name: name[1],
+                       provider: auth.provider,
+                       uid: auth.uid,
+                       # this is an invalid email, but uniqueness is guaranteed
+                       email: "#{auth.extra.raw_info.screen_name}@twitter.com",
+                       username: (auth.chosen_user_name or auth.extra.raw_info.screen_name),
+                       # random password, won't hurt
+                       password: Devise.friendly_token[0, 20])
+  end
+
 end
 
