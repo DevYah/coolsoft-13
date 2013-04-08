cf2080b256b3f4a72f24824c056e21a5cbdd1f0e
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-04-08 04:37:29 +0200

    Issue #80, Added threshold method
    The method updates the threshold every day at 12 AM. This is done
    using the gem called "whenever".

--- app/models/threshold.rb
+++ app/models/threshold.rb
@@ -1,3 +1,12 @@
 class Threshold < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :threshold
+
+  def self.threshold_run
+    Threshold.create(:threshold => VoteCount.maximum('curr_day_votes'))
+    @vote_counts = VoteCount.all
+    VoteCount.all.each do |v|
+      v.prev_day_votes = v.curr_day_votes
+      v.curr_day_votes = 0
+    end
+  end
 end
