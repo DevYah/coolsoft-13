ce1d2e7d9f9e9f11969ffc3983423d18d6760497
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-04-08 22:38:14 +0200

    Issue #80, List ideas for Idea Submitter/Committee in dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,9 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!, :only => [:index]
   def index
     @user = current_user
+    if @user.type == 'Committee'
+      @approved_ideas = Idea.where(:committee_id => @user.id)
+    end
+    @own_ideas = Idea.where(:user_id => @user.id, :approved => true)
   end
 end
