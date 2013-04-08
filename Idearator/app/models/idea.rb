89af1ef4d1fdcdb0cbe6ef71fc8bd7a03844a297
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-08 19:41:27 +0200

    Fix Idea relation with Committee

--- Idearator/app/models/idea.rb
+++ Idearator/app/models/idea.rb
@@ -5,7 +5,7 @@ class Idea < ActiveRecord::Base
   has_many :comments
   has_many :idea_notifications
   has_many :ratings
-  has_and_belongs_to_many :committees
+  belongs_to :committee
   has_and_belongs_to_many :tags
   has_and_belongs_to_many :votes, :class_name => 'User', :join_table => :votes
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
