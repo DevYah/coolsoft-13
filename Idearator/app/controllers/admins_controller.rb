c61e1fdbd49e288d07dc0dffe284195da7bd4d81
Omar Kassem <omar.kassem67@gmail.com>
2013-04-09 01:43:00 +0200

    #94, added documentation to ban_unban method

--- Idearator/app/controllers/admins_controller.rb
+++ Idearator/app/controllers/admins_controller.rb
@@ -1,4 +1,6 @@
 class AdminsController < ApplicationController
+# toggles the ban status of the selected user
+# Author: Omar Kassem
   def ban_unban
     @user=User.find(params[:id])
     @user.toggle(:banned)
