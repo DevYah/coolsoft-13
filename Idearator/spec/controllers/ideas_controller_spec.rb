0542c57b74e32f3b046ccac053ac4bb20c636d29
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-08 19:36:47 +0200

    Issue#44 Add issue tests

--- Idearator/spec/controllers/ideas_controller_spec.rb
+++ Idearator/spec/controllers/ideas_controller_spec.rb
@@ -2,38 +2,46 @@ require 'spec_helper'
 
 describe IdeasController do
   describe 'DELETE destroy' do
+    include Devise::TestHelpers
+    
     context 'idea creator wants to delete' do
       before :each do
-        @user = Factory.build(:user_one)
+        @user = FactoryGirl.build(:user)
         @user.confirm!
-        @idea = Factory.build(:idea_one)
+        @idea = FactoryGirl.create(:idea)
+        @idea.user_id = @user.id
+        @idea.save
+        sign_in @user
       end
 
       it 'deletes the idea' do
-        expect { delete :destroy, id: @idea }.to change(Idea, :count).by(-1)
+        expect { delete :destroy, :id => @idea.id }.to change(Idea, :count).by(-1)
       end
 
       it 'redirects to home' do
-        delete :destroy, id: @idea
+        delete :destroy, :id => @idea.id
           response.should redirect_to '/'
       end
     end
 
     context 'normal user wants to delete idea' do
       before :each do
-        @userone = Factory.build(:user_one)
-        @userone.confrim!
-        @usertwo = Factory.build(:user_two)
+        @userone = FactoryGirl.build(:user)
+        @userone.confirm!
+        @usertwo = FactoryGirl.build(:user_two)
         @usertwo.confirm!
-        @idea = Factory.build(:idea)
+        @idea = FactoryGirl.create(:idea)
+        @idea.user_id = @userone.id
+        @idea.save
+        sign_in @usertwo
       end
 
-      it 'deletes the idea' do
-        expect { delete :destroy, id: @idea }.to change(Idea, :count).by(0)
+      it 'does not delete the idea' do
+        expect { delete :destroy, :id => @idea.id }.to change(Idea, :count).by(0)
       end
 
       it 'redirects to idea' do
-        delete :destroy, id: @idea
+        delete :destroy, :id => @idea.id
         response.should redirect_to @idea
       end
     end
