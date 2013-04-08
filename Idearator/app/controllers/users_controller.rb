9854c63e46bf2df550c16054ae321ed9caf75d9e
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-04-09 01:48:53 +0200

    Issue #52, Fix guest user profile view
    This commit fixed the guest view of the user profile. The idea
    stream is now implemented, and a new button has been added to allow
    the user to be redirected to the new idea path.

--- Idearator/app/controllers/users_controller.rb
+++ Idearator/app/controllers/users_controller.rb
@@ -109,7 +109,7 @@ class UsersController < ApplicationController
   #Author: Hisham ElGezeery
   def show
     @user = User.find(params[:id])
-    @ideas = Idea.find(:all, :conditions => { :user_id => @user.id })
+    @approved = Idea.find(:all, :conditions => { :user_id => @user.id })
 
     respond_to do |format|
       format.html # show.html.erb
