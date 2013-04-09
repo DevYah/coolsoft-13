96369326e376f4fefe186cfad541376a5ef139a7
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-09 04:39:39 +0200

    Issue#89 Fix attribute namings and small bugs in controller methods

--- Idearator/app/controllers/user_ratings_controller.rb
+++ Idearator/app/controllers/user_ratings_controller.rb
@@ -9,16 +9,17 @@ class UserRatingsController < ApplicationController
   # Author: Mahmoud Abdelghany Hashish
   def create
     @idea = Idea.find_by_id(params[:idea_id])
-    @user_rating = UserRating.new(params[:user_rating])
-    @user_rating.rating_id = params[:rating_id].id
+    @user_rating = UserRating.new(params[:rating])
+    @user_rating.rating_id = params[:rating_id]
     @user_rating.user_id = current_user.id
     if @user_rating.save
       respond_to do |format|
-        format.html { redirect_to idea_path(@idea), :notice => 'Your rating has been saved' }
+        format.html { redirect_to @idea, :notice => 'Your rating has been saved' }
         format.js
       end
+    else
       respond_to do |format|
-        format.html { redirect_to idea_path(@idea), :notice => 'Your rating has not been saved, please retry!' }
+        format.html { redirect_to @idea, :notice => 'Your rating has not been saved, please retry!' }
         format.js
       end
     end
@@ -31,12 +32,13 @@ class UserRatingsController < ApplicationController
   # Author: Mahmoud Abdelghany Hashish
   def update
     @idea = Idea.find_by_id(params[:idea_id])
-    @user_rating = current_user.user_ratings.find_by_rating_id(@rating.id)
-    if @user_rating.update_attributes(params[:user_rating])
+    @user_rating = current_user.user_ratings.find_by_rating_id(params[:rating_id])
+    if @user_rating.update_attributes(params[:rating])
       respond_to do |format|
         format.html { redirect_to idea_path(@idea), :notice => 'Your rating has been updated' }
         format.js
       end
+    else 
       respond_to do |format|
         format.html { redirect_to idea_path(@idea), :notice => 'Your rating has not been updated, please retry!' }
         format.js
