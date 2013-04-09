89b383f89d4543e83ad6fcdf464acf29ab4d686a
AminaZoheir <amina.zoheir@gmail.com>
2013-04-09 15:27:03 +0200

    Issue #73 Make notifications read when clicked in drop down

--- Idearator/app/controllers/notifications_controller.rb
+++ Idearator/app/controllers/notifications_controller.rb
@@ -14,4 +14,34 @@ class NotificationsController < ApplicationController
     end
   end
 
+  def redirect_idea
+    idea_not = IdeaNotificationsUser.find(:first, :conditions => {idea_notification_id: params[:not_id], user_id: current_user.id })
+    idea_not.read = true
+    idea_not.save
+    respond_to do |format|
+        format.html { redirect_to controller: 'ideas', action: 'show' }
+        format.json { head :no_content }
+    end
+  end
+
+  def redirect_expertise
+    user_not = UserNotificationsUser.find(:first, :conditions => {user_notification_id: params[:not_id], user_id: current_user.id })
+    user_not.read = true
+    user_not.save
+    respond_to do |format|
+        format.html { redirect_to controller: 'users', action: 'expertise' }
+        format.json { head :no_content }
+    end
+  end
+
+  def redirect_review
+    user_not = UserNotificationsUser.find(:first, :conditions => {user_notification_id: params[:not_id], user_id: current_user.id })
+    user_not.read = true
+    user_not.save
+    respond_to do |format|
+        format.html { redirect_to controller: 'committees', action: 'review' }
+        format.json { head :no_content }
+    end
+  end
+
 end
