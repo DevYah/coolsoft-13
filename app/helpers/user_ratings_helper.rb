3b90e4ab785f8a124091ee35418ad12e3b2947c5
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-09 04:38:48 +0200

    Issue#89 Fix helper methods

--- app/helpers/user_ratings_helper.rb
+++ app/helpers/user_ratings_helper.rb
@@ -4,9 +4,9 @@ module UserRatingsHelper
   # Params:
   # +rating_id+:: is the id of the instance of +Rating+, i.e Rating Perspective, which is being rated
   # Authour: Mahmoud Abdelghany Hashish
-  def rating_ballot
-    if @user_rating = current_user.user_ratings.find_by_rating_id(params[:rating_id])
-        @User_rating
+  def rating_ballot(rating_id)
+    if @user_rating = current_user.user_ratings.find_by_rating_id(rating_id)
+        @user_rating
     else
         current_user.user_ratings.new
     end
@@ -16,8 +16,8 @@ module UserRatingsHelper
   # Params:
   # +rating_id+:: is the id of the instance of +Rating+, i.e Rating Perspective, which is being rated
   # Author: Mahmoud Abdelghany Hashish
-  def current_user_rating
-    if @user_rating = current_user.user_ratings.find_by_rating_id(params[:rating_id])
+  def current_user_rating(rating_id)
+    if @user_rating = current_user.user_ratings.find_by_rating_id(rating_id)
         @user_rating.value
     else
         'N/A'
