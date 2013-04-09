ac666c42625cc0152ad3abbfc6638f4133c6f8e6
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-04-09 17:30:31 +0200

    Issue #38 , added test controller to graph

--- Idearator/spec/controllers/dashboard_controller_spec.rb
+++ Idearator/spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
-
 describe DashboardController do
-
+  describe 'GET #index' do
+    it review a graph of tags 'ideas' do
+      ideasall = Factory(:ideasall)
+      get :index
+      assigns(:chart).should eq([:ideasall])
+    end
+    it 'renders the :index view'do
+      get :index
+      response.should render_template :index
+    end
+  end
 end
