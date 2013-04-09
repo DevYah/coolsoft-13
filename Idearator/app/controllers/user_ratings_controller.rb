bf558b4c02621d669698d28ef0309fe48efb3403
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-09 17:22:12 +0200

    Issue#89 Fix rating back end implementation and added average value

--- Idearator/app/controllers/user_ratings_controller.rb
+++ Idearator/app/controllers/user_ratings_controller.rb
@@ -13,8 +13,15 @@ class UserRatingsController < ApplicationController
     @user_rating.rating_id = params[:rating_id]
     @user_rating.user_id = current_user.id
     if @user_rating.save
+      @rating = Rating.find_by_id(params[:rating_id])
+      if @rating.value != 0
+        @rating.value = (@rating.value + @user_rating.value).to_f / (2).to_f
+      else
+        @rating.value = @user_rating.value
+      end
+      @rating.save
       respond_to do |format|
-        format.html { redirect_to @idea, :notice => 'Your rating has been saved' }
+        format.html { redirect_to idea_path(@idea), :notice => 'Your rating has been added' }
         format.js
       end
     else
@@ -33,7 +40,16 @@ class UserRatingsController < ApplicationController
   def update
     @idea = Idea.find_by_id(params[:idea_id])
     @user_rating = current_user.user_ratings.find_by_rating_id(params[:rating_id])
+    @val = @user_rating.value
     if @user_rating.update_attributes(params[:rating])
+      @rating = Rating.find_by_id(params[:rating_id])
+      @saved_r = UserRating.find_by_rating_id(params[:rating_id])
+      if @saved_r == 1
+        @rating.value = @user_rating.value.to_f
+      else
+        @rating.value = ((2 * @rating.value) - @val + @user_rating.value).to_f / (2).to_f
+      end
+      @rating.save
       respond_to do |format|
         format.html { redirect_to idea_path(@idea), :notice => 'Your rating has been updated' }
         format.js
