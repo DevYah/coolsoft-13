a6e2a9c4c1a8703b1ee248a64d707cb0c9892c3c
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-04-09 21:11:21 +0200

    Issue #80, add the bulb view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,35 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!, :only => [:index]
   def index
     @user = current_user
+    @threshold = Threshold.last
     if @user.type == 'Committee'
       @approved_ideas = Idea.where(:committee_id => @user.id)
+      @approved_thresholds = Array.new
+      @approved_ideas.each do |idea|
+        @v = VoteCount.find(:first, :conditions => { :idea_id => idea.id})
+        @percent = (@v.prev_day_votes * 100) / @threshold.threshold
+        if @percent == 0
+          @approved_thresholds << 1
+        elsif @percent < 50
+          @approved_thresholds << 2
+        else
+          @approved_thresholds << 3
+        end
+      end
     end
     @own_ideas = Idea.where(:user_id => @user.id, :approved => true)
+    @own_thresholds = Array.new
+    @own_ideas.each do |idea|
+        @v = VoteCount.find(:first, :conditions => { :idea_id => idea.id})
+        @percent = (@v.prev_day_votes * 100) / @threshold.threshold
+        if @percent == 0
+          @own_thresholds << 1
+        elsif @percent < 50
+          @own_thresholds << 2
+        else
+          @own_thresholds << 3
+        end
+    end
+    @counter = 1
   end
 end
