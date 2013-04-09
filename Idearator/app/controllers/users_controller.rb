eeec18851f0fad42154c58c47bb76da6dfdcccfc
Omar Kassem <omar.kassem67@gmail.com>
2013-04-09 13:09:30 +0200

    #94, linked the ban and unabn buttons to the ban_unabn method

--- Idearator/app/controllers/users_controller.rb
+++ Idearator/app/controllers/users_controller.rb
@@ -110,7 +110,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @ideas = Idea.find(:all, :conditions => { :user_id => @user.id })
-
+    @admin = current_user
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
