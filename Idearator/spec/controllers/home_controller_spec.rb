62dadb378b7e088d3fbb4c0a413dfe69a736b926
Hesham Nabil <heshamnabil13@gmail.com>
2013-04-09 17:46:56 +0200

    #36 , added tests

--- Idearator/spec/controllers/home_controller_spec.rb
+++ Idearator/spec/controllers/home_controller_spec.rb
@@ -4,32 +4,25 @@ describe HomeController do
 
   describe "GET index" do
 
-    it { should paginate(Home).with_default_per_page(10) }
-    
-    before(:each) do
-      assign(:ideas, Kaminari.paginate_array([
-        Factory.stub(:post),
-        Factory.stub(:post)
-      ]).page(1))
-    end
-
     it "succeeds" do
       get :index
       response.should be_success
     end
 
     it "returns results page" do
-       Program.should_receive(:paginate).with(:page => nil, :per_page => 30) do
-        [mock_program]
+      20.times do 
+        i = Idea.new
+        i.user_id = rand(1..40)
+        i.title = Faker::Name.name
+        i.description = Faker::Lorem.paragraph
+        i.problem_solved = Faker::Lorem.paragraph
+        i.approved = "true"
+        i.num_votes = rand(1..500)
+        i.save
       end
-      get :index
+      get :index , :page => 2
       response.should be_success
-      assigns(:programs).should == [mock_program]
-    end
-    it "renders the index page" do
-      get :index
-
-      response.should render_template("home/index")
+      assigns(:approved).should have(10).items
     end
   end
 end
