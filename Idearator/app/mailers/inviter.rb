e51b8af22a7d365a7566ef022fd2d1630be61d9b
mohammad-abdulkhaliq <mohammad28march1993@gmail.com>
2013-04-09 13:21:17 +0200

    Issue #96 mailer configurations

---
+++ Idearator/app/mailers/inviter.rb
@@ -0,0 +1,37 @@
+class Inviter < ActionMailer::Base
+  default from: "coolsoftc2@gmail.com"
+
+# returns invitation email
+# Params:
+# +email+:: the email of the guest
+# Author: muhammed hassan
+   def invite_email(email )
+    @email = email
+    @type = 'committee'
+    @address = 'idearator'
+    return mail(:to => email, :subject => "Welcome to My Awesome Site")
+  end
+	# Sends mail confirming registration
+	# 
+	# Params: 
+	# +email+:: the parameter is an instance of +User.email+ passed through the button_to Approve Committee
+	# Author: Mohammad Abdulkhaliq
+  def committee_accept(email)
+    @email = email
+    @type = 'committee'
+    @address = 'idearator'
+    mail(:to => email, :subject => "Congratulations!")
+  end
+	# Sends mail rejecting registration
+	# 
+	# Params: 
+	# +email+:: the parameter is an instance of +User.email+ passed through the button_to Approve Committee
+	# Author: Mohammad Abdulkhaliq
+  def committee_reject(email)
+    @email = email
+    @type = 'user'
+    @address = 'idearator'
+    mail(:to => email, :subject => "Idearator")
+  end
+
+end
--- Idearator/app/mailers/user_mailer.rb
+++ Idearator/app/mailers/user_mailer.rb
@@ -7,10 +7,4 @@ class UserMailer < ActionMailer::Base
     mail(:to => user.email, :subject => "Welcome to Shoghlana!")
   end
   
-  def committee_accept(user)
-    @user = user
-    @url  = "http://localhost:3000/users/login"
-    mail(:to => user.email, :subject => "Congratulations!")
-  end
-  
 end
