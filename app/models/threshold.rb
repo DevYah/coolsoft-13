5bc50e9a0cedbcee9dbb1233d3e57fe0bb606768
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-04-09 23:06:00 +0200

    Issue #80, add documentation in threshold model

--- app/models/threshold.rb
+++ app/models/threshold.rb
@@ -1,6 +1,13 @@
 class Threshold < ActiveRecord::Base
   attr_accessible :threshold
 
+  # recalculates the threshold every day at 12am using the gem wheneverize,
+  # as is shown in the schedule.rb file. It resets the curr_day_votes for
+  # the current day and moves the old values in prev_day_votes, to be used
+  # for comparison with the threshold.
+  # Params:
+  # none
+  # Author: Hisham ElGezeery
   def self.threshold_run
     Threshold.create(:threshold => VoteCount.maximum('curr_day_votes'))
     @vote_counts = VoteCount.all
