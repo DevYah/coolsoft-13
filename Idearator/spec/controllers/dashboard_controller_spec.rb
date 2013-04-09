c9bb7d7b78e8a117ce449edd3df305fcee3e99da
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-04-09 17:45:21 +0200

    Issue #77 adds the dashboard controllers test

--- Idearator/spec/controllers/dashboard_controller_spec.rb
+++ Idearator/spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe DashboardController do
-
+  describe 'GET #index' do
+    it 'populates an array of tags' do
+    tags = Factory(:ideatags)
+    get :index, :ideaid
+    assigns(:ideatags).should eq([tags])
+  end
+    it 'renders the index view' do
+      get :index, :tagid
+      response.should render_template :index
+    end
+  end
 end
