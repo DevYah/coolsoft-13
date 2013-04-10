b061b13c5a203c9e44a39e7931957485da7d84eb
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-04-10 23:38:21 +0200

    issue #92,corrected test

--- Idearator/spec/controllers/dashboard_controller_spec.rb
+++ Idearator/spec/controllers/dashboard_controller_spec.rb
@@ -1,14 +1,35 @@
 require 'spec_helper'
+
 describe DashboardController do
-  describe 'GET #index' do
-    it review a graph of tags 'ideas' do
-      ideasall = Factory(:ideasall)
-      get :index
-      assigns(:chart).should eq([:ideasall])
-    end
-    it 'renders the :index view'do
-      get :index
-      response.should render_template :index
+
+  describe 'GET #graph' do
+    it 'returns chart  ' do
+      l=User.new
+      l.email = "lina@gmail.com"
+      l.password = "123123123"
+      l.first_name = "lina"
+      l.save
+      tag = Tag.new
+      tag.name = "tag"
+      tag.save
+      ideas = []
+      20.times do
+        i = Idea.new
+        i.user_id = l.id
+        i.title = Faker::Name.name
+        i.description = Faker::Lorem.paragraph
+        i.problem_solved = Faker::Lorem.paragraph
+        i.approved = 'true'
+        i.num_votes = rand(1..500)
+        i.save
+        ideas+=[i]
+        tagidea = IdeasTags.new
+        tagidea.tag_id = tag.id
+        tagidea.idea_id = i.id
+        tagidea.save
+      end
+      get :graph, :ideas => ideas
+      assigns(:data_table).should have(20).items
     end
   end
 end
