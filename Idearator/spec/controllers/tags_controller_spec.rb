64725d84c95eccfc57133af10a3069fd30e03895
mohammad-abdulkhaliq <mods@hal9000.(none)>
2013-04-10 21:31:10 +0200

    Issue #97 added show action test

--- Idearator/spec/controllers/tags_controller_spec.rb
+++ Idearator/spec/controllers/tags_controller_spec.rb
@@ -2,45 +2,145 @@
 require 'spec_helper'
 
 describe TagsController do
-  describe "GET #index" do
-    it "populates an array of tags"
-    it "renders the :index view"
+  include Devise::TestHelpers
+  render_views
+  before :each do
+    @a = Admin.new
+    @a.email = 'admin@gmail.com'
+    @a.username = 'admin'
+    @a.password = '123123123'
+    @a.confirm!
+    @a.save
+
+    sign_in @a
   end
-  
-  describe "GET #show" do
-    it "assigns the requested contact to @contact"
-    it "renders the :show template"
-    it "renders the _modal"
-  end
-  
-  describe "GET #new" do
-    it "assigns a new Tag to @tag"
-    it "renders the :new template"
-  end
-  
-  describe "POST #create" do
-    context "with valid attributes" do
-      it "saves the new tag in the database"
-      it "redirects to the :index view"
+    describe "PUT #addsym" do
+      context "Synonym does not exist as a tag" do
+      it "creates a new tag" do
+        t = Tag.new(:name => 'Soft')
+        t.save
+        expect { put :addsym, id: t.id, tag: {:name => 'Eng'} } .to change(Tag, :count).by(1) 
+      end
+      it "updates the tag tags list on both sides" do
+        t = Tag.new(:name => 'Soft')
+        t.save
+       put :addsym, id: t.id, tag: {:name => 'Eng'}
+       t.tags.should eq(1)
+       t.tags[0].tags.should eq(1)
+      end
+
+      it "redirects to #show" do
+        t = Tag.new(:name => 'Soft')
+        t.save
+        put :addsym, id: t.id, tag: {:name => 'Eng'} 
+        response.should redirect_to t
+      end
+
+      end
+
+      context "Synonym exists as a tag" do
+        
+       
+       it "updates the tag tags list on both sides" do
+        t = Tag.new(:name => 'Soft')
+        t.save
+        t2 = Tag.new(:name => 'Eng')
+        t2.save
+        put :addsym, id: t.id, tag: {:name => 'Eng'}
+        t.reload
+        t2.reload
+        t.tags.should eq(1)
+        t2.tags.should eq(1)
+        end
+
+      it "redirects to #show" do
+        t = Tag.new(:name => 'Soft')
+        t.save
+        t2 = Tag.new(:name => 'Eng')
+        t2.save
+        put :addsym, id: t.id, tag: {:name => 'Eng'}
+        response.should redirect_to t
+      end 
+    end
+     
+     context "Synonym already exists in tag's sym list" do
+
+
+        it "does not do anything" do
+        t = Tag.new(:name => 'Soft')
+        t.save
+        t2 = Tag.new(:name => 'Eng')
+        t2.save
+        t.tags << t2
+        t2.tags << t
+        put :addsym, id: t.id, tag: {:name => 'Eng'}
+        t.tags.should_not eq(2)
+        t2.tags.should_not eq(2)
+        end
+
+        it "redirects to #show" do
+          t = Tag.new(:name => 'Soft')
+          t.save
+          t2 = Tag.new(:name => 'Eng')
+          t2.save
+          t.tags << t2
+          t2.tags << t
+          put :addsym, id: t.id, tag: {:name => 'Eng'}
+          response.should redirect_to t
+        end 
+      end
     end
     
-    context "with invalid attributes" do
-      it "does not save the new contact in the database"
-      it "re-renders the :new template"
+    describe "PUT #delsym" do
+      
+      it "destroys @tag.tags using params[:id2]" do
+      t = Tag.new(:name => 'Soft')
+      t.save
+      t2 = Tag.new(:name => 'Eng')
+      t2.save
+      t.tags << t2
+      t2.tags << t 
+        expect { put :delsym, id: t.id, sym: t2.id} .to change(t.tags, :count).by(1) 
+      end
+      it "redirects to :show view" do
+      t = Tag.new(:name => 'Soft')
+      t.save
+      t2 = Tag.new(:name => 'Eng')
+      t2.save
+      t.tags << t2
+      t2.tags << t 
+        put :delsym, id: t.id, sym: t2.id
+        response.should redirect_to t
+      end
     end
-    
-    describe "addsym" do
-      it "places the synonym name to :name in params[:tag][:name]"
-      it "updates the tag list of @tag"
-      it "updates the tag list of synonym tag"
-      it "redirects to :show view"
-    end
-    
-    describe "delsym" do
-      it "retrives the Tag instance from :id"
-      it "destroys @tag.tags using params[:id2]"
-      it "redirects to :show view"
-    end
-    
-  end
+
+    describe "GET #show" do
+      
+      it "gets the tag and it's associated syms" do
+      t = Tag.new
+      t.name = 'Software'
+      t.save
+      t.tags << Tag.new(:name => '1')
+      t.tags << Tag.new(:name => '2')
+      t.tags << Tag.new(:name => '3')
+      t.tags << Tag.new(:name => '4')
+      t.tags << Tag.new(:name => '5')
+      get :show, :id => t.id
+      assigns(:tag).should eq(t)
+      assigns(:tags).should eq(t.tags)
+      end
+
+      it "renders the partial view _modal" do
+      t = Tag.new
+      t.name = 'Software'
+      t.save
+      t.tags << Tag.new(:name => '1')
+      t.tags << Tag.new(:name => '2')
+      t.tags << Tag.new(:name => '3')
+      t.tags << Tag.new(:name => '4')
+      t.tags << Tag.new(:name => '5')
+      get :show, :id => t.id
+      response.should render_template(:partial => 'modal')
+      end
+      end  
 end
