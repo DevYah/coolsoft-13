a0591a511cde5c6b5ddc3af494d6c356817c7c15
Mohamed Sameh <mohamed.sameh226@gmail.com>
2013-04-11 01:06:59 +0200

    Issue #78, Fix documentation.

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 	$("#share").click(function() {
-	$("#show").toggle("slow");
+		$("#show").toggle("slow");
 	});
 	$("#fbk").click(function() {
-			var pathname = window.location;
-  		var fburl = 'http://www.facebook.com/sharer.php?u='+encodeURI(pathname);
-  		var win=window.open(fburl, 'popup');
- 			win.focus();
+		var pathname = window.location;
+  	var fburl = 'http://www.facebook.com/sharer.php?u='+encodeURI(pathname);
+  	var win=window.open(fburl, 'popup');
+ 		win.focus();
 	});
 	$("#tw").click(function() {
- 			var pathname = window.location;
-  		var tweeturl = 'http://twitter.com/share?url='+encodeURI(pathname)+'&text=Checkout this idea on idearator';
-  		var win=window.open(tweeturl, 'popup');
- 			win.focus();
+ 		var pathname = window.location;
+  	var tweeturl = 'http://twitter.com/share?url='+encodeURI(pathname)+'&text=Checkout this idea on idearator';
+  	var win=window.open(tweeturl, 'popup');
+ 		win.focus();
 	});
 	$("#fbk").tooltip({
 		toggle: "tooltip",
