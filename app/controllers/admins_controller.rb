0373e35a213246ea0974cfddc431382da04f7157
Omar Kassem <omar.kassem67@gmail.com>
2013-04-10 02:32:17 +0200

    issue#94 fixed a bug

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,9 +1,12 @@
 class AdminsController < ApplicationController
+before_filter :authenticate_user!
 # toggles the ban status of the selected user
 # Author: Omar Kassem
   def ban_unban
-    @user=User.find(session[:user_id])
-    @user.toggle(:banned)
-    @user.save
+  	if current_user.type == 'Admin'
+      @user=User.find(session[:user_id])
+      @user.toggle(:banned)
+      @user.save
+    end  
   end  
 end
