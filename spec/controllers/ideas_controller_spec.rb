1c1f8615178608f71a27fe44201c15dc2eba32a6
Marwa Mehanna <marwa.mehana@gmail.com>
2013-04-10 18:22:32 +0200

    Issue #84 ,Add test files to ideas_controller

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -3,8 +3,34 @@ require 'spec_helper'
 describe IdeasController do
   describe 'PUT vote' do
     include Devise::TestHelpers
+    context 'user wants to vote' do
+      before :each do
+        @user = FactoryGirl.build(:user)
+        @user.confirm!
+        @idea = FactoryGirl.create(:idea)
+        @idea.user_id = @user.id
+        @idea.save
+        sign_in @user
+      end
+      it 'idea id in user.votes' do
+        put :vote, :id => @idea.id
+        @idea.reload
+        @voted = @user.votes.find(@idea)
+        (@voted.id).should eql(@idea.id)
+      end
+      it 'redirects to idea' do
+        put :vote, :id => @idea.id
+        response.should redirect_to @idea
+      end
+      it 'increase idea votes' do
+        @numvotes = @idea.num_votes+1
+        put :vote, :id => @idea.id
+        @idea.reload
+        (@numvotes).should eql(@idea.num_votes)
+      end
+    end
     
-    context 'idea creator wants to vote' do
+    context 'user wants to unvote' do
       before :each do
         @user = FactoryGirl.build(:user)
         @user.confirm!
@@ -14,128 +40,22 @@ describe IdeasController do
         sign_in @user
       end
 
-      it 'archives the idea' do
-        @ideavoted = @user.votes.detect { |w|w.id == @idea.id }
-        put :vote, :id => @idea.id
-        @idea.reload
-        (@ideavoted).should eql(@idea.id)
-      end
-
-      it 'redirects to idea' do
-        put :archive, :id => @idea.id
-        response.should redirect_to @idea
-      end
-    end
-
-    context 'admin wants to archive' do
-      before :each do
-        @admin = FactoryGirl.build(:admin)
-        @admin.confirm!
-        @idea = FactoryGirl.create(:idea)
-        sign_in @admin
-      end
-
-      it 'votes for the idea the idea' do
-        @ideavoted = @admin.votes.detect { |w|w.id == @idea.id }
-        put :vote, :id => @idea.id
-        @idea.reload
-        (@ideavoted).should eql(@idea.id)
-      end
-
-      it 'redirects to idea' do
-        put :archive, :id => @idea.id
-        response.should redirect_to @idea
-      end
-    end
-
-    context 'normal user wants to vote' do
-      before :each do
-        @user = FactoryGirl.build(:user)
-        @user.confirm!
-        @idea = FactoryGirl.create(:idea)
-        sign_in @user
-      end
-
-      it 'does vote for the idea' do
-        @ideavoted = @user.votes.detect { |w|w.id == @idea.id }
-        put :vote, :id => @idea.id
-        @idea.reload
-        (@ideavoted).should eql(@idea.id)
-      end
-
-      it 'redirects to idea' do
-        put :vote, :id => @idea.id
-        response.should redirect_to @idea
-      end
-    end
-  end
-
-  describe 'PUT unvote' do
-    include Devise::TestHelpers
-    
-    context 'idea creator wants to unvote' do
-      before :each do
-        @user = FactoryGirl.build(:user)
-        @user.confirm!
-        @idea = FactoryGirl.create(:idea)
-        @idea.user_id = @user.id
-        @idea.save
-        sign_in @user
-      end
-
-      it 'unvote the idea' do
-        @ideavoted = @user.votes.detect { |w|w.id == @idea.id }
+      it 'idea id deleted from user.votes' do
         put :unvote, :id => @idea.id
         @idea.reload
-        (@ideavoted).should eql(nil)
+        @voted = @user.votes.find(:first ,:conditions => {id: @idea_id})
+        (@voted).should eql(nil)
       end
-
       it 'redirects to idea' do
         put :unvote, :id => @idea.id
         response.should redirect_to @idea
       end
-    end
-
-    context 'admin wants to unvote' do
-      before :each do
-        @admin = FactoryGirl.build(:admin)
-        @admin.confirm!
-        @idea = FactoryGirl.create(:idea)
-        sign_in @admin
-      end
-
-      it 'unarchives the idea' do
-       @ideavoted = @admin.votes.detect { |w|w.id == @idea.id }
+      it 'increase idea votes' do
+        @numvotes=@idea.num_votes-1
         put :unvote, :id => @idea.id
         @idea.reload
-        (@ideavoted).should eql(nil)
-      end
-
-      it 'redirects to idea' do
-        put :unvote, :id => @idea.id
-        response.should redirect_to @idea
+        (@numvotes).should eql(@idea.num_votes)
       end
     end
-
-    context 'normal user wants to unarchive' do
-      before :each do
-        @user = FactoryGirl.build(:user)
-        @user.confirm!
-        @idea = FactoryGirl.create(:idea)
-        sign_in @user
-      end
-
-      it 'does not unarchive the idea' do
-        @ideavoted = @user.votes.detect { |w|w.id == @idea.id }
-        put :unvote, :id => @idea.id
-        @idea.reload
-        (@ideavoted).should eql(nil)
-      end
-
-      it 'redirects to idea' do
-           put :unvote, :id => @idea.id
-        response.should redirect_to @idea
-      end
-    end
-  end
+  end  
 end
