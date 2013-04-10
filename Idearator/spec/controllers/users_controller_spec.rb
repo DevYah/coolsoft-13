1665b63592cee756ae609c0600c93d98e67d210d
Mohamed Sameh <mohamed.sameh226@gmail.com>
2013-04-11 00:47:44 +0200

    Issue #79, Fix Test errors

--- Idearator/spec/controllers/users_controller_spec.rb
+++ Idearator/spec/controllers/users_controller_spec.rb
@@ -15,5 +15,18 @@ describe UsersController do
 			u.own_idea_notifications.should eq(true)
 			u.participated_idea_notifications.should eq(false)
 		end
+		it "changes user settings" do
+			u1= User.new
+			u1.email="test1@gmail.com"
+			u1.username= "sameh"
+			u1.password= "123123123"
+			u1.confirm!
+			u1.save
+			sign_in u1
+			put :change_settings, :user=> []
+			u1.reload
+			u1.own_idea_notifications.should eq(false)
+			u1.participated_idea_notifications.should eq(false)
+		end
 	end
 end
