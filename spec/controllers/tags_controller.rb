f154e7241ed6151164ed57e1e6e56b3223fe9dd1
muhammedabuali <muhammedabuali@gmail.com>
2013-04-10 03:19:28 +0200

    Issue #85 , added tests to tags/ajax action

---
+++ spec/controllers/tags_controller.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe TagsController do
+
+  describe "GET ajax" do
+
+    it "auto completes tags" do
+      t = Tag.new
+      t.name = 'computer science'
+      t.save
+
+      t2 = Tag.new
+      t2.name = 'life'
+      t2.save
+
+      get :ajax, :q => 'te' 
+      response.body.should ==  "[{\"name\":\"computer science\"}]"
+    end
+
+  end
+end
