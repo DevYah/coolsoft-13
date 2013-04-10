6e34147c321fd7a69189291e55df623357732a15
Hesham Nabil <heshamnabil13@gmail.com>
2013-04-10 23:07:13 +0200

    #87, fixed indentation

--- Idearator/app/controllers/home_controller.rb
+++ Idearator/app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
-	#Used to display the idea stream, top ten and trending ideas.
-	#Author: Hesham Nabil
-	def index
+  #Used to display the idea stream, top ten and trending ideas.
+  #Author: Hesham Nabil
+  def index
      @approved = Idea.find(:all, :conditions => { :approved => true })
      @user = current_user
          @top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
