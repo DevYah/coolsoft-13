9794e8c86a5fa4708ed019de945ce5b3f5c0228a
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-04-10 17:09:09 +0200

    Issue #52, edit users controller and add my profile link in navigation bar
    Edits in the users controller were made because they were caused by the
    commit that is before that.

--- Idearator/app/controllers/users_controller.rb
+++ Idearator/app/controllers/users_controller.rb
@@ -32,7 +32,8 @@ def deactivate
   end
   else
     respond_to do |format|
-     format.html { redirect_to action: 'confirm_deactivate' flash[:notice] = 'Wrong password' }
+     format.html { redirect_to action: 'confirm_deactivate' 
+      flash[:notice] = 'Wrong password' }
      format.json { head :no_content }
     end
   end
@@ -61,24 +62,31 @@ def expertise
       @tags= Tag.all
     else
       respond_to do |format|
-      format.html { redirect_to controller: 'home', action: 'index' }
+      format.html{
+        redirect_to controller: 'home', action: 'index'
+      }
     end
     end
   else
     respond_to do |format|
-      format.html { redirect_to controller: 'home', action: 'index' }
+      format.html{
+        redirect_to controller: 'home', action: 'index'
+      }
     end
   end
 end
 
-# Enter chosen tags sent from expertise view, in committeestags table
+# Enter chosen tags sent from expertise view, in committeestags table 
 # Params:
 # +tags[]+:: the parameter is ana instance of +tag+ passed through the form from expertise action
 # Author: Mohamed Sameh
 def new_committee_tag
   if params[:user] == nil
     respond_to do |format|
-      format.html {flash[:notice] = 'You must choose at least 1 area of expertise' redirect_to action: 'expertise' }
+      format.html{
+        flash[:notice] = 'You must choose at least 1 area of expertise'
+        redirect_to action: 'expertise'
+      }
     end
   else
     @tags= params[:user][:tags]
@@ -86,15 +94,15 @@ def new_committee_tag
       CommitteesTags.create(:committee_id => current_user.id , :tag_id => tag)
     end
     respond_to do |format|
-      format.html { redirect_to controller: 'home', action: 'index' }
+      format.html{
+        redirect_to controller: 'home', action: 'index'
+      }
     end
   end
 end
 
 #This method is used to generate the view of each User Profile. A specific user and his ideas are made
 #available to the view to be presented in the appropriate manner.
-#Params:
-#none
 #Author: Hisham ElGezeery
 def show
   @user = User.find(params[:id])
