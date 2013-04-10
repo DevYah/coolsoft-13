efce6f27a0f3e2d4eb3f9ae3b9a70fe2c1b4a2ed
mennaamr <menna.amr2@gmail.com>
2013-04-10 19:18:46 +0300

    Fixed indentation in user_mailer.rb

--- Idearator/app/mailers/user_mailer.rb
+++ Idearator/app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ActionMailer::Base
 
-  	#This method sends an email to the Admin
-  	#if a user signed up as committee to inform them
-  	#that they should accept/reject their application
-  	#Params: admin
-  	#Author: Menna Amr
-  	def committee_signup(admin)
-  		mail(:to => admin, :subject => "New Committee Member Sign Up")
-  	end
+  #This method sends an email to the Admin
+  #if a user signed up as committee to inform them
+  #that they should accept/reject their application
+  #Params: admin
+  #Author: Menna Amr
+  def committee_signup(admin)
+  	mail(:to => admin, :subject => "New Committee Member Sign Up")
+  end
 end
