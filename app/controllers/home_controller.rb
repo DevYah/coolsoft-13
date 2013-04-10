52d3f9aae7da9400b5f42d872b409e3d67f3ae56
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-04-10 17:10:20 +0200

    issue #38 ,corrected test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,12 @@
 class HomeController < ApplicationController
-	#Used to display the idea stream, top ten and trending ideas.
-	#Author: Hesham Nabil
-	#Method gets all ideas, order them in descending order according to number of votes 
-	#and sends top ten ideas to index view page
-	#Author: Lina Basheer 
-	def index
-		@approved = Idea.find(:all, :conditions => { :approved => true})
-		@user = current_user
+  #Used to display the idea stream, top ten and trending ideas.
+  #Author: Hesham Nabil
+  #Method gets all ideas, order them in descending order according to number of votes
+  #and sends top ten ideas to index view page
+  #Author: Lina Basheer
+  def index
+    @approved = Idea.find(:all, :conditions => { :approved => true})
     @top = Idea.find(:all, :conditions => { :approved => true }, :order=> 'num_votes desc', :limit=>10)
     render :action => 'index.html.erb'
   end
 end
-
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,15 +1,27 @@
-
 require 'spec_helper'
+
 describe HomeController do
-  describe 'GEt #index' do
-    it 'populates an array of Top ten ideas' do
-    top = Factory(:top)
-    get :index
-    assigns(:top).should eq([top])
-  end
-  it 'renders the :index view' do
-    get :index
-    response.should render_template :index
-  end
+
+  describe 'GET index' do
+
+    it 'succeeds' do
+      get :index
+      response.should be_success
+    end
+    it 'returns top ten ' do
+      20.times do
+        i = Idea.new
+        i.user_id = rand(1..40)
+        i.title = Faker::Name.name
+        i.description = Faker::Lorem.paragraph
+        i.problem_solved = Faker::Lorem.paragraph
+        i.approved = 'true'
+        i.num_votes = rand(1..500)
+        i.save
+      end
+      get :index , :page => 1
+      response.should be_success
+      assigns(:top).should have(10).items
+    end
   end
 end
