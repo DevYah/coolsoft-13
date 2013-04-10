6ca208e04e89bcba577c2d20a6d9daaeabe74a8e
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-10 19:55:34 +0200

    Issue #73 Fix tests

--- Idearator/spec/models/approve_committee_notification_spec.rb
+++ Idearator/spec/models/approve_committee_notification_spec.rb
@@ -4,9 +4,8 @@ describe ApproveCommitteeNotification do
   it 'adds approve committee notification to idea_notifications table'do
     user1 = User.create(email: 'amina@gmail.com')
     user2 = User.create(email: 'marwa@gmail.com')
-    ApproveCommitteeNotification.send_notification(user1, idea, [user2])
+    ApproveCommitteeNotification.send_notification(user1, [user2])
     expect(UserNotification.last.type).to eq('ApproveCommitteeNotification')
-    expect(UserNotification.last.idea_id).to eq(idea.id)
-    expect(UserNotification.last.user_id).to eq(user.id)
+    expect(UserNotification.last.user_id).to eq(user1.id)
   end
 end
