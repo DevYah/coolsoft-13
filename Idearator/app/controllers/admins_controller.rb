834c1069785a6754b2fdd632c9668f2f9ccb1d2c
Omar Kassem <omar.kassem67@gmail.com>
2013-04-10 23:40:09 +0200

    #94, added test files

--- Idearator/app/controllers/admins_controller.rb
+++ Idearator/app/controllers/admins_controller.rb
@@ -1,12 +1,11 @@
+
 class AdminsController < ApplicationController
-before_filter :authenticate_user!
 # toggles the ban status of the selected user
 # Author: Omar Kassem
   def ban_unban
-  	if current_user.type == 'Admin'
-      @user=User.find(session[:user_id])
+      @user=User.find(params[:id])
       @user.toggle(:banned)
       @user.save
-    end  
+     
   end  
 end
