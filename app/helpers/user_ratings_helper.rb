e1750df4bfe1c3aa16b9e36215f483e8209ee882
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-11 18:44:10 +0200

    Issue#89 Fix variable names in user ratings helper

--- app/helpers/user_ratings_helper.rb
+++ app/helpers/user_ratings_helper.rb
@@ -5,10 +5,10 @@ module UserRatingsHelper
   # +rating_id+:: is the id of the instance of +Rating+, i.e Rating Perspective, which is being rated
   # Authour: Mahmoud Abdelghany Hashish
   def rating_ballot(rating_id)
-    if @user_rating = current_user.user_ratings.find_by_rating_id(rating_id)
-        @user_rating
+    if user_rating = current_user.user_ratings.find_by_rating_id(rating_id)
+      user_rating
     else
-        current_user.user_ratings.new
+      current_user.user_ratings.new
     end
   end
 
@@ -17,10 +17,10 @@ module UserRatingsHelper
   # +rating_id+:: is the id of the instance of +Rating+, i.e Rating Perspective, which is being rated
   # Author: Mahmoud Abdelghany Hashish
   def current_user_rating(rating_id)
-    if @user_rating = current_user.user_ratings.find_by_rating_id(rating_id)
-        @user_rating.value
+    if user_rating = current_user.user_ratings.find_by_rating_id(rating_id)
+      user_rating.value
     else
-        'N/A'
+      'N/A'
     end
   end
 end
