5a603bf0c603b5c8cfe6bd919b16e0f0e849c2f3
Mohamed Sameh <mohamed.sameh226@gmail.com>
2013-04-11 14:20:12 +0200

    Issue #78, Fix Indentation

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
 	});
 	$("#fbk").click(function() {
 		var pathname = window.location;
-  	var fburl = 'http://www.facebook.com/sharer.php?u='+encodeURI(pathname);
-  	var win=window.open(fburl, 'popup');
+  		var fburl = 'http://www.facebook.com/sharer.php?u='+encodeURI(pathname);
+  		var win=window.open(fburl, 'popup');
  		win.focus();
 	});
 	$("#tw").click(function() {
  		var pathname = window.location;
-  	var tweeturl = 'http://twitter.com/share?url='+encodeURI(pathname)+'&text=Checkout this idea on idearator';
-  	var win=window.open(tweeturl, 'popup');
+ 	 	var tweeturl = 'http://twitter.com/share?url='+encodeURI(pathname)+'&text=Checkout this idea on idearator';
+  		var win=window.open(tweeturl, 'popup');
  		win.focus();
 	});
 	$("#fbk").tooltip({
