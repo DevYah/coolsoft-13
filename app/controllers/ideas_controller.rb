050db3eaba7354ff76d4af2ae357d1d308615ea7
muhammedabuali <muhammedabuali@gmail.com>
2013-04-11 10:55:53 +0200

    Issue #85 , fixed indentation and moved inline scripts and styles

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,16 +1,18 @@
 class IdeasController < ApplicationController
+
   def show()
   end
-  # filters the ideas that have one or more of given tags
+
+# filters the ideas that have one or more of given tags
 # Params:
 # +tags:: the parameter is an list of +Tag+ passed through tag autocomplete field
 # Author: muhammed hassan
   def filter()
-  @approved = Idea.joins(:tags).where(:tags => {:name => params[:myTags]}).uniq.page(params[:page]).per(10)
-  @tags = params[:myTags]  
+    @approved = Idea.joins(:tags).where(:tags => {:name => params[:myTags]}).uniq.page(params[:page]).per(10)
+    @tags = params[:myTags]
     respond_to do |format|
-        format.js
-        format.json  { render :json => @approved }
+      format.js
+      format.json  { render :json => @approved }
     end
   end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,6 @@ class TagsController < ApplicationController
 # Author: muhammed hassan
   def ajax
     @tags = Tag.find(:all, conditions: ['name LIKE(?)', "%#{ params[:q] }%"]).take(5)
-    render :json => @tags , only: [:name]
-  end 
+    render :json => @tags , only: [:name,:id]
+  end
 end
