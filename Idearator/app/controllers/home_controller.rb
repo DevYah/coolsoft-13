68122e04f231bda169b3acd1aef0db27a87ed03e
Hesham Nabil <heshamnabil13@gmail.com>
2013-04-11 12:19:23 +0200

    #87 , fixed indentation in home_controller

--- Idearator/app/controllers/home_controller.rb
+++ Idearator/app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
   def index
      @approved = Idea.find(:all, :conditions => { :approved => true })
      @user = current_user
-         @top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
-         render :action => 'index.html.erb'
+     @top= Idea.find(:all,:order=> "num_votes",:limit=>10).reverse
+     render :action => 'index.html.erb'
   end
 
 end
