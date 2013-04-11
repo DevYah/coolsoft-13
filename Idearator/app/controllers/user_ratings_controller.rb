68ad65fbe55697c0a72f4f49c07863e2e8d7b766
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-11 18:42:31 +0200

    Issue#89 Fix variable names for user ratings controller methods and some modifications

--- Idearator/app/controllers/user_ratings_controller.rb
+++ Idearator/app/controllers/user_ratings_controller.rb
@@ -8,25 +8,25 @@ class UserRatingsController < ApplicationController
   # +rating_id+:: is the id of the instance of +Rating+, i.e Rating Perspective, which is being rated
   # Author: Mahmoud Abdelghany Hashish
   def create
-    @idea = Idea.find_by_id(params[:idea_id])
-    @user_rating = UserRating.new(params[:rating])
-    @user_rating.rating_id = params[:rating_id]
-    @user_rating.user_id = current_user.id
+    idea = Idea.find_by_id(params[:idea_id])
+    user_rating = UserRating.new(params[:rating])
+    user_rating.rating_id = params[:rating_id]
+    user_rating.user_id = current_user.id
     
-    if @user_rating.save
-      @rating = Rating.find_by_id(params[:rating_id])
-      @saved_r = UserRating.find_by_rating_id(params[:rating_id])
+    if user_rating.save
+      rating = Rating.find_by_id(params[:rating_id])
+      saved_r = UserRating.find_by_rating_id(params[:rating_id])
 
-      if @saved_r.class != Array
-        @rating.value = @user_rating.value.to_f
+      if saved_r.class != Array
+        rating.value = user_rating.value.to_f
       else
-        @rating.value = ((@saved_r.size * @rating.value) + @user_rating.value).to_f / (@saved_r.size).to_f
+        rating.value = ((saved_r.size * @rating.value) + user_rating.value).to_f / (saved_r.size).to_f
       end
 
       @rating.save
     else
       respond_to do |format|
-        format.html { redirect_to @idea, :notice => 'Your rating has not been saved, please retry!' }
+        format.html { redirect_to idea, :notice => 'Your rating has not been saved, please retry!' }
         format.js
       end
     end
@@ -39,24 +39,24 @@ class UserRatingsController < ApplicationController
   # +rating_id+:: is the id of the instance of +Rating+, i.e Rating Perspective, which is being rated
   # Author: Mahmoud Abdelghany Hashish
   def update
-    @idea = Idea.find_by_id(params[:idea_id])
-    @user_rating = current_user.user_ratings.find_by_rating_id(params[:rating_id])
-    @val = @user_rating.value
+    idea = Idea.find_by_id(params[:idea_id])
+    user_rating = current_user.user_ratings.find_by_rating_id(params[:rating_id])
+    val = user_rating.value
     
-    if @user_rating.update_attributes(params[:rating])
-      @rating = Rating.find_by_id(params[:rating_id])
-      @saved_r = UserRating.find_by_rating_id(params[:rating_id])
+    if user_rating.update_attributes(params[:rating])
+      rating = Rating.find_by_id(params[:rating_id])
+      saved_r = UserRating.find_by_rating_id(params[:rating_id])
 
-      if @saved_r.class != Array
-        @rating.value = @user_rating.value.to_f
+      if saved_r.class != Array
+        rating.value = user_rating.value.to_f
       else
-        @rating.value = ((@saved_r.size * @rating.value) - @val + @user_rating.value).to_f / (@saved_r.size).to_f
+        rating.value = ((saved_r.size * rating.value) - val + user_rating.value).to_f / (saved_r.size).to_f
       end
 
-      @rating.save
+      rating.save
     else 
       respond_to do |format|
-        format.html { redirect_to idea_path(@idea), :notice => 'Your rating has not been updated, please retry!' }
+        format.html { redirect_to idea_path(idea), :notice => 'Your rating has not been updated, please retry!' }
         format.js 
       end
     end
