d40b1654056374aa51cade8ab5cf0cf4353e9c2a
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-11 12:11:25 +0200

    Revert "Merge branch 'C3_Marwa_39_SubmitEditIdea' into C1_AminaZoheir_#73_ViewNotifications"
    This reverts commit 9f9dc8af33810c1dc0b5af7558e7759cc177b6a2, reversing
    changes made to 669982d3bdcf93009294ebe958638c6412967234.

--- app/models/vote_count.rb
+++
@@ -1,4 +0,0 @@
-class VoteCount < ActiveRecord::Base
-  attr_accessible :prev_day_votes, :curr_day_votes
-  belongs_to :idea
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Sprint0::Application.routes.draw do
+  match '/users/expertise' => 'users#expertise'
+  match '/users/new_committee_tag' => 'users#new_committee_tag'
+  match '/home/index' => 'home#index'
+
+  #get "ideas/new"
+  resources :ideas
+
+
 
 
   default_url_options :host => 'localhost:3000'
   devise_for :users, :controllers => { :registrations => 'registrations' }
-  resources :ideas, :controller =>'ideas'
-  match '/users/expertise' => 'users#expertise'
-  match '/users/new_committee_tag' => 'users#new_committee_tag'
-  match '/home/index' => 'home#index'
-  devise_for :committees, :controllers => { :registrations => 'registrations' }
 
+
+
+  devise_for :committees, :controllers => { :registrations => 'registrations' }
 
 
   # The priority is based upon order of creation:
@@ -32,7 +38,6 @@ Sprint0::Application.routes.draw do
   #     resources :comments, :sales
   #     resource :seller
   #   end
-  #root:to =>'ideas#index'
   root:to => 'home#index'
   # Sample resource route with more complex sub-resources
   #   resources :products do
@@ -51,7 +56,7 @@ Sprint0::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
- #root :to => 'ideas#index'
+ #root :to => 'ideas#show'
 
   # See how all your routes lay out with "rake routes"
 
