fb0b8f5c2bfb9e2846638898f5670576b10afc36
mohammad-abdulkhaliq <mohammad28march1993@gmail.com>
2013-04-12 01:08:03 +0200

    Revert "Merge branch 'C1_AminaZoheir_#73_ViewNotifications' into C2_mohammad-abdulkhaliq_#96_approve_committee"
    This reverts commit 87b3cda810a8c7d32acb19507c30ce624deb1bfa, reversing
    changes made to 672eb371ea167b1d0d1fce19ae2ed8eaf17d6fb9.

--- Idearator/app/models/user.rb
+++ Idearator/app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
   has_many :user_notifications_users
   has_many :user_notifications, :through => :user_notifications_users
   has_and_belongs_to_many :comments, :join_table => :likes
-  has_and_belongs_to_many :likes, :class_name => 'Comment', :join_table => :votes
+  has_and_belongs_to_many :likes, :class_name => 'Comment', :join_table => :likes
   has_and_belongs_to_many :votes, :class_name => 'Idea', :join_table => :votes
   has_attached_file :photo, :styles => { :small => '60x60>', :medium => "300x300>",:thumb => '10x10!' }, :default_url => '/images/:style/missing.png'
 end
