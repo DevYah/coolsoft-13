fa0dddc7ad04e2e46aa8e401241b49230b6ab161
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-11 16:50:04 +0200

    Issue#44 Add new test cases to delete idea tests

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -11,6 +11,14 @@ describe IdeasController do
         @idea = FactoryGirl.create(:idea)
         @idea.user_id = @user.id
         @idea.save
+        @comment = FactoryGirl.build(:comment)
+        @comment.user_id = @user.id
+        @comment.idea_id = @idea.id
+        @comment.save
+        @vote = FactoryGirl.build(:vote)
+        @vote.user_id = @user.id
+        @vote.idea_id = @idea.id
+        @vote.save
         sign_in @user
       end
 
@@ -20,7 +28,15 @@ describe IdeasController do
 
       it 'redirects to home' do
         delete :destroy, :id => @idea.id
-          response.should redirect_to '/'
+        response.should redirect_to '/'
+      end
+
+      it 'deletes idea comments' do
+        expect { delete :destroy, :id => @idea.id }.to change(Comment, :count).by(-1)
+      end
+
+      it 'deletes idea votes' do
+        expect { delete :destroy, :id => @idea.id }.to change(Vote, :count).by(-1)
       end
     end
 
@@ -33,6 +49,14 @@ describe IdeasController do
         @idea = FactoryGirl.create(:idea)
         @idea.user_id = @userone.id
         @idea.save
+        @comment = FactoryGirl.build(:comment)
+        @comment.user_id = @userone.id
+        @comment.idea_id = @idea.id
+        @comment.save
+        @vote = FactoryGirl.build(:vote)
+        @vote.user_id = @userone.id
+        @vote.idea_id = @idea.id
+        @vote.save
         sign_in @usertwo
       end
 
@@ -43,6 +67,14 @@ describe IdeasController do
       it 'redirects to idea' do
         delete :destroy, :id => @idea.id
         response.should redirect_to @idea
+      end
+
+      it 'does not delete idea comments' do
+        expect { delete :destroy, :id => @idea.id }.to change(Comment, :count).by(0)
+      end
+
+      it 'does not delete idea votes' do
+        expect { delete :destroy, :id => @idea.id }.to change(Vote, :count).by(0)
       end
     end
   end
