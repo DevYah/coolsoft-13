68674d838fec1a1642d649f9f4a5250948b8f9d8
Marwa Mehanna <marwa.mehana@gmail.com>
2013-04-11 16:44:55 +0200

    Revert "Merge branch 'C2_abuali_tag_autocomplete' into C3_Marwa_39_SubmitEditIdea"
    This reverts commit 1aef6be70e602ea6ed90f04785135ab608156fb6, reversing
    changes made to 3f2b1e0eaf74e8e004040fce22cb64313eb2aed8.

--- Idearator/config/routes.rb
+++ Idearator/config/routes.rb
@@ -1,4 +1,5 @@
 Sprint0::Application.routes.draw do
+
   default_url_options :host => "localhost:3000"
   devise_for :users, :controllers => { :registrations => "registrations" }
   resources :ideas, :controller =>'ideas'
@@ -7,8 +8,8 @@ Sprint0::Application.routes.draw do
   match '/home/index' => 'home#index'
   match '/ideas/update' => 'ideas#update'
   devise_for :committees, :controllers => { :registrations => "registrations" }
-match '/ideas/filter' => 'ideas#filter'
-get '/tags/ajax'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
