5ef74236bc8475513c4c8e04f70ddb8bcafd7dce
mohammad-abdulkhaliq <mohammad28march1993@gmail.com>
2013-04-11 17:52:04 +0200

    Issue #96 fixed indentation and tests

--- Idearator/spec/controllers/users_controller_spec.rb
+++ Idearator/spec/controllers/users_controller_spec.rb
@@ -1,46 +1,56 @@
-require 'rspec-helper'
+require 'spec_helper'
 
 describe UsersController do 
 
 	describe "approve-committee" do
-		a = Admin.new
-		a.email = 'admin@gmail.com'
-		a.password = '123123123'
-		a.username = 'admin'
-		a.confirm!
-		a.save
-		c = Comittee.new
-		c.email = 'mohammad28march1993@gmail.com'
-		c.password = '123123123'
-		c.username = 'c'
-		c.confirm!
-		c.save
-		it 'it sets the approved value of committee to approved'
-		put :approve_committee, :id => c.id
-		c.reload
-		expect(c.approved).to eq(true)
-		it 'redirects to admins index page'
-		response.should redirect_to("admins/index")
+		before :each do
+			a = Admin.new
+			a.email = 'admin@gmail.com'
+			a.password = '123123123'
+			a.username = 'admin'
+			a.confirm!
+			a.save
+			c = Committee.new
+			c.email = 'mohammad28march1993@gmail.com'
+			c.password = '123123123'
+			c.username = 'c'
+			c.confirm!
+			c.save
+		end
+		it 'it sets the approved value of committee to approved' do
+			put :approve_committee, :id => c.id
+			c.reload
+			expect(c.approved).to eq(true)
+		end
+		it 'redirects to admins index page' do
+			put :approve_committee, :id => c.id
+			response.should redirect_to("admins/index")
+		end
 	end
 
 	describe "reject_committee" do
+		before :each do
 			a = Admin.new
-		a.email = 'admin@gmail.com'
-		a.password = '123123123'
-		a.username = 'admin'
-		a.confirm!
-		a.save
-		c = Comittee.new
-		c.email = 'mohammad28march1993@gmail.com'
-		c.password = '123123123'
-		c.username = 'c'
-		c.confirm!
-		c.save
-		it 'it sets the type of user to nil hence revoking his status as Comittee'
-		put :reject_committee, :id => c.id
-		c.reload
-		expect(c.type).to eq(nil)
-		it 'redirects to admins index page'
-		response.should redirect_to("admins/index")
+			a.email = 'admin@gmail.com'
+			a.password = '123123123'
+			a.username = 'admin'
+			a.confirm!
+			a.save
+			c = Committee.new
+			c.email = 'mohammad28march1993@gmail.com'
+			c.password = '123123123'
+			c.username = 'c'
+			c.confirm!
+			c.save
+		end
+		it 'it sets the type of user to nil hence revoking his status as Comittee' do
+			put :reject_committee, :id => c.id
+			c.reload
+			expect(c.type).to eq(nil)
+		end
+		it 'redirects to admins index page' do
+			put :reject_committee, :id => c.id
+			response.should redirect_to("admins/index")
+		end
 	end
 end
