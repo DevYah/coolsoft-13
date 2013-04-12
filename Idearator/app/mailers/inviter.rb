a68d15bbd036e879f36c4d1ffbad772caa744909
mohammad-abdulkhaliq <mohammad28march1993@gmail.com>
2013-04-12 17:16:23 +0200

    Issue #96 registration integration, notifications and scenario fixes
    Moved to a new branch

--- Idearator/app/mailers/inviter.rb
+++ Idearator/app/mailers/inviter.rb
@@ -11,27 +11,6 @@ class Inviter < ActionMailer::Base
     @address = 'idearator'
     return mail(:to => email, :subject => "Welcome to My Awesome Site")
   end
-	# Sends mail confirming registration
-	# 
-	# Params: 
-	# +email+:: the parameter is an instance of +User.email+ passed through the button_to Approve Committee
-	# Author: Mohammad Abdulkhaliq
-  def committee_accept(email)
-    @email = email
-    @type = 'committee'
-    @address = 'idearator'
-    mail(:to => email, :subject => "Congratulations!")
-  end
-	# Sends mail rejecting registration
-	# 
-	# Params: 
-	# +email+:: the parameter is an instance of +User.email+ passed through the button_to Approve Committee
-	# Author: Mohammad Abdulkhaliq
-  def committee_reject(email)
-    @email = email
-    @type = 'user'
-    @address = 'idearator'
-    mail(:to => email, :subject => "Idearator")
-  end
+
 
 end
