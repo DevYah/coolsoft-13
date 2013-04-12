f853737a668dd6a4fc7954128c5e4868bc306b08
Lina B. Elakabawy <linabbasheer@gmail.com>
2013-04-12 15:05:14 +0200

    Issue #92,Merged dashboard tag

--- Idearator/spec/controllers/dashboard_controller_spec.rb
+++ Idearator/spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-
+include Devise::TestHelpers
 describe DashboardController do
   describe 'GET #gettags' do
     it 'populates one tags' do
@@ -59,38 +59,39 @@ describe DashboardController do
       assigns(:ideatags).size.should eq(0)
     end
   end
-end
-  #include Devise::TestHelpers
 
-  #describe 'GET #graph' do
-  #  it 'returns chart  ' do
-   #   l=User.new
-    #  l.email = "lina@gmail.com"
-     # l.password = "123123123"
-      #l.first_name = "lina"
-      #l.save
-      #tag = Tag.new
-      #tag.name = "tag"
-      #tag.save
-      #sign_in l
 
-      #20.times do
-       # i = Idea.new
-       # i.user_id = l.id
-       # i.title = Faker::Name.name
-        #i.description = Faker::Lorem.paragraph
-        #i.problem_solved = Faker::Lorem.paragraph
-        #i.approved = 'true'
-        #i.num_votes = rand(1..500)
-        #i.save
+  
+
+  describe 'GET #getideas' do
+    it 'returns chart  ' do
+      l=User.new
+      l.email = "lina@gmail.com"
+      l.password = "123123123"
+      l.first_name = "lina"
+      l.save
+      tag = Tag.new
+      tag.name = "tag"
+      tag.save
+      sign_in l
+
+      20.times do
+        i = Idea.new
+        i.user_id = l.id
+        i.title = Faker::Name.name
+        i.description = Faker::Lorem.paragraph
+        i.problem_solved = Faker::Lorem.paragraph
+        i.approved = 'true'
+        i.num_votes = rand(1..500)
+        i.save
       
-        #tagidea = IdeasTags.new
-        #tagidea.tag_id = tag.id
-        #tagidea.idea_id = i.id
-        #tagidea.save
-      #end 
-      #get :graph, :tagid => tag.id
-      #assigns(:no).should eq(20)
-    #end
-  #end
-#end
+        tagidea = IdeasTags.new
+        tagidea.tag_id = tag.id
+        tagidea.idea_id = i.id
+        tagidea.save
+      end 
+      get :getideas, :tagid => tag.id
+      assigns(:no).should eq(20)
+    end
+  end
+end
