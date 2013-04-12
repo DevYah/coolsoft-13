70494d06946532669febdc6c8d34309eb819da20
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-04-12 23:38:03 +0200

    Issue #80, edited tests

--- Idearator/spec/controllers/dashboard_controller_spec.rb
+++ Idearator/spec/controllers/dashboard_controller_spec.rb
@@ -1,47 +1,90 @@
-Require 'spec_helper'
+require 'spec_helper'
 
 describe DashboardController do
   include Devise::TestHelpers
 
-    context 'check if signed in' do
-      before :each do
-        sign_in @user
-      end
-
     describe 'Get index' do
 
-      it 'gets approved ideas for the committee member' do
-        user = Committee.create(email: "a@a.com", password: "123123123")
+      it 'gets own ideas for the current user' do
+        user = User.create(email: "ae@a.com", password: "123123123")
         user.confirm!
+        user.save
+        sign_in user
         2.times do 
           idea = Idea.create(title: "title", description: "description", problem_solved: "problem_solved")
-          idea.committee = user
+          idea.approved = true
+          idea.user = user
           idea.save
+          v = VoteCount.new
+          v.idea = idea
+          v.save 
         end
-        assigns(:approved_ideas).should have(2).items
+          t = Threshold.new
+          t.threshold = 120
+          t.save
+        get :index
+        assigns(:own_ideas).should have(2).items
       end
 
-      it 'gets own ideas for the current user' do
-        user = User.create(email: "a@a.com", password: "123123123")
+      it 'gets approved ideas for the committee member' do
+        user = Committee.create(email: "ae@a.com", password: "123123123")
         user.confirm!
+        user.save
+        sign_in user
         2.times do 
           idea = Idea.create(title: "title", description: "description", problem_solved: "problem_solved")
+          idea.approved = true
           idea.committee = user
           idea.save
+          v = VoteCount.new
+          v.idea = idea
+          v.save 
         end
+          t = Threshold.new
+          t.threshold = 120
+          t.save
+        get :index
         assigns(:approved_ideas).should have(2).items
       end
       it 'gets current threshold' do
-        threshold = Threshold.create(threshold: 150)
-        assigns(:threshold).should eq(150)
-      end
-      it 'gets previous day vote count of ideas' do
+        user = Committee.create(email: "ae@a.com", password: "123123123")
+        user.confirm!
+        user.save
+        sign_in user
         2.times do 
           idea = Idea.create(title: "title", description: "description", problem_solved: "problem_solved")
-          idea.user = @user
-          vote_count = VoteCount.create(idea_id: idea.id)
+          idea.approved = true
+          idea.committee = user
+          idea.save
+          v = VoteCount.new
+          v.idea = idea
+          v.save 
         end
-        assigns(:own_ideas).should have(2).items
+          t = Threshold.new
+          t.threshold = 120
+          t.save
+        get :index
+        assigns(:threshold).threshold.should eq(120)
+      end
+      it 'gets previous day vote count of ideas' do
+        user = User.create(email: "ae@a.com", password: "123123123")
+        user.confirm!
+        user.save
+        sign_in user
+        2.times do 
+          idea = Idea.create(title: "title", description: "description", problem_solved: "problem_solved")
+          idea.approved = true
+          idea.user = user
+          idea.save
+          v = VoteCount.new
+          v.idea = idea
+          v.save 
+        end
+          t = Threshold.new
+          t.threshold = 120
+          t.save
+        get :index
+        assigns(:own_thresholds).should have(2).items
       end
   end
 end
