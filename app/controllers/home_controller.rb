898de985221f9c24e094d24679e4730caf823ea6
Hesham Nabil <heshamnabil13@gmail.com>
2013-04-12 16:14:31 +0200

    #36 , fixed indentation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,18 @@
 class HomeController < ApplicationController
-	#returns a list of ideas ordered by the date of creation in pages 
-	#of 10 ideas.
-	#Params:
-	#+page+:: the parameter is the page user is currently browsing.
-	#Author: Hesham Nabil
-	def index
+    #returns a list of ideas ordered by the date of creation in pages 
+    #of 10 ideas.
+    #Params:
+    #+page+:: the parameter is the page user is currently browsing.
+    #Author: Hesham Nabil
+    def index
         @approved = Idea.order(:created_at).page(params[:page]).per(10)
         @top= Idea.find(:all,:order=> "num_votes DESC",:limit=>10)
         
         respond_to do |format|
-   		 	format.js
-    		format.html # index.html.erb
-    		format.xml  { render :xml => @approved }
-    	end	
+            format.js
+            format.html # index.html.erb
+            format.xml  { render :xml => @approved }
+        end
     end
 
 end
