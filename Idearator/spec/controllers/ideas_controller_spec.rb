54cab70d68e273d16b27b5fa9c3295ed29c4ce25
Dayna161193 <daynahany@gmail.com>
2013-04-12 16:57:31 +0200

    Test Add

--- Idearator/spec/controllers/ideas_controller_spec.rb
+++ Idearator/spec/controllers/ideas_controller_spec.rb
@@ -14,13 +14,17 @@ describe IdeasController do
         @comment = Comment.new
         @comment.content = "dayna" 
         @comment.idea_id = idea.id
+        @comment.num_likes = 0
         @comment.save
          @like = Like.new
          @like.user_id = @user.id
         @like.comment_id = @comment.id
         @like.save
-        @comment.num_likes = @comment.num_likes + 1
-        get :show ,:idea_id  => idea.id , :id => @comment.id 
+        sign_in @user
+        get :show , :id => idea.id , :commentid => @comment.id
+        @comment.reload
+        @comment.num_likes.should eq(1)
+
    end
  end
 
