e3d31d239481d52de7a2a01f50b130301898bfdf
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-12 12:07:18 +0200

    Issue #73 Fix tests

--- Idearator/spec/models/invite_committee_notification_spec.rb
+++ Idearator/spec/models/invite_committee_notification_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe InviteCommitteeNotification do
   it 'adds invite committee notification to idea_notifications table'do
-    user1 = User.create(email: 'amina@gmail.com')
-    user2 = User.create(email: 'marwa@gmail.com')
+    user1 = User.create(email: 'amina@gmail.com', password: '123123123', username: 'amina')
+    user2 = User.create(email: 'marwa@gmail.com', password: '123123123', username: 'marwa')
     InviteCommitteeNotification.send_notification(user1, [user2])
     expect(UserNotification.last.type).to eq('InviteCommitteeNotification')
     expect(UserNotification.last.user_id).to eq(user1.id)
+    expect(UserNotification.last.users).to include(user2)
   end
 end
