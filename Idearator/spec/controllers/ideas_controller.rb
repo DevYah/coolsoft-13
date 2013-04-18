ccec94ddbf9e6693d74ddefd40cdac4a0881682c
muhammedabuali <muhammedabuali@gmail.com>
2013-04-19 00:22:18 +0200

    some cleanup

--- Idearator/spec/controllers/ideas_controller.rb
+++ Idearator/spec/controllers/ideas_controller.rb
@@ -7,7 +7,6 @@ describe IdeasController do
     # tests multitag filtering
     # Author: muhammed hassan
     it 'filters on multiple tags' do
-
       i = Idea.new
       i.user_id = rand(1..40)
       i.title = Faker::Name.name
@@ -16,7 +15,6 @@ describe IdeasController do
       i.approved = 'true'
       i.num_votes = rand(1..500)
       i.save
-
       i2 = Idea.new
       i2.user_id = rand(1..40)
       i2.title = Faker::Name.name
@@ -25,7 +23,6 @@ describe IdeasController do
       i2.approved = 'true'
       i2.num_votes = rand(1..500)
       i2.save
-
       i3 = Idea.new
       i3.user_id = rand(1..40)
       i3.title = Faker::Name.name
@@ -34,25 +31,20 @@ describe IdeasController do
       i3.approved = 'true'
       i3.num_votes = rand(1..500)
       i3.save
-
       t = Tag.new
       t.name = Faker::Name.name
       t.save
-
       t2 = Tag.new
       t2.name = Faker::Name.name
       t2.save
-
       it = IdeasTags.new
       it.idea_id = 1
       it.tag_id = 1
       it.save
-
       it2 = IdeasTags.new
       it2.idea_id = 2
       it2.tag_id = 2
       it2.save
-
       post :filter, :myTags => [t.name, t2.name]
       assigns(:approved).should have(2).items
     end
@@ -68,25 +60,20 @@ describe IdeasController do
       i.approved = 'true'
       i.num_votes = rand(1..500)
       i.save
-
       t = Tag.new
       t.name = Faker::Name.name
       t.save
-
       it = IdeasTags.new
       it.idea_id = 1
       it.tag_id = 1
       it.save
-
       post :filter, :myTags => [t.name] , :format => :json
       response.body.should == assigns(:approved).to_json
-
     end
 
     # tests rendered js file
     # Author: muhammed hassan
     it 'renders js file' do
-
       i = Idea.new
       i.user_id = rand(1..40)
       i.title = Faker::Name.name
@@ -95,18 +82,17 @@ describe IdeasController do
       i.approved = 'true'
       i.num_votes = rand(1..500)
       i.save
-
       t = Tag.new
       t.name = Faker::Name.name
       t.save
-
       it = IdeasTags.new
       it.idea_id = 1
       it.tag_id = 1
       it.save
-
       post :filter, :myTags => [t.name] , :format => :js
       response.should be_success
     end
+
   end
+
 end
--- Idearator/spec/views/ideas/_show.html.erb_spec.rb
+++ Idearator/spec/views/ideas/_show.html.erb_spec.rb
@@ -2,16 +2,14 @@ require "spec_helper"
 
 # tests partial  contains auto complete form
 # Author: muhammed hassan
-  describe "ideas/_show.html.erb" do
-    it "display auto complete form" do
+describe "ideas/_show.html.erb" do
 
-      render :partial => "ideas/show.html.erb"
+  it "display auto complete form" do
+    render :partial => "ideas/show.html.erb"
 
-      rendered.should include("id=\"input-facebook-theme\"")
-      rendered.should include("id=\"filter\"")
-    end
+    rendered.should include("id=\"input-facebook-theme\"")
+    rendered.should include("id=\"filter\"")
   end
 
-
-
+end
 
