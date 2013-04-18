df86416150806ddc170f11cd049b1ce0bf3394a5
Marwa Mehanna <marwa.mehana@gmail.com>
2013-04-18 22:51:53 +0200

    Cleaned up spec/controllers/ideas_controller.rb

--- spec/controllers/ideas_controller.rb
+++ spec/controllers/ideas_controller.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe IdeasController do
 
-  describe "Post Filter" do
+  describe 'Post Filter' do
 
-# tests multitag filtering
-# Author: muhammed hassan
-    it "filters on multiple tags" do
+    # tests multitag filtering
+    # Author: muhammed hassan
+    it 'filters on multiple tags' do
 
       i = Idea.new
       i.user_id = rand(1..40)
       i.title = Faker::Name.name
       i.description = Faker::Lorem.paragraph
       i.problem_solved = Faker::Lorem.paragraph
-      i.approved = "true"
+      i.approved = 'true'
       i.num_votes = rand(1..500)
       i.save
 
@@ -22,7 +22,7 @@ describe IdeasController do
       i2.title = Faker::Name.name
       i2.description = Faker::Lorem.paragraph
       i2.problem_solved = Faker::Lorem.paragraph
-      i2.approved = "true"
+      i2.approved = 'true'
       i2.num_votes = rand(1..500)
       i2.save
 
@@ -31,7 +31,7 @@ describe IdeasController do
       i3.title = Faker::Name.name
       i3.description = Faker::Lorem.paragraph
       i3.problem_solved = Faker::Lorem.paragraph
-      i3.approved = "true"
+      i3.approved = 'true'
       i3.num_votes = rand(1..500)
       i3.save
 
@@ -42,30 +42,30 @@ describe IdeasController do
       t2 = Tag.new
       t2.name = Faker::Name.name
       t2.save
-      
+
       it = IdeasTags.new
       it.idea_id = 1
       it.tag_id = 1
       it.save
-      
+
       it2 = IdeasTags.new
       it2.idea_id = 2
       it2.tag_id = 2
       it2.save
 
-      post :filter, :myTags => [t.name,t2.name] 
+      post :filter, :myTags => [t.name, t2.name]
       assigns(:approved).should have(2).items
     end
 
-# tests rendered json
-# Author: muhammed hassan
-    it "renders ideas as json" do
+    # tests rendered json
+    # Author: muhammed hassan
+    it 'renders ideas as json' do
       i = Idea.new
       i.user_id = rand(1..40)
       i.title = Faker::Name.name
       i.description = Faker::Lorem.paragraph
       i.problem_solved = Faker::Lorem.paragraph
-      i.approved = "true"
+      i.approved = 'true'
       i.num_votes = rand(1..500)
       i.save
 
@@ -77,22 +77,22 @@ describe IdeasController do
       it.idea_id = 1
       it.tag_id = 1
       it.save
-      
+
       post :filter, :myTags => [t.name] , :format => :json
       response.body.should == assigns(:approved).to_json
 
     end
 
-# tests rendered js file
-# Author: muhammed hassan
-    it "renders js file" do
+    # tests rendered js file
+    # Author: muhammed hassan
+    it 'renders js file' do
 
       i = Idea.new
       i.user_id = rand(1..40)
       i.title = Faker::Name.name
       i.description = Faker::Lorem.paragraph
       i.problem_solved = Faker::Lorem.paragraph
-      i.approved = "true"
+      i.approved = 'true'
       i.num_votes = rand(1..500)
       i.save
 
@@ -104,8 +104,8 @@ describe IdeasController do
       it.idea_id = 1
       it.tag_id = 1
       it.save
-      
-      post :filter, :myTags => [t.name] , :format => :js 
+
+      post :filter, :myTags => [t.name] , :format => :js
       response.should be_success
     end
   end
