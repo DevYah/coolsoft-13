505f36f232375118a25c4df06c9fe48a07511a37
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-19 17:25:03 +0200

    Issue #90 Fix test files

--- Idearator/spec/controllers/ideas_controller_spec.rb
+++ Idearator/spec/controllers/ideas_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe IdeasController do
   describe 'PUT archive' do
     include Devise::TestHelpers
-    
+
     context 'idea creator wants to archive' do
       before :each do
         @user = FactoryGirl.build(:user)
@@ -23,23 +23,18 @@ describe IdeasController do
       end
 
       it 'archives the idea' do
-        put :archive, :id => @idea.id
+        put :archive, :id => @idea.id, :format => 'js'
         @idea.reload
         (@idea.archive_status).should eql(true)
       end
 
-      it 'redirects to idea' do
-        put :archive, :id => @idea.id
-        response.should redirect_to @idea
-      end
-
       it 'deletes idea comments' do
-        expect { put :archive, :id => @idea.id }.to change(Comment, :count).by(-1)
+        expect { put :archive, :id => @idea.id, :format => 'js' }.to change(Comment, :count).by(-1)
       end
 
       it 'deletes idea votes' do
-        expect { put :archive, :id => @idea.id }.to change(Vote, :count).by(-1)
-      end      
+        expect { put :archive, :id => @idea.id, :format => 'js' }.to change(Vote, :count).by(-1)
+      end
     end
 
     context 'admin wants to archive' do
@@ -63,22 +58,17 @@ describe IdeasController do
       end
 
       it 'archives the idea' do
-        put :archive, :id => @idea.id
+        put :archive, :id => @idea.id, :format => 'js'
         @idea.reload
         (@idea.archive_status).should eql(true)
       end
 
-      it 'redirects to idea' do
-        put :archive, :id => @idea.id
-        response.should redirect_to @idea
-      end
-
       it 'deletes idea comments' do
-        expect { put :archive, :id => @idea.id }.to change(Comment, :count).by(-1)
+        expect { put :archive, :id => @idea.id, :format => 'js' }.to change(Comment, :count).by(-1)
       end
 
       it 'deletes idea votes' do
-        expect { put :archive, :id => @idea.id }.to change(Vote, :count).by(-1)
+        expect { put :archive, :id => @idea.id, :format => 'js' }.to change(Vote, :count).by(-1)
       end
     end
 
@@ -92,29 +82,24 @@ describe IdeasController do
 
       it 'does not archive the idea' do
         @arch_stat = @idea.archive_status
-        put :archive, :id => @idea.id
+        put :archive, :id => @idea.id, :format => 'js'
         @idea.reload
         (@idea.archive_status).should eql(@arch_stat)
       end
 
-      it 'redirects to idea' do
-        put :archive, :id => @idea.id
-        response.should redirect_to @idea
-      end
-
       it 'does not delete idea votes' do
-        expect { put :archive, :id => @idea.id }.to change(Vote, :count).by(0)
+        expect { put :archive, :id => @idea.id, :format => 'js' }.to change(Vote, :count).by(0)
       end
 
       it 'does not delete idea comments' do
-        expect { put :archive, :id => @idea.id }.to change(Comment, :count).by(0)
+        expect { put :archive, :id => @idea.id, :format => 'js' }.to change(Comment, :count).by(0)
       end
     end
   end
 
   describe 'PUT unarchive' do
     include Devise::TestHelpers
-    
+
     context 'idea creator wants to unarchive' do
       before :each do
         @user = FactoryGirl.build(:user)
@@ -126,14 +111,9 @@ describe IdeasController do
       end
 
       it 'unarchives the idea' do
-        put :unarchive, :id => @idea.id
+        put :unarchive, :id => @idea.id, :format => 'js'
         @idea.reload
         (@idea.attributes['archive_status']).should eql(false)
-      end
-
-      it 'redirects to idea' do
-        put :unarchive, :id => @idea.id
-        response.should redirect_to @idea
       end
     end
 
@@ -146,14 +126,9 @@ describe IdeasController do
       end
 
       it 'unarchives the idea' do
-        put :unarchive, :id => @idea.id
+        put :unarchive, :id => @idea.id, :format => 'js'
         @idea.reload
         (@idea.archive_status).should eql(false)
-      end
-
-      it 'redirects to idea' do
-        put :unarchive, :id => @idea.id
-        response.should redirect_to @idea
       end
     end
 
@@ -167,14 +142,9 @@ describe IdeasController do
 
       it 'does not unarchive the idea' do
         @arch_stat = @idea.archive_status
-        put :unarchive, :id => @idea.id
+        put :unarchive, :id => @idea.id, :format => 'js'
         @idea.reload
         (@idea.archive_status).should eql(@arch_stat)
-      end
-
-      it 'redirects to idea' do
-        put :unarchive, :id => @idea.id
-        response.should redirect_to @idea
       end
     end
   end
