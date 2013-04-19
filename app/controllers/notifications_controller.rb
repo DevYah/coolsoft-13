4457d93de996790cdd28a7b5e30c17145375c9fc
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-19 03:49:29 +0200

    Issue #73 Add view_new_notifications method and update polling to get only new notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,14 @@
 class NotificationsController < ApplicationController
   before_filter :authenticate_user!, :only => [:view_all_notifications]
 
+  def view_new_notifications
+    idea_notifications = IdeaNotification.joins(:idea_notifications_users).where('idea_notifications_users.user_id = ? and created_at > ?', current_user, Time.at(params[:after].to_i + 1))
+    user_notifications = UserNotification.joins(:user_notifications_users).where('user_notifications_users.user_id = ? and created_at > ?', current_user, Time.at(params[:after].to_i + 1))
+    notifications = idea_notifications + user_notifications
+    sorted_notifications = notifications.sort_by &:created_at
+    @new_notifications = sorted_notifications.reverse
+  end
+
   # gets all current users notifications.
   # Params: none.
   # Author: Amina Zoheir
