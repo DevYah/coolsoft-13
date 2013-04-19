e16e6eebfbae5cd03732e3429ba52a46841a7cf4
Dayna161193 <daynahany@gmail.com>
2013-04-19 14:19:02 +0200

    Add controllers

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,37 +1,79 @@
 class IdeasController < ApplicationController
-  before_filter :authenticate_user!, :only => [:show, :create, :edit, :update]
+  before_filter :authenticate_user!, :only => [:show, :create , :edit, :update]
   # view idea of current user
   # Params
   # +id+:: is passed in params through the new idea view, it is used to identify the instance of +Idea+ to be viewed
   # Marwa Mehanna
+  def show
+    @user = current_user.id
+    @username = current_user.username
+    @idea = Idea.find(params[:id])
+    @likes = Like.find(:all, :conditions => {:user_id => current_user.id})
 
+    rescue ActiveRecord::RecordNotFound
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @idea }
+    end
+  end
 
-def show
-  @user=current_user.id
-  @idea = Idea.find(params[:id])
-  @likes = Like.find(:all, :conditions => {:user_id => current_user.id})
-end
   # making new Idea
   #Marwa Mehanna
   def new
-    @idea=Idea.new
-    @tags= Tag.all
-    @chosentags=[]
+    @idea = Idea.new
+    @tags = Tag.all
+    @chosentags = []
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @idea }
     end
   end
+
+# filters the ideas that have one or more of given tags
+# Params:
+# +tags:: the parameter is an list of +Tag+ passed through tag autocomplete field
+# Author: muhammed hassan
+  def filter()
+    @approved = Idea.joins(:tags).where(:tags => {:name => params[:myTags]}).uniq.page(params[:page]).per(10)
+    @tags = params[:myTags]
+    respond_to do |format|
+      format.js
+      format.json  { render :json => @approved }
+    end
+  end
+
   # editing Idea
   # Params
   # +id+ :: this is an instance of +Idea+ passed through _form.html.erb, used to identify which +Idea+ to edit
   # Author: Marwa Mehanna
   def edit
-      @idea = Idea.find(params[:id])
-      @tags = Tag.all
-      @chosentags = Idea.find(params[:id]).tags
-    end
+    @idea = Idea.find(params[:id])
+    @tags = Tag.all
+    @chosentags = Idea.find(params[:id]).tags
+    @boolean = true
+    @ideavoters = @idea.votes
+    @ideacommenters = @idea.comments
+    @userVreceivers = []
+    @userCreceivers = []
+    @ideavoters.each {|user|
+       if user.participated_idea_notifications
+        @userVreceivers << user 
+      end }
+    
+    
+      @ideacommenters.each {|user|
+       if user.participated_idea_notifications
+        @userCreceivers << user 
+      end}
+    EditNotification.send_notification(current_user,@idea,@userVreceivers)
+    EditNotification.send_notification(current_user,@idea,@userCreceivers)
+ end
 
+  # updating Idea
+  # Params
+  # +ideas_tags:: this is an instance of +IdeasTag+ passed through _form.html.erb, this is where +tags+ will be added
+  # +tags+ :: this is an instance of +Tags+ passed through _form.html.erb, used to identify which +Tags+ to add
+  # Author: Marwa Mehanna
   def update
     @idea = Idea.find(params[:id])
     puts(params[:ideas_tags][:tags])
@@ -45,9 +87,8 @@ end
         format.json { render json: @idea.errors, status: :unprocessable_entity }
       end
     end
-  end 
+  end
 
-  
   # creating new Idea
   # Params
   # +idea+ :: this is an instance of +Idea+ passed through _form.html.erb, identifying the idea which will be added to records
@@ -71,6 +112,7 @@ end
       end
     end
   end
+
   #create new like
   #Params:
   #+comment_id+ :: the parameter is an instance   
@@ -95,10 +137,44 @@ end
         respond_to do|format|
           format.js
         end
+      end 
       else
         redirect_to @idea , :notice => "This comment was removed by the user"
+    end 
+  end 
+
+  # Deletes all records related to a specific idea
+  # Params:
+  # +id+:: is used to specify which instance of +Idea+ will be deleted
+  # Author: Mahmoud Abdelghany Hashish
+  def destroy
+    idea = Idea.find(params[:id])
+
+    if current_user.id == idea.user_id
+      list_of_comments = Comment.where(idea_id: idea.id)
+      list_of_commenters = []
+      list_of_voters = idea.votes
+      idea.destroy
+
+      list_of_comments.each do |c|
+        c.destroy
+      end
+
+      list_of_comments.each do |c|
+        list_of_commenters.append(User.find(c.user_id)).flatten!
+      end
+
+      list = list_of_commenters.append(list_of_voters).flatten!
+
+      DeleteNotification.send_notification(current_user, idea, list)
+
+      respond_to do |format|
+        format.html { redirect_to '/', alert: 'Your Idea has been successfully deleted!' }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to idea, alert: 'You do not own the idea, so it cannot be deleted!' }
       end
     end
   end
 end
-
