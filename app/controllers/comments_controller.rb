e39d1c72d97feffff6f94e62f9cde72bc9af5302
Dayna161193 <daynahany@gmail.com>
2013-04-20 14:55:43 +0200

    Comments Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,48 +1,48 @@
 class CommentsController < ApplicationController
-respond_to :html, :json
+  respond_to :html, :json
 
-#Show all Comments
-#Params:
-#+idea_id+ :: the parameter is an instance of +Idea+
-#passed through the form of action create
-#author dayna
-def show
-  @user = current_user.id
-  @username = current_user.username
-  @idea = Idea.find(params[:id])
-end
+  #Show all Comments
+  #Params:
+  #+idea_id+ :: the parameter is an instance of +Idea+
+  #passed through the form of action create
+  #author dayna
+  def show
+    @user = current_user.id
+    @username = current_user.username
+    @idea = Idea.find(params[:id])
+  end
 
-#create new Comment
-#Params:
-#+idea_id+ :: the parameter is an instance
-#of +Idea+ passed to get the id of the idea to build the comments
-#+comment_id+ :: the parameter is an instance
-# of +Comment+ and it's used to show the comments after posting it
-#author dayna
-def create
-  @idea = Idea.find(params[:idea_id])
-  @comment = @idea.comments.create(params[:comment])
-  @comment.update_attributes(:idea_id => @idea.id)
-  respond_to do |format|
-   if @comment.save
-     format.html { redirect_to(@idea, :notice => 'Comment was successfully created.') }
-     format.xml  { render :xml => @idea, :status => :created, :location => @idea }
-  else
-     format.html { redirect_to(@idea, :notice => 'Comment could not be saved. Please fill in all fields') }
-      format.xml { render :xml => @comment.errors, :status => :unprocessable_entity }
-    end
-   end
-end
+  #create new Comment
+  #Params:
+  #+idea_id+ :: the parameter is an instance
+  #of +Idea+ passed to get the id of the idea to build the comments
+  #+comment_id+ :: the parameter is an instance
+  # of +Comment+ and it's used to show the comments after posting it
+  #author dayna
+  def create
+    @idea = Idea.find(params[:idea_id])
+    @comment = @idea.comments.create(params[:comment])
+    @comment.update_attributes(:idea_id => @idea.id)
+    respond_to do |format|
+     if @comment.save
+       format.html { redirect_to(@idea, :notice => 'Comment was successfully created.') }
+       format.xml  { render :xml => @idea, :status => :created, :location => @idea }
+    else
+       format.html { redirect_to(@idea, :notice => 'Comment could not be saved. Please fill in all fields') }
+        format.xml { render :xml => @comment.errors, :status => :unprocessable_entity }
+      end
+     end
+  end
 
-#edit comment and update it
-#Params:
-#+comment_id+ :: the parameter is an instance of +Comment+
-#to get the comment's id in order to modify it
-#author dayna
-def edit
-  @comment = Comment.find(params[:id])
-  @idea = Idea.find(params[:idea_id])
-end
+  #edit comment and update it
+  #Params:
+  #+comment_id+ :: the parameter is an instance of +Comment+
+  #to get the comment's id in order to modify it
+  #author dayna
+  def edit
+    @comment = Comment.find(params[:id])
+    @idea = Idea.find(params[:idea_id])
+  end
 
 def update
   @comment = Comment.find(params[:id])
@@ -69,6 +69,8 @@ def update
     @idea = Idea.find(params[:idea_id])
     @comment = @idea.comments.find(params[:id])
     @comment.destroy
-    redirect_to idea_path(@idea)
+      respond_to do|format|
+        format.js
+      end
   end
 end
