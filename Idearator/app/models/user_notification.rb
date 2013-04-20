28b57d5ddb93d70cf5150027bcc4b078f18eea71
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-20 19:51:30 +0200

    Issue #73 Add text, read_by?, read_for in idea/user notification models

--- Idearator/app/models/user_notification.rb
+++ Idearator/app/models/user_notification.rb
@@ -8,4 +8,21 @@ class UserNotification < ActiveRecord::Base
   def self.send_notification(user_sender, idea, users_receivers)
   end
 
+  def text
+  end
+
+  def read_by?(user)
+    if UserNotificationsUser.find(:first, :conditions => {user_notification_id: self.id, user_id: user.id }).read
+      true
+    else
+      false
+    end
+  end
+
+  def read_for(user)
+    notification = UserNotificationsUser.find(:first, :conditions => {user_notification_id: self.id, user_id: user.id })
+    notification.read = true
+    notification.save
+  end
+
 end
