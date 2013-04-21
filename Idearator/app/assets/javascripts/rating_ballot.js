bf793ca4a4a46ace1bf29be3c5a2a9e31544a2a0
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-21 15:24:14 +0200

    Issue #89 Fix rating_ballot.js asset

--- Idearator/app/assets/javascripts/rating_ballot.js
+++ Idearator/app/assets/javascripts/rating_ballot.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     // Submits the form (saves data) after user makes a change.
-    $('form.rating_ballot').change(function() {
-        $('form.rating_ballot').submit();
+    $('input.rating_button').rating({
+      callback: function(value, link){
+        this.form.submit();
+      }
     });
+
+    // Removes Cancel Rating Button
+    $('.rating-cancel').remove();
 });
