3dda03ca1c535bce376eeb0e1e2f6588b696c0b4
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-21 15:56:23 +0200

    Issue #73 Change redirecting to get_script
    before redirecting pass by methods in notification controller to set read to true

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,6 +1,6 @@
 // When The user clicks on facebook share or twitter share button, this method gets the current URL of the current page and apends it to the default
-// facebook and twitter sharing URLs.  
-// This page's URl is then shared on The user's facebook or twitter account. 
+// facebook and twitter sharing URLs.
+// This page's URl is then shared on The user's facebook or twitter account.
 // Author: Mohamed Sameh
 $(document).ready(function() {
 	$("#share").click(function() {
@@ -24,6 +24,43 @@ $(document).ready(function() {
 	});
 	$("#tw").tooltip({
 		toggle: "tooltip",
-		title: "Share on Twitter" 
+		title: "Share on Twitter"
 	});
+});
+
+$(document).ready(function() {
+  var prePopulate = [];
+
+  $("#idea-tags .idea-tag input:checked").each(function(i, checkbox) {
+    checkbox = $(checkbox);
+    prePopulate.push({id: checkbox.val(), name: checkbox.data("tag-name")});
+  });
+
+  $("#idea-tags").html('<input type="text" id="tag_token_input" name="blah2" />')
+
+  $("#tag_token_input").tokenInput('/tags/ajax', {
+    theme: "facebook",
+    preventDuplicates: true,
+    tokenLimit: 5,
+    tokenFormatter: function(item){
+      return "<li>" + item.name
+           + "<input id='ideas_tags_tags_' type='hidden' value='" + item.id + "' name='idea[tag_ids][]' />"
+           + "</li>";
+      //"<li>" + "<img src='" + item.url + "' title='"
+      //+ item.first_name + " " + item.last_name + "' height='25px' width='25px' />"
+      //+ "<div style='display: inline-block; padding-left: 10px;'><div class='full_name'>"
+      //+ item.first_name + " " + item.last_name + "</div><div class='email'>" + item.email
+      //+ "</div></div></li>";
+    },
+    prePopulate: prePopulate
+  });
+
+
+  $('.best_in_place').best_in_place();
+  $('#image-edit-link,.best_in_place').tooltip({animation: true, title:'Click to edit' , trigger: 'hover', html: true });
+
+  $('.best_in_place').bind("ajax:success", function(){
+    $('#edited-check-mark').remove();
+    $(this).append("<i class='icon-ok pull-right' id ='edited-check-mark'></i>");
+  });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,12 @@ Sprint0::Application.routes.draw do
 
   resources :ideas do
     match 'filter', on: :collection
+    member do
+      match 'vote'
+      match 'unvote'
+      match 'archive'
+      match 'unarchive'
+    end
   end
 
   controller :home do
@@ -95,6 +101,9 @@ Sprint0::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
 
   # See how all your routes lay out with "rake routes"
 
