8962dc8019a9c5bf0e2f1b357594c106a1516cc6
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-21 16:38:08 +0200

    Issue #73 re-add redirecting methods

--- Idearator/app/controllers/notifications_controller.rb
+++ Idearator/app/controllers/notifications_controller.rb
@@ -15,4 +15,53 @@ class NotificationsController < ApplicationController
   def view_all_notifications
   end
 
+  # sets the read field to true for the specified notification and redirects to ideas#show.
+  # Params:
+  # +not_id+:: the parameter is an instance of +IdeaNotification+ passed through the view_notifications view.
+  # Author: Amina Zoheir
+  def redirect_idea
+    notification = IdeaNotification.find(params[:notification])
+    notification.set_read_for current_user
+    respond_to do |format|
+      format.js { render 'redirect', locals:{path: '/ideas/' + ((notification.idea.id).to_s)} }
+      format.json { head :no_content }
+    end
+  end
+
+
+  # sets the read field to true for the specified notification and redirects to users#expertise.
+  # Params:
+  # +not_id+:: the parameter is an instance of +UserNotification+ passed through the view_notifications view.
+  # Author: Amina Zoheir
+  def redirect_expertise
+    notification = UserNotification.find(params[:notification])
+    notification.set_read_for current_user
+    respond_to do |format|
+      format.js { render 'users/send_expertise' }
+      format.json { head :no_content }
+    end
+  end
+
+
+  # sets the read field to true for the specified notification and redirects to committees#review.
+  # Params:
+  # +not_id+:: the parameter is an instance of +UserNotification+ passed through the view_notifications view.
+  # Author: Amina Zoheir
+  def redirect_review
+    notification = UserNotification.find(params[:notification])
+    notification.set_read_for current_user
+    respond_to do |format|
+      format.js { render 'redirect', locals:{path: '/users/' + (notification.user.becomes(User).id).to_s} }
+      format.json { head :no_content }
+    end
+  end
+
+  def set_read
+    notification = IdeaNotification.find(params[:notification])
+    notification.set_read_for current_user
+    respond_to do |format|
+      format.js { render 'layouts/update_nav_bar' }
+    end
+  end
+
 end
