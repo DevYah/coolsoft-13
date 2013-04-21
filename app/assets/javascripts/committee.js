b3fb7cf4ad158e42f81026facedc4b5794548cd9
Omar Kassem <omar.kassem67@gmail.com>
2013-04-21 13:21:19 +0200

    #93, added ajax to review ideas

--- app/assets/javascripts/committee.js
+++ app/assets/javascripts/committee.js
@@ -1,8 +1,11 @@
 $(window).load(function(){
-$("#rating_complete").tokenInput('/ratings/ajax', {
+$(".rating_complete").tokenInput('/ratings/ajax', {
     theme: "facebook",
     preventDuplicates: true,
     tokenLimit: 5
   });
+$('.btn-success.add-rating').click(function(){
+$('.btn-success.add-rating').parent().find('div.add-ratings').hide();
+$(this).parent().find('div.add-ratings').show();
 });
-$('#myModal').modal();
+});
