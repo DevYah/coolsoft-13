0f97ae4b73e459aa689cb980abf16fcda264db87
Dayna161193 <daynahany@gmail.com>
2013-04-22 14:32:47 +0200

    Fixed Indentation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,7 +28,7 @@ class CommentsController < ApplicationController
     if @comment.save
       respond_to do |format|
         format.js
-      end    
+      end
     else
       respond_to do |format|
         format.html { redirect_to(@idea, :notice => 'Comment could not be saved. Please fill in all fields') }
@@ -46,20 +46,20 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @idea = Idea.find(params[:idea_id])
   end
-  
- def update
-  @comment = Comment.find(params[:id])
-  @idea = Idea.find(params[:idea_id])
-  respond_to do |format|
-   if @comment.update_attributes(params[:comment])
-      format.html { redirect_to(@idea, :notice => 'Comment was successfully updated.') }
-      format.json { respond_with_bip(@comment) }
-    else
-      format.html { render :action => "edit" }
-      format.json { respond_with_bip(@comment) }
-    end  
+
+  def update
+    @comment = Comment.find(params[:id])
+    @idea = Idea.find(params[:idea_id])
+    respond_to do |format|
+      if @comment.update_attributes(params[:comment])
+        format.html { redirect_to(@idea, :notice => 'Comment was successfully updated.') }
+        format.json { respond_with_bip(@comment) }
+      else
+        format.html { render :action => "edit" }
+        format.json { respond_with_bip(@comment) }
+      end
+    end
   end
- end
 
   #delete comment
   #Params:
@@ -72,8 +72,8 @@ class CommentsController < ApplicationController
     @idea = Idea.find(params[:idea_id])
     @comment = @idea.comments.find(params[:id])
     @comment.destroy
-      respond_to do|format|
-        format.js
-      end
+    respond_to do|format|
+      format.js
+    end
   end
 end
