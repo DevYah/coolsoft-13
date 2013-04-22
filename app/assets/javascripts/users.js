6531ac8d83612d43f4bcedf5cc0c51e2b4f73270
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-04-22 22:40:36 +0200

    Issue #197, change styles and javascript related to user editing

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-   $("#submit").click(function() {
-      $('#myModal').modal('hide');
-    });
-    $("#cancel").click(function() {
-      $('#myModal').modal('hide');
-    });
-    $('.best_in_place').best_in_place()
+
+  $("#submit").click(function() {
+    $('#myModal').modal('hide');
+  });
+
+  $("#cancel").click(function() {
+    $('#myModal').modal('hide');
+  });
+
+  $('.best_in_place').best_in_place();
+
+  $.datepicker.setDefaults({ dateFormat: 'yy/mm/dd', changeMonth: true,
+      changeYear: true, yearRange: '1900:' + (new Date().getFullYear() - 8)
+  });
+
 });
