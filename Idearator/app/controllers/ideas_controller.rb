2e507d182c682911391675837c48639b4e2c8714
Marwa Mehanna <marwa.mehana@gmail.com>
2013-04-22 14:45:42 +0200

    Issue #39, correct error in vote

--- Idearator/app/controllers/ideas_controller.rb
+++ Idearator/app/controllers/ideas_controller.rb
@@ -13,7 +13,7 @@ class IdeasController < ApplicationController
       @username = current_user.username
       @tags = Tag.all
       @chosentags = Idea.find(params[:id]).tags
-      ideavoted = @current_user.votes.detect { |w|w.id == @idea.id }rescue ActiveRecord::RecordNotFound
+      @ideavoted = current_user.votes.detect { |w|w.id == @idea.id }rescue ActiveRecord::RecordNotFound
     end
     respond_to do |format|
       format.html # show.html.erb
