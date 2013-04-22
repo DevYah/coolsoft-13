8d5c02c0daf5093d4635165a62bfe41e24edc5ce
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-22 13:36:43 +0200

    Issue #90 Move rating_ballot js asset in a handler function

--- Idearator/app/assets/javascripts/rating_ballot.js
+++ Idearator/app/assets/javascripts/rating_ballot.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+function add_rating_handler() {
     // Submits the form (saves data) after user makes a change.
     $('input.rating_button').rating({
       callback: function(value, link){
@@ -8,4 +8,6 @@ $(document).ready(function() {
 
     // Removes Cancel Rating Button
     $('.rating-cancel').remove();
-});
+}
+
+$(document).ready(add_rating_handler);
