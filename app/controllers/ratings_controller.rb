958733f1e61d894a8ead8a7c15b65c9c77013fc4
Omar Kassem <omar.kassem67@gmail.com>
2013-04-24 01:53:28 +0200

    #93, fixed the drop down list of adding the rating prespectives

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -5,7 +5,7 @@ class RatingsController < ApplicationController
 # +q+:: the parameter is  a string passed through auttocomplete field
 # Author: muhammed hassan
   def ajax
-    @ratings = Rating.find(:all, conditions: ['name LIKE(?)', "%#{ params[:q] }%"]).take(5)
+    @ratings = Rating.find(:all,:select =>'DISTINCT name', conditions: ['name LIKE(?)', "%#{ params[:q] }%"]).uniq
     render :json => @ratings , only: [:name,:id]
   end
 
