6ae5ef2e557867c348258689702c0a813136e57c
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-23 22:39:20 +0200

    Add custom pre-commit checks.
    These use standard tools (grep, git diff-index, etc).
    
    Implemented checks:
      - Whitespace errors
      - Committed merge conflicts
      - Non ASCII filenames

--- Idearator/config/initializers/git_config.rb
+++ Idearator/config/initializers/git_config.rb
@@ -7,6 +7,7 @@ FileUtils.cd "#{Rails.root}/.." do
   File.open('.git/hooks/pre-commit', 'w') do |f|
     f.write <<-eos.strip_heredoc
       #!/bin/sh
+      . Idearator/script/pre-commit-checks.sh
     eos
   end
   FileUtils.chmod(0755, '.git/hooks/pre-commit')
