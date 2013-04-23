6579434df9f5fd4bd7e9c40c6832e245c4e54f9a
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-23 21:28:24 +0200

    Issue #73 Change css class names

--- Idearator/app/assets/javascripts/notifications.js
+++ Idearator/app/assets/javascripts/notifications.js
@@ -1,22 +1,22 @@
 function add_notification_event_handlers() {
-  $('div.invite-committee').click(function () {
+  $('div.invite-committee-notification').click(function () {
     var notification = $(this).data('notification');
     $.getScript("/redirect_expertise.js?&notification=" + notification);
   });
 
-  $('div.idea').click(function () {
+  $('div.idea-notification').click(function () {
     //var idea = this.getAttribute('data-idea');
     var notification = $(this).data('notification');
     $.getScript("/redirect_idea.js?&notification=" + notification);
   });
 
-  $('div.approve-committee').click(function () {
+  $('div.approve-committee-notification').click(function () {
     //var user = this.getAttribute('data-user');
     var notification = $(this).data('notification');
     $.getScript("/redirect_review.js?&notification=" + notification);
   });
 
-  $('div.delete').click(function () {
+  $('div.delete-notification').click(function () {
     var notification = $(this).data('notification');
     $.getScript("/set_read.js?&notification=" + notification);
   });
