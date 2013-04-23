fc34d2f7d2c3bc78032660c76345cc345e21c927
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-23 07:23:04 +0200

    Issue #73 fix inheritance in idea/user notifications subclasses and remove IdeaNotification inheritance from DeleteNotification

--- app/models/invite_committee_notification.rb
+++ app/models/invite_committee_notification.rb
@@ -1,5 +1,5 @@
 class InviteCommitteeNotification < UserNotification
-
+  inherits_from :notification
   def self.send_notification(user_sender, users_receivers) 
     invite_notification = InviteCommitteeNotification.create(user: user_sender, users: users_receivers)
   end
--- app/models/vote_notification.rb
+++ app/models/vote_notification.rb
@@ -1,5 +1,5 @@
 class VoteNotification < IdeaNotification
-
+  inherits_from :notification
   def self.send_notification (user_sender, idea, users_receivers) 
     vote_notification = VoteNotification.create(user: user_sender, idea: idea, users: users_receivers)
   end
