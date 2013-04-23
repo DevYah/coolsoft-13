eaca52acad65ce024075556236077430b7af6190
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-23 21:36:09 +0200

    Remove pre-commit gem.
    Too buggy. Resorting to custom shell scripts and simpler tools.

--- config/initializers/git_config.rb
+++ config/initializers/git_config.rb
@@ -3,31 +3,14 @@
 
 require 'fileutils'
 
-FileUtils.cd '..' do
-  # We install a custom hook because pre-commit is installed in the bundle
-  interp =
-    if system('which rvm')
-      'rvm default do ruby'
-    else
-      'ruby'
-    end
+FileUtils.cd "#{Rails.root}/.." do
   File.open('.git/hooks/pre-commit', 'w') do |f|
     f.write <<-eos.strip_heredoc
-      #!/usr/bin/env #{interp}
-      require 'fileutils'
-      FileUtils::cd '#{Rails.root}' do
-        require 'bundler/setup'
-        require 'pre-commit'
-        FileUtils::cd '..' do
-          PreCommit.run
-        end
-      end
+      #!/bin/sh
     eos
   end
   FileUtils.chmod(0755, '.git/hooks/pre-commit')
 
-  system('git config pre-commit.checks ' +
-         '"rubocop_all, debugger, pry, merge_conflict, white_space, tabs, console_log"')
   system('git config core.fileMode false')
   system('git config core.eol lf')
   system('git config core.autocrlf input')
