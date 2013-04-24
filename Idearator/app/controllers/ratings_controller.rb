fcf8b013c744bf1a6930fac49008ee0f8176cdca
Omar Kassem <omar.kassem67@gmail.com>
2013-04-24 16:38:56 +0200

    #93, adding new prespectives

--- Idearator/app/controllers/ratings_controller.rb
+++ Idearator/app/controllers/ratings_controller.rb
@@ -1,13 +1,13 @@
 class RatingsController < ApplicationController
 
-# auto completes rating perspectives
-# Params:
-# +q+:: the parameter is  a string passed through auttocomplete field
-# Author: muhammed hassan
+  # auto completes rating perspectives
+  # Params:
+  # +q+:: the parameter is  a string passed through auttocomplete field
+  # Author: muhammed hassan
   def ajax
-    @ratings = Rating.find(:all,:select =>'DISTINCT name', conditions: ['name LIKE(?)', "%#{ params[:q] }%"]).uniq
+    @ratings = Rating.find(:all, :select =>'DISTINCT name', conditions: ['name LIKE(?)', "%#{ params[:q] }%"]).uniq
+    @ratings << Rating.new(name: params[:q])
     render :json => @ratings , only: [:name,:id]
   end
-
 
 end
