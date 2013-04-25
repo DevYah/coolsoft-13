b16e9205740bc0ae831cdb70463a0ba8246487bf
Omar Kassem <omar.kassem67@gmail.com>
2013-04-25 11:51:13 +0200

    #93, fixed rating prespectives

--- Idearator/app/controllers/ratings_controller.rb
+++ Idearator/app/controllers/ratings_controller.rb
@@ -5,8 +5,11 @@ class RatingsController < ApplicationController
   # +q+:: the parameter is  a string passed through auttocomplete field
   # Author: muhammed hassan
   def ajax
+    r=Rating.find(:all, conditions:{:name => params[:q]})
     @ratings = Rating.find(:all, :select =>'DISTINCT name', conditions: ['name LIKE(?)', "%#{ params[:q] }%"]).uniq
-    @ratings << Rating.new(name: params[:q])
+    if r.empty?
+      @ratings << Rating.new(name: params[:q])
+    end
     render :json => @ratings , only: [:name,:id]
   end
 
