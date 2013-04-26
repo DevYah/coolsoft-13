df6a73d87489db60c8e8542f35fcbda9d3196074
Dayna161193 <daynahany@gmail.com>
2013-04-26 16:30:41 +0200

    Added Tests

--- spec/views/ideas/popover.html.erb_spec.rb
+++ spec/views/ideas/popover.html.erb_spec.rb
@@ -1,27 +1,32 @@
 require "spec_helper"
 
 describe "ideas/popover" do
-  # tests popover is rendered
-  # Author: Dayna
-  before :each do
-    @user = User.new
-    @user.username = 'Dayna'
-    @username = @user.username
-    @user.save
-    @username.save
-    @idea= Idea.new
-    @idea.title= "idea1"
-    @idea.description= "blablabla"
-    @idea.problem_solved= "ay nila"
-    @idea.save
-  end
-
-  it "displays popover " do
-    render
-    rendered.should include("<div = img ")
-    rendered.should include("<div = @username")
-    rendered.should include("<div = @idea.title")
-    rendered.should include("<div = @idea.describtion")
-
-  end
+# tests popover is rendered
+# Author: Dayna
+before :each do
+@user = FactoryGirl.build(:user)
+        @user.confirm!
+        @idea = FactoryGirl.create(:idea)
+        @idea.user_id = @user.id
+        @idea.save
+        @comment = FactoryGirl.build(:comment)
+        @comment.user_id = @user.id
+        @comment.idea_id = @idea.id
+        @comment.save
+        @vote = FactoryGirl.build(:vote)
+        @vote.user_id = @user.id
+        @vote.idea_id = @idea.id
+        @vote.save
 end
+
+it "displays popover " do
+render
+rendered.should include("
+<div = img >")
+rendered.should include("<div = @user.username >")
+rendered.should include("<div = @idea.title>")
+rendered.should include("<div = @idea.describtion >")
+
+end
+end
+
