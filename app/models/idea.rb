506d335a4b0d19223b7ecda265b507a38cd2a8bb
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-04-26 12:15:32 +0200

    Issue #225, edit the like and vote relations
    Votes and likes could now be created as follows:
    User.first.votes.create(:user_id => 1) or
    Idea.first.votes.create(:idea_id => 1).
    The same has been done with likes.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -14,7 +14,8 @@ class Idea < ActiveRecord::Base
   has_many :ratings
   belongs_to :committee
   has_and_belongs_to_many :tags
-  has_and_belongs_to_many :votes, :class_name => 'User', :join_table => :votes
+  has_many :votes
+  has_many :users, :through => :votes
   has_attached_file :photo, :styles => { :small => '60x60>', :medium => "300x300>", :thumb => '10x10!' }, :default_url => '/images/:style/missing.png'
 
   def self.search(search)
