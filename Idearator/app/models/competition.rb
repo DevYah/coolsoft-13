cda4bee2b5027e7545faf8f3c46517f335015a94
Hisham ElGezeery <hisham.elgezeery@gmail.com>
2013-04-26 20:01:24 +0200

    Issue #225, override foreign key idea_id in competition with winner

--- Idearator/app/models/competition.rb
+++ Idearator/app/models/competition.rb
@@ -2,6 +2,6 @@ class Competition < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :investor
   has_and_belongs_to_many :ideas
-  belongs_to :idea
+  belongs_to :winner, :class_name => 'Idea', :foreign_key => 'idea_id'
   has_and_belongs_to_many :tags
 end
