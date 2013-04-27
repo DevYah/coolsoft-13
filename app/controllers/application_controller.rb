726b97abd04bbcd258267ca21049ac10609232bb
mennaamr <menna.amr2@gmail.com>
2013-04-27 18:56:04 +0200

    Log out user if they were banned

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!, :only => [:load_notifications, :update_nav_bar]
   before_filter :load_notifications
+  before_filter :banned?
   protect_from_forgery
 
   # gets first 10 current users notifications and the number of unread notificaations.
@@ -25,6 +26,19 @@ class ApplicationController < ActionController::Base
   def update_nav_bar
     respond_to do |format|
       format.js { render 'layouts/update_nav_bar' }
+    end
+  end
+
+  # Logs user out if they got banned
+  #
+  # Params: None
+  #
+  # Author: Menna Amr
+  def banned?
+    if current_user.present? && current_user.banned?
+      sign_out current_user
+      flash[:error] = "This account has been suspended."
+      root_path
     end
   end
 end
