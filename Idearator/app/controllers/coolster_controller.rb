73307cf5702b7566057cbb6c91441f7bc5cfe3e5
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-28 01:05:00 +0200

    Issue #226 Add who_is_this method to coolster controller

--- Idearator/app/controllers/coolster_controller.rb
+++ Idearator/app/controllers/coolster_controller.rb
@@ -4,4 +4,15 @@ class CoolsterController
     Coolster.add_to_online_users(params[:user])
   end
 
+  def who_is_this
+    
+  end
+
+
+render text: "dgdgfg"
+
+post to idearator/who_is_this
+send session in post params
+idearator replies with user idearator
+
 end
