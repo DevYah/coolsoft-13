d8dbc816073324ed5d10b2b881e5923f35b52800
Hesham Nabil <heshamnabil13@gmail.com>
2013-04-28 00:36:38 +0200

    modified trends_controller

--- Idearator/app/controllers/trends_controller.rb
+++ Idearator/app/controllers/trends_controller.rb
@@ -3,19 +3,20 @@ class TrendsController < ApplicationController
   class VoteHooks
     def after_save(vote)
       v = Vote.find(:all , :conditions => { :idea_id  => vote.idea_id }, :order => 'created_at desc', :limit=> 2 )
-      diff = v[0].created_at - v[1].created_at
-      puts(diff.to_i)
-      if diff.to_i < 200
+      diff = v.last.created_at - v.first.created_at
         t = Trend.find_by_idea_id(vote.idea_id)
         t.trending = t.trending + 4
+        t.save
       end
       if diff.to_i <400 && diff.to_i >200
         t = Trend.find_by_idea_id(vote.idea_id)
         t.trending = t.trending + 2
+        t.save
       end    
       if diff.to_i <1200 && diff.to_i >400
         t = Trend.find_by_idea_id(vote.id)
         t.trending = t.trending + 1
+        t.save
       end    
 
     end
