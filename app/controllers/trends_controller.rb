65e55434b90fcc225f71eb4b0bc7228ea41d4f87
Hesham Nabil <heshamnabil13@gmail.com>
2013-04-28 01:31:28 +0200

    add documentation

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,29 +1,36 @@
 class TrendsController < ApplicationController
-
   class VoteHooks
+    #Select the recent two votes and calculates the time between them
+    #in order to set trending
+    #Params:
+    #+vote+ :: the parameter is an instance of +Vote+
+    #Assigns the trending value of the idea
+    #author Hesham Nabil
     def after_save(vote)
       v = Vote.find(:all , :conditions => { :idea_id  => vote.idea_id }, :order => 'created_at desc', :limit=> 2 )
       diff = v.last.created_at - v.first.created_at
-        t = Trend.find_by_idea_id(vote.idea_id)
-        t.trending = t.trending + 4
-        t.save
-      end
-      if diff.to_i <400 && diff.to_i >200
-        t = Trend.find_by_idea_id(vote.idea_id)
-        t.trending = t.trending + 2
-        t.save
-      end    
-      if diff.to_i <1200 && diff.to_i >400
-        t = Trend.find_by_idea_id(vote.id)
-        t.trending = t.trending + 1
-        t.save
-      end    
-
+      t = Trend.find_by_idea_id(vote.idea_id)
+      t.trending = t.trending + 4
+      t.save
     end
-  end
+    if diff.to_i <400 && diff.to_i >200
+      t = Trend.find_by_idea_id(vote.idea_id)
+      t.trending = t.trending + 2
+      t.save
+    end
+    if diff.to_i < 1200 && diff.to_i > 400
+      t = Trend.find_by_idea_id(vote.id)
+      t.trending = t.trending + 1
+      t.save
+    end
 
-  def self.trends_run
-    @trending = Idea.find(:all, :conditions => { :approved => true }, :order=> 'trending desc', :limit=>4)
   end
+end
+
+#show the trending ideas
+#author Hesham Nabil
+def self.show
+  @trending = Idea.find(:all, :conditions => { :approved => true }, :order=> 'trending desc', :limit=>4)
+end
 
 end
