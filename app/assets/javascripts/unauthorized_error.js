c9c997b5b284e61d5fedfbeb403e6004cbe67330
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-27 22:27:44 +0200

    Issue #219 Update unauthorized modal dialog

--- app/assets/javascripts/unauthorized_error.js
+++ app/assets/javascripts/unauthorized_error.js
@@ -3,11 +3,3 @@ $(document).bind("ajaxError", function (e, xhr) {
     $('#signedout').modal('show');
   }
 });
-
-function add_unauthenticated_event_handlers() {
-  $('a.unauthorized-sign-up').click(function () {
-    $.getScript("/users/show_sign_in_form.js");
-  });
-}
-
-$(document).ready(add_unauthenticated_event_handlers);
