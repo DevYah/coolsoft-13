1555d6afe61ccd1953a4f48c8ead91136d612356
Hesham Nabil <heshamnabil13@gmail.com>
2013-04-27 23:42:22 +0200

    added initial trending algorithm

--- Idearator/app/models/vote.rb
+++ Idearator/app/models/vote.rb
@@ -1,5 +1,7 @@
 class Vote < ActiveRecord::Base
-  attr_accessible :user_id, :idea_id
+  attr_accessible :user_id, :idea_id, :created_at
+  
+  after_save TrendsController::VoteHooks.new
 
   belongs_to :user
   belongs_to :idea
