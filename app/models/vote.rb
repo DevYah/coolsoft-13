cb34d6061223c76dbe2110820d038a42ab53ab56
Marwa Mehanna <marwa.mehana@gmail.com>
2013-04-28 20:25:36 +0200

    ISsue #39, Add counter cache

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   attr_accessible :user_id, :idea_id
 
   belongs_to :user
-  belongs_to :idea ,:counter_cache => true
+  belongs_to :idea , :counter_cache =>  'num_votes'
 end
