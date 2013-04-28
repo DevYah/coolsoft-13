30e4b800bd3b13d7e348f83b56cdf794d498b0b1
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-28 03:14:23 +0200

    Add default ordering to the Similarity model.
    order by similarity

--- Idearator/app/models/similarity.rb
+++ Idearator/app/models/similarity.rb
@@ -2,4 +2,6 @@ class Similarity < ActiveRecord::Base
   belongs_to :idea
   belongs_to :similar_idea, class_name: 'Idea'
   attr_accessible :similarity
+
+  default_scope order('similarity DESC')
 end
