a9a9176ca120750d617afe757204bef3960eaf0c
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-28 05:39:02 +0200

    Issue #234 Add twitter config to omniauth configuration file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,12 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '446451938769749', '7e67a1173177a17d9774812138092a8f'
   provider :twitter, 'lwriQjeyfOMojlldOQbiZQ', 'GD9olZVUib7uSpqXEut1bczCbpltsu5x7a6FfazcOE'
+
+
+  Twitter.configure do |config|
+    config.consumer_key = 'lwriQjeyfOMojlldOQbiZQ'
+    config.consumer_secret = 'GD9olZVUib7uSpqXEut1bczCbpltsu5x7a6FfazcOE'
+    config.oauth_token = '14387745-2lYByp052zlAyWFopVB6g5xjtI5DsZGPPAQyPWPJG'
+    config.oauth_token_secret = 'vdpHOupNhuubeRDC2IUOMqCAYjgx5rg3peoNffaomeg'
+  end
 end
