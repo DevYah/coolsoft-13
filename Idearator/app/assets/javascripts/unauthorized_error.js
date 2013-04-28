7808fe8a238e6fac0f491d0a454a1a335d2ca918
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-28 02:21:32 +0200

    Issue #219 Add eventhandlers, update css

--- Idearator/app/assets/javascripts/unauthorized_error.js
+++ Idearator/app/assets/javascripts/unauthorized_error.js
@@ -1,5 +1,13 @@
 $(document).bind("ajaxError", function (e, xhr) {
   if (xhr.status === 401) {
     $('#signedout').modal('show');
+    $('#unauth-sign-in-button').click(function () {
+      var newValue = { opacity : 100 }
+      $('.unauth-huge').addClass('unauth-right', 500);
+    });
+    $('#unauth-sign-up-button').click(function () {
+      var newValue = { opacity : 100 }
+      $('.unauth-huge').removeClass('unauth-right', 500);
+    });
   }
 });
