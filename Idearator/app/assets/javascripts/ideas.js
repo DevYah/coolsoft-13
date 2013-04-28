cc8821fbc0eb35b27eaffc4cf14bdef49e935e92
mennaamr <menna.amr2@gmail.com>
2013-04-28 02:10:03 +0200

    Add js and css files

--- Idearator/app/assets/javascripts/ideas.js
+++ Idearator/app/assets/javascripts/ideas.js
@@ -70,3 +70,25 @@ $(document).ready(function() {
   });
 
 });
+
+function prepareInputsForHints() {
+  var inputs = document.getElementsByTagName("input");
+  for (var i=0; i<inputs.length; i++){
+    inputs[i].onfocus = function () {
+      this.parentNode.getElementsByTagName("span")[0].style.display = "inline";
+    }
+    inputs[i].onblur = function () {
+      this.parentNode.getElementsByTagName("span")[0].style.display = "none";
+    }
+  }
+  var selects = document.getElementsByTagName("select");
+  for (var k=0; k<selects.length; k++){
+    selects[k].onfocus = function () {
+      this.parentNode.getElementsByTagName("span")[0].style.display = "inline";
+    }
+    selects[k].onblur = function () {
+      this.parentNode.getElementsByTagName("span")[0].style.display = "none";
+    }
+  }
+}
+addLoadEvent(prepareInputsForHints);
