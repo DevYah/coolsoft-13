b6697a1eb76a37636a46434d44c81b6ef479d44e
Hesham Nabil <heshamnabil13@gmail.com>
2013-04-28 22:48:46 +0200

    added test data script for votes and adjusted trends_controller

--- Idearator/app/models/trend.rb
+++ Idearator/app/models/trend.rb
@@ -1,4 +1,4 @@
 class Trend < ActiveRecord::Base
-  attr_accessible :vote, :idea_id , :trending , :created_at
+  attr_accessible :vote, :idea_id , :trending , :created_at , :rounds
   has_many :ideas
 end
--- Idearator/db/migrate/20130427185521_create_trends.rb
+++ Idearator/db/migrate/20130427185521_create_trends.rb
@@ -2,7 +2,8 @@ class CreateTrends < ActiveRecord::Migration
   def change
     create_table :trends do |t|
       t.references :idea
-      t.integer :trending
+      t.integer :trending , :default => 0
+      t.integer :rounds , :default => 0
     end
   end
 end
