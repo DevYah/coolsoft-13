ef6345baf7a13294bc83c4b972f1628f99979a28
Mina Nagy Zaki <mnzaki@gmail.com>
2013-04-28 03:14:04 +0200

    Add similar_ideas relation to the Idea model.

--- Idearator/app/models/idea.rb
+++ Idearator/app/models/idea.rb
@@ -20,7 +20,11 @@ class Idea < ActiveRecord::Base
   has_and_belongs_to_many :competitions
   has_many :winning_competitions, :class_name => 'Competition'
 
+  has_many :similarities
+  has_many :similar_ideas, through: :similarities
+
   has_attached_file :photo, :styles => { :small => '60x60>', :medium => "300x300>", :thumb => '10x10!' }, :default_url => '/images/:style/missing.png'
+
   def self.search(search)
     if search
       where('title LIKE ?', "%#{search}%")
@@ -28,4 +32,5 @@ class Idea < ActiveRecord::Base
       find(:all)
     end
   end
+
 end
