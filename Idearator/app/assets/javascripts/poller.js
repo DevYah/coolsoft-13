cc28c543b157dcbd4df293cd68ef843adfc6c655
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-28 19:39:27 +0200

    Issue #226 Update url for coolster/poll

--- Idearator/app/assets/javascripts/poller.js
+++ Idearator/app/assets/javascripts/poller.js
@@ -1,4 +1,14 @@
 $(function(){
-  console.log("POLLLLING");
-  $.getScript('coolster_app/poll');
+  poll();
 });
+
+function poll(){
+  console.log("POLLLLING");
+  $.ajax({
+  url: 'http://localhost:3000/coolster_app/poll',
+  dataType: "script",
+  success: poll,
+  error: function wait(){setTimeout(poll, 5000);},
+  timeout: 180000
+  });
+}
