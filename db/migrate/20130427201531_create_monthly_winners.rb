3c529a0d3d14096e067607b500296d066efa21b2
Omar Kassem <omar.kassem67@gmail.com>
2013-04-28 14:04:36 +0200

    Fixed the migration

--- db/migrate/20130427201531_create_monthly_winners.rb
+++ db/migrate/20130427201531_create_monthly_winners.rb
@@ -2,8 +2,6 @@ class CreateMonthlyWinners < ActiveRecord::Migration
   def change
     create_table :monthly_winners do |t|
       t.column :idea, :idea
-      t.column :date, :date
-
       t.timestamps
     end
   end
