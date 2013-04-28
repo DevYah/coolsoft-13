690b6370f1ca47c808b461460b2e28550d34d541
mennaamr <menna.amr2@gmail.com>
2013-04-28 23:49:10 +0200

    Update form hints

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -69,26 +69,10 @@ $(document).ready(function() {
     $(this).append("<i class='icon-ok pull-right' id ='edited-check-mark'></i>");
   });
 
-});
 
-function prepareInputsForHints() {
-  var inputs = document.getElementsByTagName("input");
-  for (var i=0; i<inputs.length; i++){
-    inputs[i].onfocus = function () {
-      this.parentNode.getElementsByTagName("span")[0].style.display = "inline";
-    }
-    inputs[i].onblur = function () {
-      this.parentNode.getElementsByTagName("span")[0].style.display = "none";
-    }
-  }
-  var selects = document.getElementsByTagName("select");
-  for (var k=0; k<selects.length; k++){
-    selects[k].onfocus = function () {
-      this.parentNode.getElementsByTagName("span")[0].style.display = "inline";
-    }
-    selects[k].onblur = function () {
-      this.parentNode.getElementsByTagName("span")[0].style.display = "none";
-    }
-  }
-}
-addLoadEvent(prepareInputsForHints);
+  $("input, select, textarea").on("focus", function(){
+   $(this).siblings("span").css("display", "inline");
+  }).on("blur", function(){
+   $(this).siblings("span").css("display", "none");
+  });
+});
