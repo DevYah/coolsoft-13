4c548dc4cc38c5f8e05989ae9e94e0b3fc51e261
Dayna161193 <daynahany@gmail.com>
2013-04-29 19:32:32 +0200

    Added Testing

--- spec/views/ideas/popover.html.erb_spec.rb
+++ spec/views/ideas/popover.html.erb_spec.rb
@@ -4,25 +4,19 @@ describe "ideas/popover" do
 # tests popover is rendered
 # Author: Dayna
 before :each do
-@user = FactoryGirl.build(:user)
-@user.confirm!
-@idea = FactoryGirl.create(:idea)
-@idea.user_id = @user.id
-@idea.save
-@vote = FactoryGirl.build(:vote)
-@vote.user_id = @user.id
-@vote.idea_id = @idea.id
-@vote.save
+  @user = FactoryGirl.build(:user)
+  @user.confirm!
+  @idea = FactoryGirl.create(:idea)
+  @idea.user_id = @user.id
+  @idea.save
+  @vote = FactoryGirl.build(:vote)
+  @vote.user_id = @user.id
+  @vote.idea_id = @idea.id
+  @vote.save
 end
-
-it "displays popover " do
-render
-rendered.should include("
-<div = img >")
-rendered.should include("<div = @user.username >")
-rendered.should include("<div = @idea.title>")
-rendered.should include("<div = @idea.describtion >")
-
+it "has a button vote" do
+  render :template => "/ideas/_popover", :locals => {:idea => @idea , :vote =>@vote ,:user_id => @user}
+  rendered.should have_button('vote')
 end
 end
 
