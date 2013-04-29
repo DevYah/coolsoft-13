4bb8fcefe7829bc241ef5989b36c6f38b1abc0a7
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-29 21:45:05 +0200

    Issue #73 call push_notification method in notification models

--- Idearator/app/models/create_competition_notification.rb
+++ Idearator/app/models/create_competition_notification.rb
@@ -3,6 +3,11 @@ class CreateCompetitionNotification < CompetitionNotification
 
   def self.send_notification (user_sender, competition, users_receivers)
     create_competition_notification = CreateCompetitionNotification.create(user: user_sender, competition: competition, users: users_receivers)
+    user_ids = []
+    users_receivers.each do |user|
+      user_ids << user.id.to_s
+    end
+    NotificationsController::CoolsterPusher.new.push_notification user_ids, create_competition_notification
   end
 
   def text
--- Idearator/app/models/delete_competition_notification.rb
+++ Idearator/app/models/delete_competition_notification.rb
@@ -7,6 +7,11 @@ class DeleteCompetitionNotification < ActiveRecord::Base
 
   def self.send_notification(user_sender,competition, users_receivers)
     delete_competition_notification = DeleteCompetitionNotification.create(user: user_sender,competition: competition, competition_title: competition.title, users: users_receivers)
+    user_ids = []
+    users_receivers.each do |user|
+      user_ids << user.id.to_s
+    end
+    NotificationsController::CoolsterPusher.new.push_notification user_ids, delete_competition_notification
   end
 
   def text
