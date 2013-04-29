1c15bdd774afd65d46ed56bdfff31a806bd5b6ba
muhammedabuali <muhammedabuali@gmail.com>
2013-04-29 02:21:52 +0200

    Issue #240 , added infinite scrolling

--- Idearator/app/controllers/competitions_controller.rb
+++ Idearator/app/controllers/competitions_controller.rb
@@ -1,20 +1,21 @@
 class CompetitionsController < ApplicationController
-
-  before_filter :authenticate_user!, :only => [:new ,:create , :edit, :update]
   # GET /competitions
   # GET /competitions.json
   def index
-    @competitions = Competition.all
-
+    @firstTime = false
+    if(params[:myPage])
+      @competitions = Competition.page(params[:myPage]).per(10)
+    else
+      @firstTime = true
+      @competitions = Competition.page(1).per(10)
+    end
     respond_to do |format|
       format.js
     end
   end
 
-  # view competition of current user
-  # Params
-  # +id+:: is passed in params through the new competition view, it is used to identify the instance of +Competition+ to be viewed
-  # Marwa Mehanna
+  # GET /competitions/1
+  # GET /competitions/1.json
   def show
     @competition = Competition.find(params[:id])
     @chosen_tags_competition = Competition.find(params[:id]).tags
@@ -24,8 +25,8 @@ class CompetitionsController < ApplicationController
     end
   end
 
-  # making new Competition
-  #Marwa Mehann
+  # GET /competitions/new
+  # GET /competitions/new.json
   def new
     @competition = Competition.new
     chosen_tags_competition=[]
@@ -35,19 +36,14 @@ class CompetitionsController < ApplicationController
     end
   end
 
-  ## editing Idea
-  # Params
-  # +id+ :: this is an instance of +Competition+ passed through _form.html.erb, used to identify which +Competition+ to edit
-  # Author: Marwa Mehanna
+  # GET /competitions/1/edit
   def edit
     @competition = Competition.find(params[:id])
     @chosen_tags_competition = Competition.find(params[:id]).tags
   end
 
-  # creating new Idea
-  # Params
-  # +competition+ :: this is an instance of +Competition+ passed through _form.html.erb, identifying the competition which will be added to records
-  # Author: Marwa Mehanna
+  # POST /competitions
+  # POST /competitions.json
   def create
     @competition = Competition.new(params[:competition])
     @competition.investor_id = current_user.id
@@ -62,40 +58,31 @@ class CompetitionsController < ApplicationController
     end
   end
 
-  # updating Idea
-  # Params
-   # +id+ :: this is an instance of +Competition+ passed through _form.html.erb, used to identify which +Competition+ to edit
-  # Author: Marwa Mehanna
+  # PUT /competitions/1
+  # PUT /competitions/1.json
   def update
     @competition = Competition.find(params[:id])
 
     respond_to do |format|
       if @competition.update_attributes(params[:competition])
-        format.html { redirect_to @competition, :notice => 'Competition was successfully updated.' }
-        format.json { respond_with_bip(@competition) }
+        format.html { redirect_to @competition, notice: 'Competition was successfully updated.' }
+        format.json { head :no_content }
       else
-        format.html { render :action => 'edit' }
-        format.json { respond_with_bip(@competition) }
+        format.html { render action: "edit" }
+        format.json { render json: @competition.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # Deletes all records related to a specific idea
-  # Params:
-  # +id+:: is used to specify the instance of +competition+ to be deleted
-  #Author: Marwa Mehanna
+  # DELETE /competitions/1
+  # DELETE /competitions/1.json
   def destroy
     @competition = Competition.find(params[:id])
-    if current_user.id == @competition.investor_id
-      @competition.destroy
-      respond_to do |format|
-        format.html { redirect_to '/', alert: 'Your Competition has been successfully deleted!' }
-      end
+    @competition.destroy
 
-    else
-      respond_to do |format|
-        format.html { redirect_to idea, alert: 'You do not own the idea, so it cannot be deleted!' }
-      end
+    respond_to do |format|
+      format.html { redirect_to competitions_url }
+      format.json { head :no_content }
     end
   end
 end
