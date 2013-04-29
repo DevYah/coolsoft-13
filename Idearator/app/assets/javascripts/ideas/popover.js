8e6315aa034deb444969380e213c66995804b4ae
Dayna161193 <daynahany@gmail.com>
2013-04-30 01:26:52 +0200

    trying to merge with top ten

--- Idearator/app/assets/javascripts/ideas/popover.js
+++ Idearator/app/assets/javascripts/ideas/popover.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
   $('a[data-popover-idea-id]').click(function (e) {
     e.preventDefault();
-    //alert("hesham");
     var id = $(this).data("popover-idea-id");
     $.getScript('/ideas/' + id + '/popover');
 
-});
+  });
 });
