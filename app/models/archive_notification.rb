7ff080ca7bf9c9489b0dfa2c4a1b555e62039b91
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-29 20:19:47 +0200

    Issue #226 Call push_notiifcation in send_notification method in all notification models

--- app/models/archive_notification.rb
+++ app/models/archive_notification.rb
@@ -2,6 +2,11 @@ class ArchiveNotification < IdeaNotification
   inherits_from :notification
   def self.send_notification(user_sender, idea, users_receivers)
     archive_notification = ArchiveNotification.create(user: user_sender, idea: idea, users: users_receivers)
+    user_ids = []
+    users_receivers.each do |user|
+      user_ids << user.id.to_s
+    end
+    NotificationsController::CoolsterPusher.new.push_notification user_ids, archive_notification
   end
 
   def text
--- app/models/invite_committee_notification.rb
+++ app/models/invite_committee_notification.rb
@@ -3,6 +3,11 @@ class InviteCommitteeNotification < UserNotification
 
   def self.send_notification(user_sender, users_receivers)
     invite_notification = InviteCommitteeNotification.create(user: user_sender, users: users_receivers)
+    user_ids = []
+    users_receivers.each do |user|
+      user_ids << user.id.to_s
+    end
+    NotificationsController::CoolsterPusher.new.push_notification user_ids, invite_notification
   end
 
   def text
