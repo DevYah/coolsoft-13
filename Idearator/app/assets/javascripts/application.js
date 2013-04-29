bd734296b0a1c6a2c644b709cf437377fd0cbf61
Gezeery <hisham.elgezeery@gmail.com>
2013-04-29 03:09:01 +0200

    Issue #255, add jquery hook function
    This function intercepts redirects to user profiles and shows a modal dialog instead.

--- Idearator/app/assets/javascripts/application.js
+++ Idearator/app/assets/javascripts/application.js
@@ -79,4 +79,23 @@ $(document).ready(function () {
     container: 'header'
   });
 
+  $("#main a[href^='/users/']").each(function(){
+    var href = $(this).attr('href');
+    var href_url = href.split('/users/');
+    if($.isNumeric(href_url[1]) && String(href_url[3])){
+      var user_id = href_url[1];
+      $(this).attr('href', "/users/profile_modal?id="+user_id+"&remote=true");
+      $(this).attr('data-remote', 'true');
+      $(this).bind('click', function(event) {
+        event.preventDefault();
+        $("#user-profile-modal").modal('show');
+      });
+    }
+  });
+
+  $("#modal-close").click(function () {
+    $("#user-profile-modal").modal('hide');
+  });
+
+
 });
