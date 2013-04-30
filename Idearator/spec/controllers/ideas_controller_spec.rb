010811229a1518e9d04c39228af034c24ccb27b4
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-30 18:47:23 +0200

    Issue #44 Fix destroy method tests

--- Idearator/spec/controllers/ideas_controller_spec.rb
+++ Idearator/spec/controllers/ideas_controller_spec.rb
@@ -178,14 +178,17 @@ describe IdeasController do
         @idea = FactoryGirl.create(:idea)
         @idea.user_id = @user.id
         @idea.save
-        @comment = FactoryGirl.build(:comment)
-        @comment.user_id = @user.id
-        @comment.idea_id = @idea.id
-        @comment.save
         @vote = FactoryGirl.build(:vote)
         @vote.user_id = @user.id
         @vote.idea_id = @idea.id
         @vote.save
+        @rating = FactoryGirl.create(:rating)
+        @rating.idea_id = @idea.id
+        @rating.save
+        @user_rating = FactoryGirl.build(:user_rating)
+        @user_rating.rating_id = @rating.id
+        @user_rating.user_id = @user.id
+        @user_rating.save
         sign_in @user
       end
 
@@ -198,12 +201,16 @@ describe IdeasController do
         response.should redirect_to '/'
       end
 
-      it 'deletes idea comments' do
-        expect { delete :destroy, :id => @idea.id }.to change(Comment, :count).by(-1)
-      end
-
       it 'deletes idea votes' do
         expect { delete :destroy, :id => @idea.id }.to change(Vote, :count).by(-1)
+      end
+
+      it 'deletes idea ratings' do
+        expect { delete :destroy, :id => @idea.id }.to change(Rating, :count).by(-1)
+      end
+
+      it 'deletes idea user ratings' do
+        expect { delete :destroy, :id => @idea.id }.to change(UserRating, :count).by(-1)
       end
     end
 
@@ -216,14 +223,17 @@ describe IdeasController do
         @idea = FactoryGirl.create(:idea)
         @idea.user_id = @userone.id
         @idea.save
-        @comment = FactoryGirl.build(:comment)
-        @comment.user_id = @userone.id
-        @comment.idea_id = @idea.id
-        @comment.save
         @vote = FactoryGirl.build(:vote)
         @vote.user_id = @userone.id
         @vote.idea_id = @idea.id
         @vote.save
+        @rating = FactoryGirl.build(:rating)
+        @rating.idea_id = @idea.id
+        @rating.save
+        @user_rating = FactoryGirl.build(:user_rating)
+        @user_rating.rating_id = @rating.id
+        @user_rating.user_id = @userone.id
+        @user_rating.save
         sign_in @usertwo
       end
 
@@ -236,12 +246,16 @@ describe IdeasController do
         response.should redirect_to @idea
       end
 
-      it 'does not delete idea comments' do
-        expect { delete :destroy, :id => @idea.id }.to change(Comment, :count).by(0)
-      end
-
       it 'does not delete idea votes' do
         expect { delete :destroy, :id => @idea.id }.to change(Vote, :count).by(0)
+      end
+
+      it 'does not delete idea ratings' do
+        expect { delete :destroy, :id => @idea.id }.to change(Rating, :count).by(0)
+      end
+
+      it 'does not delete idea user ratings' do
+        expect { delete :destroy, :id => @idea.id }.to change(UserRating, :count).by(0)
       end
     end
   end
