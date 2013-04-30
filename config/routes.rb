254aa7b494fd66b1416c3c9a47b5a5a4229d6e50
muhammedabuali <muhammedabuali@gmail.com>
2013-04-30 18:16:15 +0200

    Issue #46 ,fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,7 @@ Sprint0::Application.routes.draw do
   # Admin actions routes
   controller :admins do
     match 'admins/invite'
-    match 'admins/invite_committee'
+    match 'invite_committee'
   end
 
   # Committe actions routes
