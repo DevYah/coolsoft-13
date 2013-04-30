fecb851e6d6796d2dbe5014490ea125fb1744db0
Gezeery <hisham.elgezeery@gmail.com>
2013-04-30 19:05:30 +0200

    Issue #52, edit the effect at ajax callback

--- Idearator/app/assets/javascripts/users.js
+++ Idearator/app/assets/javascripts/users.js
@@ -28,6 +28,11 @@ $(document).ready(function() {
 
   $('.best_in_place').best_in_place();
 
+  $('.best_in_place').bind("ajax:success", function(){
+    $('#edited-check-mark').remove();
+    $(this).append("<i class='icon-ok pull-right' id ='edited-check-mark'></i>");
+  });
+
   $.datepicker.setDefaults({
     dateFormat: 'yy/mm/dd', changeMonth: true,
     changeYear: true, yearRange: 'c-65:c+10'
