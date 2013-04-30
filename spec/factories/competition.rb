54165c5fe4bce939cb7d0fcc58c3ab922f1453a6
muhammedabuali <muhammedabuali@gmail.com>
2013-04-30 16:30:49 +0200

    Issue #240 ,added mmodel tests

---
+++ spec/factories/competition.rb
@@ -0,0 +1,10 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :competition do |f|
+    f.title Faker::Name.name
+    f.description Faker::Lorem.paragraph
+    f.start_date 2.days.ago
+    f.end_date 8.days.since Time.now.to_date
+  end
+end
