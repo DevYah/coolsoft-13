6657db4fdbe74ffa2d828596e98f43d36d66a3b1
Mohammad Abdulkhaliq <mohammad28march1993@gmail.com>
2013-05-01 00:00:30 +0200

    Issue #95 removed empty lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!, :only => [:deactivate, :confirm_deactivate, :activate, :expertise, :change_settings]
 
-
-
   # displays a form where the user enters his password to confrim deactivation.
   # Params: none
   # Author: Amina Zoheir
