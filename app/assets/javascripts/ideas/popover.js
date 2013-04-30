23aedf4c4bf19e6a3f3c9811e61e358ed603f9e6
Dayna161193 <daynahany@gmail.com>
2013-04-30 03:39:38 +0200

    Finally Fixed the popover in the TOP TEN

--- app/assets/javascripts/ideas/popover.js
+++ app/assets/javascripts/ideas/popover.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
   $('a[data-popover-idea-id]').click(function (e) {
     e.preventDefault();
+    e.stopPropagation();
     var id = $(this).data("popover-idea-id");
-    $.getScript('/ideas/' + id + '/popover');
+    $.getScript('/ideas/'+id+'/popover?id=' + id);
 
   });
 });
