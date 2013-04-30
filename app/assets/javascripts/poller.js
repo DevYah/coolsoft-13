9db9b79f5397e7e7484201cd0d1cb633bed8209f
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-30 12:17:36 +0200

    Issue #226 Fix polling to repeat after timeout

--- app/assets/javascripts/poller.js
+++ app/assets/javascripts/poller.js
@@ -3,12 +3,30 @@ function poll() {
   $.ajax({
     url: 'http://localhost:9292/poll',
     dataType: "script",
-    success: poll,
-    error: function () { setTimeout(poll, 5000); },
-    timeout: 180000
+    tryCount : 0,
+    retryLimit : 5,
+    success: function(data, textStatus, jXHR){
+      $.ajax(this);
+        return;
+    },
+    error: function(jXHR, textStatus, errorThrown){
+      if(errorThrown == 'timeout'){
+        $.ajax(this);
+        return;
+      }else{
+        tryCount ++;
+        if(tryCount <= retryLimit){
+          {setTimeout(poll, 10000)}
+        }else{
+          alert('You\'ve been disconnected');
+          return;
+        }
+      }
+    },
+    timeout: 30000
   });
 }
 
 $(function () {
-  poll();
+  setTimeout(poll, 1000);
 });
