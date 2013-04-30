8b55f00aee7e08873b9ab2e14db76ab0dd5ecb6a
Marwa Mehanna <marwa.mehana@gmail.com>
2013-04-30 20:56:22 +0200

    Issue #218,integrating with the stream

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -11,35 +11,35 @@ class StreamController < ApplicationController
     @search_with_user = params[:search_user] == "true"
     @searching_with = params[:searchtype] == "true"
 
-  if @page != nil
-    if !@search_with_user
-      if @searchtext.to_s.strip.length == 0 and @filter.empty?
-        @ideas = Idea.order(:created_at).page(params[:mypage]).per(10)
-        respond_to do |format|
-          format.html
-          format.js
-        end
-      else
-        if @searchtext.to_s.strip.length > 0 and @filter.empty?
-          @ideas = Idea.search(params[:search]).order(:created_at).page(params[:mypage]).per(10)
+    if @page != nil
+      if !@search_with_user
+        if @searchtext.to_s.strip.length == 0 and @filter.empty?
+          @ideas = Idea.order(:created_at).page(params[:mypage]).per(10)
+          respond_to do |format|
+            format.html
+            format.js
+          end
         else
-          @ideas = Idea.filter(@filter).sort{|i1,i2| i1.created_at <=> i2.created_at}.uniq
-          @ideas = Kaminari.paginate_array(@ideas).page(params[:mypage]).per(10)
+          if @searchtext.to_s.strip.length > 0 and @filter.empty?
+            @ideas = Idea.search(params[:search]).order(:created_at).page(params[:mypage]).per(10)
+          else
+            @ideas = Idea.filter(@filter).sort{|i1,i2| i1.created_at <=> i2.created_at}.uniq
+            @ideas = Kaminari.paginate_array(@ideas).page(params[:mypage]).per(10)
+          end
         end
-      end
-    else
-      @users = User.search(params[:search]).page(params[:mypage]).per(10)
-    end
-  else
-    if @searchtext.nil?
-      @ideas = Idea.order(:created_at).page(1).per(10)
-    else
-      if !@searching_with
-        @ideas = Idea.search(params[:search]).order(:created_at).page(params[:mypage]).per(10)
       else
         @users = User.search(params[:search]).page(params[:mypage]).per(10)
       end
+    else
+      if @searchtext.nil?
+        @ideas = Idea.order(:created_at).page(1).per(10)
+      else
+        if !@searching_with
+          @ideas = Idea.search(params[:search]).order(:created_at).page(params[:mypage]).per(10)
+        else
+          @users = User.search(params[:search]).page(params[:mypage]).per(10)
+        end
+      end
     end
-	end
-end
+  end
 end
