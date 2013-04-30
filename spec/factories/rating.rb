67b580210005425b744355469d7fa893629e6022
Mahmoud Hashish <m7moud.ahly@gmail.com>
2013-04-30 18:47:02 +0200

    Issue #44 Fix Rating Factory

--- spec/factories/rating.rb
+++ spec/factories/rating.rb
@@ -2,7 +2,7 @@ require 'faker'
 require 'factory_girl_rails'
 
 FactoryGirl.define do
-  factory :rating do |f|
+  factory :rating, :class => Rating do |f|
     f.name 'rating-perspective'
     f.value 0
   end
