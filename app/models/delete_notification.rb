f339d3d29ea8d5734c16340f0c7c02d104eded84
Amina Zoheir <amina.zoheir@gmail.com>
2013-04-30 13:25:37 +0200

    Issue #73 Add documentation to model methods

--- app/models/delete_notification.rb
+++ app/models/delete_notification.rb
@@ -5,6 +5,12 @@ class DeleteNotification < ActiveRecord::Base
   belongs_to :user
   attr_accessible :type, :user, :idea, :idea_title, :users
 
+  # creates new DeleteNotification and pushes the notification to the online receivers
+  # Params:
+  # +user_sender+:: the parameter is an instance of +User+.
+  # +idea+:: the parameter is an instance of +Idea+.
+  # +users_receivers+:: the parameter is an array of instances of +User+.
+  # Author: Amina Zoheir
   def self.send_notification(user_sender, idea, users_receivers)
     delete_notification = DeleteNotification.create(user: user_sender, idea: idea, idea_title: idea.title, users: users_receivers)
     user_ids = []
@@ -14,18 +20,25 @@ class DeleteNotification < ActiveRecord::Base
     NotificationsController::CoolsterPusher.new.push_notification user_ids, delete_notification
   end
 
+  # returns notification text
+  # Params: none
+  # Author: Amina Zoheir
   def text
     User.find(self.user_id).username + " deleted his idea " + self.idea_title + "."
   end
 
+  # returns the value of the read field for a certain user and this notification
+  # Params:
+  # +user+:: the parameter is an instance of +User+.
+  # Author: Amina Zoheir
   def read_by?(user)
-    if NotificationsUser.find(:first, :conditions => {notification_id: self.id, user_id: user.id }).read
-      true
-    else
-      false
-    end
+    NotificationsUser.find(:first, :conditions => {notification_id: self.id, user_id: user.id }).read
   end
 
+  # sets the value of the read field for a certain user and this notification to true
+  # Params:
+  # +user+:: the parameter is an instance of +User+.
+  # Author: Amina Zoheir
   def set_read_for(user)
     notification = NotificationsUser.find(:first, :conditions => {notification_id: self.id, user_id: user.id})
 
