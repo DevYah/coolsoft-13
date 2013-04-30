9a05abb1e678cf7ab6137a6e15f2fa08b0224773
Omar Kassem <omar.kassem67@gmail.com>
2013-04-30 15:44:29 +0200

    #227, edited tests

--- spec/controllers/competitions_controller_spec.rb
+++ spec/controllers/competitions_controller_spec.rb
@@ -12,6 +12,12 @@ describe CompetitionsController do
         @i.username='i'
         @i.confirm!
         @i.save
+        @u=Investor.new
+        @u.email='u@gmail.com'
+        @u.password=123123123
+        @u.username='u'
+        @u.confirm!
+        @u.save
         @c=Competition.new
         @c.title='bla'
         @c.description='bla'
@@ -22,33 +28,62 @@ describe CompetitionsController do
         @idea.description='blabla'
         @idea.problem_solved='blabla'
         @idea.save
+        @idea1=Idea.new
+        @idea1.title='bla'
+        @idea1.description='bla'
+        @idea1.problem_solved='bla'
+        @idea1.save
+        @idea2=Idea.new
+        @idea2.title='ay7aga'
+        @idea2.description='ay7aga'
+        @idea2.problem_solved='ay7aga'
+        @idea2.save
+        @idea3=Idea.new
+        @idea3.title='blablabla'
+        @idea3.description='blablabla'
+        @idea3.problem_solved='blablabla'
+        @idea3.save
         @ce=CompetitionEntry.new
         @ce.idea_id=@idea.id
         @ce.competition_id=@c.id
         @ce.save
-        @u=Investor.new
-        @u.email='u@gmail.com'
-        @u.password=123123123
-        @u.username='u'
+        @ce1=CompetitionEntry.new
+        @ce1.idea_id=@idea1.id
+        @ce1.competition_id=@c.id
+        @ce1.approved=true
+        @ce1.save
+        @ce2=CompetitionEntry.new
+        @ce2.idea_id=@idea2.id
+        @ce2.competition_id=@c.id
+        @ce2.rejected=true
+        @ce2.save
+        @ce2=CompetitionEntry.new
+        @ce2.idea_id=@idea2.id
+        @ce2.competition_id=@c.id
+        @ce2.rejected=true
+        @ce2.save
 
       end
 
-      it 'Investor review the ideas' do
+      it 'should show the investor only the ideas on his competition that are not approved or rejected yet' do
         sign_in @i
         get :review_competitions_ideas, :id => @c.id
         assigns(:ideas).count.should eql(1)
       end
-      it 'guest should redirect to home page' do
+
+      it 'should redirect guest to home page' do
         get :review_competitions_ideas, :id => @c.id
         response.should redirect_to '/'
       end
-      it 'Investor who doesnt own the Competition should redirect to home page' do
+
+      it 'should redirect Investor who doesnt own the Competition to home page' do
         sign_in @u
         get :review_competitions_ideas, :id => @c.id
         response.should redirect_to '/'
       end
     end
   end
+
   describe 'approve' do
     context 'approving a certain idea to enter the competition' do
       before :each do
@@ -76,26 +111,28 @@ describe CompetitionsController do
         @u.email='u@gmail.com'
         @u.password=123123123
         @u.username='u'
-
       end
-      it 'Competition Investor change the approve status of the Competition entry' do
+
+      it 'should change the approve status of the Competition entry' do
         sign_in @i
         get :approve, :id =>@c.id ,:idea_id => @idea.id
         @ce.reload
         @ce.approved.should eql(true)
       end
-      it 'guest redirect to home page' do
-        get :approve, :id =>@c.id ,:idea_id => @idea.id
-        response.should redirect_to '/'
-      end
-      it 'Investor who doesnt own the Competition doesnt change the approve status of the Competition Entry' do
-        sign_in @u
+
+      it 'should redirect guest to home page' do
         get :approve, :id =>@c.id ,:idea_id => @idea.id
         response.should redirect_to '/'
       end
 
+      it 'should redirect Investor who doesnt own the Competition to home page' do
+        sign_in @u
+        get :approve, :id =>@c.id ,:idea_id => @idea.id
+        response.should redirect_to '/'
+      end
     end
   end
+
   describe 'reject' do
     context 'rejecting the participation of a certain idea in the competition' do
       before :each do
@@ -123,24 +160,25 @@ describe CompetitionsController do
         @u.email='u@gmail.com'
         @u.password=123123123
         @u.username='u'
-
       end
-      it 'Competition Investor change the approve status of the Competition entry' do
+
+      it 'should change the reject status of the competition entry' do
         sign_in @i
         get :reject, :id =>@c.id ,:idea_id => @idea.id
         @ce.reload
         @ce.rejected.should eql(true)
       end
-      it 'guest redirect to home page' do
-        get :reject, :id =>@c.id ,:idea_id => @idea.id
-        response.should redirect_to '/'
-      end
-      it 'Investor who doesnt own the Competition doesnt change the approve status of the Competition Entry' do
-        sign_in @u
+
+      it 'should redirect guest to home page' do
         get :reject, :id =>@c.id ,:idea_id => @idea.id
         response.should redirect_to '/'
       end
 
+      it 'should redirect Investor who doesnt own the Competition to home page ' do
+        sign_in @u
+        get :reject, :id =>@c.id ,:idea_id => @idea.id
+        response.should redirect_to '/'
+      end
     end
   end
 end
