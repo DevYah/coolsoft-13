810b7cb6fa0b9bb096d055e5735b9f156df5ac56
muhammedabuali <muhammedabuali@gmail.com>
2013-04-30 17:22:56 +0200

    Issue #240 , added controller tests

--- spec/factories/competition.rb
+++ spec/factories/competition.rb
@@ -6,5 +6,7 @@ FactoryGirl.define do
     f.description Faker::Lorem.paragraph
     f.start_date 2.days.ago
     f.end_date 8.days.since Time.now.to_date
+    f.tags [ Tag.new(:name => 'Science')]
   end
+
 end
