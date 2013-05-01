a8fda5bfa2bac3352a24251eb8dec14eb59bf173
Mina Nagy Zaki <mnzaki@gmail.com>
2013-05-01 19:07:00 +0200

    Revamp the similarity index algorithm.

--- lib/similarity_engine.rb
+++ lib/similarity_engine.rb
@@ -1,21 +1,166 @@
+# Relatively simple similarity index calculation based on:
+# 1- Jaccard set similarity of tags
+# 2- Jaccard set similarity of keywords in the title, where keywords are simply
+#      defined as words that are at least KEYWORD_LENGTH (currently 5) long
+# 3- Hamming distance between the simhashes of title, description and
+#      problem solved
+
+require 'set'
+
 class SimilarityEngine
 
+  # Shingle lengths for Title, Description and Problem Solved
+  DEFAULT_SHINGLE_LEN = 3
+  DESC_SHINGLE_LEN = 3
+  PROB_SHINGLE_LEN = 3
+  TITLE_SHINGLE_LEN = 4
+
+  # Minimum length of a word to be considered a keyword in the title
+  KEYWORD_LENGTH = 5
+
+  # Weighing model
+  SIMILARITY_MODEL = {
+    # Jaccard coeffs are 0.0..1.0
+    tags_jacc_idx:           10,
+    title_keywords_jacc_idx: 5,
+    # Hamming distance are -16..16
+    title_hamming_dist:      0.5,
+    desc_hamming_dist:       0.1,
+    prob_hamming_dist:       0.1
+  }
+
+  # Hook to recalculate the similarity coefficients for an idea upon saving
   class IdeaHooks
     #FIXME delayed_job?
     def after_save(idea)
-      SimilarityEngine.recalculate_similarity(idea)
+      SimilarityEngine.rebuild_similarities(idea)
     end
   end
 
-  WEIGHT = {jaccard: 0.5, k_shingles: 0.5}
-
-  def self.calculate_jaccard_similarity(idea1, idea2)
-    union = idea1.tag_ids | idea2.tag_ids
-    inter = idea1.tag_ids & idea2.tag_ids
-    Float(inter.size) / union.size
+  # Break a string into its constituent n-gram shingles
+  #
+  # Params:
+  # +str+:: +String+ to shingle
+  # +ngram_len+:: the length of each shingle
+  #
+  # Author: Mina Nagy
+  def self.shingle(str, ngram_len = DEFAULT_SHINGLE_LEN)
+    ngrams = Set.new
+    return ngrams if str.nil? || str.length < ngram_len
+    (str.length - ngram_len+1).times do |i|
+      ngrams << str[i, ngram_len]
+    end
+    ngrams
   end
 
-  def self.recalculate_similarity(idea)
+  # Calculate the jaccard index of two sets
+  # This is a coefficient between 0 (disjoint sets) and 1 (same set)
+  #
+  # Params:
+  # +a+:: the first set
+  # +b+:: the second set
+  #
+  # Autho: Mina Nagy
+  def self.jaccard_index(a, b)
+    return 0 if a.empty? && b.empty?
+    (a & b).size.to_f / (a | b).size
+  end
+
+  # Calculate the 32bit simhash of a string
+  # This is a hash with the property that 'similar' strings produce 'similar'
+  # hash values
+  #
+  # Params:
+  # +str+:: the string to hash
+  #
+  # Author: Mina Nagy
+  def self.simhash(str, ngram_len = DEFAULT_SHINGLE_LEN)
+    v = [0] * 32
+    shingle(str, ngram_len).each do |sh|
+      sh = sh.hash
+      32.times do |i|
+        if (sh & 1) == 1
+          v[i] += 1
+        else
+          v[i] -= 1
+        end
+        sh >>= 1
+      end
+    end
+
+    res = 0
+    32.times do |i|
+      res |= 1 if v[i] > 0
+      res <<= 1
+    end
+    res
+  end
+
+  # Calculate the normalized hamming distance between two Fixnums
+  # Assuming 32bit integers
+  # this uses the builtin String#count instead of doing a manual population
+  # count because this is apparently faster. Ruby. Go figure.
+  #
+  # Params:
+  # +a+:: first +Fixnum+
+  # +b+:: second +Fixnum+
+  #
+  # Author: Mina Nagy
+  def self.hamming_distance(a, b, half_width = 16)
+    return 0 if a == b && b == 0
+    half_width - (a^b).to_s(2).count('1')
+  end
+
+
+  # Split a string into a set of hashed keywords
+  # Keywords are currently defined as words more than KEYWORD_LENGTH chars in length
+  # The idea is that such a keyword can be considered as important as a tag if
+  # it is common between two Idea titles.
+  #
+  # Params:
+  # +str+:: the +String+ to split
+  #
+  # Autho: Mina Nagy
+  def self.hashed_keywords(str)
+    return [] if str.nil?
+    str = str.split
+    str.reject! { |s| s.length <= KEYWORD_LENGTH }
+    str.map(&:hash).to_set
+  end
+
+  # Calculate the compound similarity coeff weighted according to the
+  # SIMILARITY_MODEL class constant
+  #
+  # Params:
+  # +idea1+:: first +Idea+
+  # +idea2+:: second +Idea+
+  #
+  # Autho: Mina Nagy
+  def self.idea_similarity_coeff(idea1, idea2)
+    vals = {
+      tags_jacc_idx:           jaccard_index(idea1.tag_ids, idea1.tag_ids),
+      title_hamming_dist:      hamming_distance(simhash(idea1.title, TITLE_SHINGLE_LEN),
+                                                simhash(idea2.title, TITLE_SHINGLE_LEN)),
+      title_keywords_jacc_idx: jaccard_index(hashed_keywords(idea1.title),
+                                             hashed_keywords(idea2.title)),
+      desc_hamming_dist:       hamming_distance(simhash(idea1.description, DESC_SHINGLE_LEN),
+                                                simhash(idea2.description, DESC_SHINGLE_LEN)),
+      prob_hamming_dist:       hamming_distance(simhash(idea1.problem_solved, PROB_SHINGLE_LEN),
+                                                simhash(idea2.problem_solved, PROB_SHINGLE_LEN))
+    }
+
+    vals.reduce(0) do |acc, kv|
+      acc + SIMILARITY_MODEL[kv[0]] * kv[1]
+    end
+  end
+
+  # Calculate and store the similarity coeffs for an idea
+  #
+  # Params:
+  # +idea+:: +Idea+ to calculate similarities for
+  #
+  # Author: Mina Nagy
+  def self.rebuild_similarities(idea)
     # find ideas in tagged by the same tags
     ideas_in_tags = Idea.joins(:tags).where(tags: { id: idea.tags }).select('ideas.id').preload(:tags)
 
@@ -25,8 +170,7 @@ class SimilarityEngine
 
     # calculate similarity with each idea
     ideas_in_tags.find_each do |similar_idea|
-      jacc = calculate_jaccard_similarity(idea, similar_idea)
-      similarity = WEIGHT[:jaccard] * jacc
+      similarity = idea_similarity_coeff(idea, similar_idea)
 
       similarities.push("(#{idea.id},#{similarity},#{similar_idea.id},#{timestamp})," +
                         "(#{similar_idea.id},#{similarity},#{idea.id},#{timestamp})")
