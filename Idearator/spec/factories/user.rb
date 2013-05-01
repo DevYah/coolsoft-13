de666c8abd26b61fc98fafa3ebfe3ff5169d2a93
Amina Zoheir <amina.zoheir@gmail.com>
2013-05-02 01:26:44 +0200

    Issue #73 Add model tests

--- Idearator/spec/factories/user.rb
+++ Idearator/spec/factories/user.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
   factory :user, :class => User do |f|
     f.email 'useremail@domain.com'
     f.password 'mahmoudhashish'
+    f.username 'user'
   end
 
   factory :user_two, :class => User do |f|
