6167be28cc292ba2a116a99fe2485f579af9f6e9
Amina Zoheir <amina.zoheir@gmail.com>
2013-05-01 15:10:21 +0200

    Issue #73 Fix indentation

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,7 +7,7 @@ class NotificationsController < ApplicationController
       user_ids = users.collect! { |u| u.id.to_s }
       Coolster.update_each(user_ids) do |user_id|
         count = User.find(user_id).unread_notifications_count
-       render 'notifications/add_notification',
+        render 'notifications/add_notification',
               locals: { notification: notification, read: false, count: count}
       end
     end
