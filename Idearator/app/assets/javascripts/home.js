67d6c8d7dbc314ddb068553561287be7c80e8ef4
mennaamr <menna.amr2@gmail.com>
2013-05-01 02:05:22 +0200

    update tour

--- Idearator/app/assets/javascripts/home.js
+++ Idearator/app/assets/javascripts/home.js
@@ -122,17 +122,20 @@ $(document).ready(function () {
     element: ".bootstrap-tour-1", // string (jQuery selector) - html element next to which the step popover should be shown
     title: "Welcome to Idearator!", // string - title of the popover
     content: "Why don't you take a look around?", // string - content of the popover
-    placement: "down"
+    placement: "bottom",
 });
 
   tour.addStep({
+    path: "/home/index",
     element: ".btn-block", // string (jQuery selector) - html element next to which the step popover should be shown
     title: "Post your idea!", // string - title of the popover
     content: "Want to share your idea with the world? Click here to submit!", // string - content of the popover
+    backdrop: true,
     placement: "left"
 });
 
     tour.addStep({
+    path: "/home/index",
     element: ".title", // string (jQuery selector) - html element next to which the step popover should be shown
     title: "What's trending?", // string - title of the popover
     content: "Here you'll find Idearator's current top 10 ideas!", // string - content of the popover
