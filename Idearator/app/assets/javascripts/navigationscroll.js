920ff9cb2d5ba8b0e6176e88c076c95fc74e9350
Mohamed Sameh <mohamed.sameh226@gmail.com>
2013-05-02 13:04:49 +0200

    Issue #167, Fix Sign in

--- Idearator/app/assets/javascripts/navigationscroll.js
+++ Idearator/app/assets/javascripts/navigationscroll.js
@@ -1,12 +1,6 @@
 $(document).ready(function () {
   $(".scrollshow").hide();
-  $(".scrollshowbadge").hide();
   $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-        $(".scrollshowbadge").slideDown("fast");
-    } else {
-        $(".scrollshowbadge").slideUp("fast");
-    }
     if ($(this).scrollTop() > 650) {
         $(".scrollshow").slideDown("fast");
     } else {
