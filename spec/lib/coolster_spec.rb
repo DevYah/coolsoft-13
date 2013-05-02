1d209a2968e0af57d003d525ee1953ef77c589fb
Amina Zoheir <amina.zoheir@gmail.com>
2013-05-03 00:21:47 +0200

    Issue #226 Add tests

--- spec/lib/coolster_spec.rb
+++ spec/lib/coolster_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe Coolster do
 
+  before :each do
+    RestClient.stub!(:post)
+  end
   describe 'add_to_online_users' do
     it 'adds the user id passed to it to the list of online users' do
       Coolster.add_to_online_users("1")
@@ -17,12 +20,24 @@ describe Coolster do
   end
 
   describe 'update_all' do
+    it 'sends http POST request' do
+      RestClient.should_receive(:post)
+      post :update_all, script: 'alert(1);'
+    end
   end
 
   describe 'update' do
+    it'sends http POST request' do
+      RestClient.should_receive(:post)
+      post :update, script: 'alert(1);', users: ['1', '2']
+    end
   end
 
   describe 'update_each' do
+    it 'sends http POST request' do
+      RestClient.should_receive(:post)
+      post :update_each, script: 'alert(1);'
+    end
   end
 
 end
