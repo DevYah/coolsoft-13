bcc946c56791ed83983e0657950c2fff55136ad9
Mohamed Salah Nazir <mohamedsalahnazir93@gmail.com>
2013-05-02 23:11:41 +0200

    Issue 217 edits the partial of the users stream

--- Idearator/config/routes.rb
+++ Idearator/config/routes.rb
@@ -146,11 +146,6 @@ Sprint0::Application.routes.draw do
   # Note: This route will make all actions in every controller
   # accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-
-  match '/review_ideas' => 'committees#review_ideas'
-  match '/users/confirm_deactivate' => 'users#confirm_deactivate'
-  match '/users/deactivate' => 'users#deactivate'
-  match '/stream/index' => 'stream#index'
   #2.3 Create/Edit Tags
   resources :tags
 
