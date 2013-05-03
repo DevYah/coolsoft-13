6765d34fe7308969ef47a1e3bdf6d33d25951313
mennaamr <menna.amr2@gmail.com>
2013-05-03 21:27:41 +0200

    Revert registrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -33,21 +33,9 @@ class RegistrationsController < Devise::RegistrationsController
     if resource.save
       if resource.type == "Committee"
         UserMailer.committee_signup("menna.amr2@gmail.com").deliver
+        ApproveCommitteeNotification.send_notification(resource, Admin.all)
         resource.becomes(Committee).tag_ids = params[:tags]
       end
-
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_up(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      respond_with resource
     end
   end
 
