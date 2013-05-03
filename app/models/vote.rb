dcf8cd7f03f161a422c0092d3e691107506b44a4
Mina Nagy Zaki <mnzaki@gmail.com>
2013-05-03 19:54:13 +0200

    Check if user is nil in facebook_api_* hooks and change to after_save
    instead of after_create

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,9 +1,8 @@
 class Vote < ActiveRecord::Base
   attr_accessible :user_id, :idea_id, :created_at
 
-  after_save TrendsController::VoteHooks.new
-
-  after_create ::FacebookApiVote.new
+  after_save ::TrendsController::VoteHooks.new
+  after_save ::FacebookApiVote.new
 
   belongs_to :user
   belongs_to :idea , :counter_cache =>  'num_votes'
