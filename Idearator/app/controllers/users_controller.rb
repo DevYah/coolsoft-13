ec12f25b8a14bad187118698a89a62a0fdd50bc7
Gezeery <hisham.elgezeery@gmail.com>
2013-05-16 00:10:42 +0200

    repair merge - incorrect variables

--- Idearator/app/controllers/users_controller.rb
+++ Idearator/app/controllers/users_controller.rb
@@ -107,8 +107,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @approved = Idea.where(:user_id => @user.id, :archive_status => false).all
-    @admin = current_user
     @registered = @user.approved == false && @user.type == 'Committee'
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
