8cf94077a4e680471161117a7d563ff1ffe7ed39
Amina Zoheir <amina.zoheir@gmail.com>
2013-05-24 12:59:42 +0200

    Update modal sign up/in

--- Idearator/app/assets/javascripts/unauthorized_sign_in_up_modal.js
+++ Idearator/app/assets/javascripts/unauthorized_sign_in_up_modal.js
@@ -9,14 +9,26 @@ $(document).bind("ajaxError", function (e, xhr) {
 // Adds event handler on click for sign in and sign up buttons to show their forms
 // Author: Amina Zoheir
 $(document).ready(function() {
-  $('#unauth-sign-in-button').click(function () {
-    $('.unauth-huge').addClass('unauth-right', 500);
+  $('.unauth-sign-in').hide();
+
+  $('.unauth-sign-up .switch').click(function () {
+    $('.unauth-sign-up').hide("slide", { direction: "right" }, 500);
+    setTimeout(show_sign_in, 501);
   });
-  $('#unauth-sign-up-button').click(function () {
-    $('.unauth-huge').removeClass('unauth-right', 500);
+  $('.unauth-sign-in .switch').click(function () {
+    $('.unauth-sign-in').hide("slide", { direction: "right" }, 500);
+    setTimeout(show_sign_up, 501);
   });
   $('.post-an-idea').click(function (e) {
     e.preventDefault();
     $('#signedout').modal('show');
   });
 });
+
+function show_sign_up(){
+  $('.unauth-sign-up').show();
+}
+
+function show_sign_in(){
+  $('.unauth-sign-in').show();
+}
