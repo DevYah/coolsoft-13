26d1b42bc1289ddc27b9b7192c4ba48dac6ee617
Amina Zoheir <amina.zoheir@gmail.com>
2013-05-24 17:32:26 +0200

    uodate sign in/up forms in landing and modal dialog

--- Idearator/app/assets/javascripts/unauthorized_sign_in_up_modal.js
+++ Idearator/app/assets/javascripts/unauthorized_sign_in_up_modal.js
@@ -9,26 +9,30 @@ $(document).bind("ajaxError", function (e, xhr) {
 // Adds event handler on click for sign in and sign up buttons to show their forms
 // Author: Amina Zoheir
 $(document).ready(function() {
+  var w = $('#signedout').outerWidth();
+  $('#signedout').css({'margin-left':-Math.round(w/2) + 30})
   $('.unauth-sign-in').hide();
 
-  $('.unauth-sign-up .switch').click(function () {
-    $('.unauth-sign-up').hide("slide", { direction: "right" }, 500);
-    setTimeout(show_sign_in, 501);
+  $('.unauth-centered .unauth-sign-up .switch').click(function () {
+    $('.unauth-centered .unauth-sign-up').hide();
+    $('.unauth-centered .unauth-sign-in').fadeIn('slow');
   });
-  $('.unauth-sign-in .switch').click(function () {
-    $('.unauth-sign-in').hide("slide", { direction: "right" }, 500);
-    setTimeout(show_sign_up, 501);
+  $('.unauth-centered .unauth-sign-in .switch').click(function () {
+    $('.unauth-centered .unauth-sign-in').hide();
+    $('.unauth-centered .unauth-sign-up').fadeIn('slow');
   });
+
+  $('.landing-centered .unauth-sign-up .switch').click(function () {
+    $('.landing-centered .unauth-sign-up').hide();
+    $('.landing-centered .unauth-sign-in').fadeIn('slow');
+  });
+  $('.landing-centered .unauth-sign-in .switch').click(function () {
+    $('.landing-centered .unauth-sign-in').hide();
+    $('.landing-centered .unauth-sign-up').fadeIn('slow');
+  });
+
   $('.post-an-idea').click(function (e) {
     e.preventDefault();
     $('#signedout').modal('show');
   });
 });
-
-function show_sign_up(){
-  $('.unauth-sign-up').show();
-}
-
-function show_sign_in(){
-  $('.unauth-sign-in').show();
-}
