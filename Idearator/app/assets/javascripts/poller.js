0720e19740c8685981edefea40e4d2bccd54e81e
Amina Zoheir <amina.zoheir@gmail.com>
2013-05-26 17:48:20 +0200

    always poll

--- Idearator/app/assets/javascripts/poller.js
+++ Idearator/app/assets/javascripts/poller.js
@@ -5,25 +5,13 @@ function poll() {
   $.ajax({
     url: 'http://' + COOLSTER_URL + '/poll',
     dataType: "script",
-    tryCount : 0,
-    retryLimit : 5,
     success: function(data, textStatus, jXHR){
       $.ajax(this);
         return;
     },
     error: function(jXHR, textStatus, errorThrown){
-      if(errorThrown == 'timeout'){
         $.ajax(this);
         return;
-      }else{
-        tryCount ++;
-        if(tryCount <= retryLimit){
-          {setTimeout(poll, 10000)}
-        }else{
-          alert('You\'ve been disconnected');
-          return;
-        }
-      }
     },
     timeout: 60000
   });
