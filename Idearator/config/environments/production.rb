1e6b9fee7760966994aa997c570f9c2fd6d881ae
Mina Nagy Zaki <mnzaki@gmail.com>
2013-05-30 20:20:03 +0200

    Setup mailer configuration for production environment

--- Idearator/config/environments/production.rb
+++ Idearator/config/environments/production.rb
@@ -53,6 +53,18 @@ Sprint0::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.default from: ENV['DEFAULT_FROM_EMAIL']
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => ENV['SMTP_SERVER'],
+    :port                 => ENV['SMTP_PORT'],
+    :user_name            => ENV['SMTP_USERNAME'],
+    :password             => ENV['SMTP_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 
